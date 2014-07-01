0b455de3ba38977a46c067790b53dd16fbb6b27e
Craig Allwardt <Craig.Allwardt@pnnl.gov>
2014-06-30 21:40:28 -0700

    Verified that different key/signings do produce exceptions.

--- volttron/tests/sign-tests.py
+++ volttron/tests/sign-tests.py
@@ -35,6 +35,44 @@ class TestAuth(unittest.TestCase):
     def test_self_signed_cert_are_created(self):
         self.assertTrue(exists(join(KEY_DIR, PRIVATE_KEY_NAME)))
         self.assertTrue(exists(join(KEY_DIR, PUBLIC_KEY_NAME)))
+    
+    def test_unsigned_wheel(self):
+        test_wheel = 'listeneragent-0.1-py2-none-any.whl'
+        wheel_copy = join('/tmp', test_wheel)
+        try:
+            if exists(wheel_copy):
+                os.remove(wheel_copy)
+            
+            with self.assertRaises(KeyError) as context:
+                verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME))
+            
+        finally:
+            if exists(wheel_copy):
+                os.remove(wheel_copy)
+        
+
+    def test_wrong_key(self):
+        
+        test_wheel = 'listeneragent-0.1-py2-none-any.whl'
+        wheel_copy = join('/tmp', test_wheel)
+        try:
+            other_key_root= 'other'
+            other_key = create_self_signed_cert(KEY_DIR, other_key_root)
+            if exists(wheel_copy):
+                os.remove(wheel_copy)
+            
+            shutil.copy(join('fixtures', test_wheel), wheel_copy)
+            self.assertTrue(exists(wheel_copy))
+            self.assertTrue(sign_wheel(wheel_copy, join(KEY_DIR, PRIVATE_KEY_NAME)))
+            self.assertTrue(verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME)))
+            
+            with self.assertRaises(Exception) as context:
+                verify_wheel_signature(wheel_copy, join(KEY_DIR, other_key_root+'.crt'))
+            
+        finally:
+            if exists(wheel_copy):
+                os.remove(wheel_copy)
+        
 
     def test_can_sign_wheel(self):
         test_wheel = 'listeneragent-0.1-py2-none-any.whl'
