524267b74fe4020d345a3168a162efcf250832e8
Nathan Tenney <nathan.tenney@pnnl.gov>
2014-07-15 16:10:32 -0700

    Fixed code for PEP8 compliance

--- volttron/tests/packaging-tests.py
+++ volttron/tests/packaging-tests.py
@@ -5,55 +5,56 @@ import sys
 import uuid
 
 from collections import namedtuple
-from wheel.install import (WheelFile,
-                           VerifyingZipFile)
+from wheel.install import (WheelFile, VerifyingZipFile)
 
 from volttron.platform.packaging import (create_package,
                                          extract_package)
-                                 
+
 from volttron.platform.packaging import AgentPackageError
 
 # this is located in the tests/fixtures directory.
 AGENT_TESTCASE1_NAME = 'test-agent-package'
 
+
 class TestPackaging(unittest.TestCase):
 
     def get_agent_fixture(self, agent_name):
         return os.path.join(self.fixtureDir, agent_name)
-    
+
     def setUp(self):
         self.fixtureDir = os.path.join(os.path.dirname(__file__), "fixtures")
-    
+
 #     def test_can_sign_wheel(self):
 #         wheel_package = create_package(get_agent_fixture(AGENT_TESTCASE1_NAME))
-#         
-        
+#
+
     def test_can_extract_package(self):
-        
+
         wheelhouse = '/tmp/extract_package'
         expected_install_at = os.path.join(wheelhouse, 'listeneragent-0.1')
         test_wheel_name = 'listeneragent-0.1-py2-none-any.whl'
         wheel_file = os.path.join(self.fixtureDir, test_wheel_name)
-         
+
         installed_at = extract_package(wheel_file, wheelhouse)
-         
+
         try:
             self.assertIsNotNone(installed_at)
             self.assertTrue(os.path.isdir(installed_at))
             self.assertEqual(expected_install_at, installed_at)
-            
-            # use the wheel file to verify that everything was extracted properly.
+
+            # use the wheel file to verify that everything was extracted
+            # properly.
             wf = WheelFile(wheel_file)
             self.assertIsNone(wf.verify())
-            
+
             for o in wf.zipfile.infolist():
-                self.assertTrue(os.path.exists(os.path.join(expected_install_at, o.filename)))            
-            
+                self.assertTrue(
+                    os.path.exists(os.path.join(expected_install_at, o.filename)))
+
             wf.zipfile.close()
         finally:
             shutil.rmtree(installed_at)
             shutil.rmtree(wheelhouse)
-
 
     def test_can_create_package(self):
         '''
@@ -63,43 +64,44 @@ class TestPackaging(unittest.TestCase):
         agent_name = AGENT_TESTCASE1_NAME
         package_tmp_dir = "/tmp/create_package"
         expected_package_name = 'listeneragent-0.1-py2-none-any.whl'
-        
-        returned_package = create_package(self.get_agent_fixture(agent_name), package_tmp_dir)
-         
-        self.assertIsNotNone(returned_package, "Invalid package name {}".format(returned_package))
+
+        returned_package = create_package(
+            self.get_agent_fixture(agent_name), package_tmp_dir)
+
+        self.assertIsNotNone(
+            returned_package, "Invalid package name {}".format(returned_package))
         self.assertTrue(os.path.exists(returned_package))
-        self.assertEqual(expected_package_name, os.path.basename(returned_package))
+        self.assertEqual(
+            expected_package_name, os.path.basename(returned_package))
         # Wheel is in the correct location.
-        self.assertEqual(os.path.join(package_tmp_dir, expected_package_name), returned_package)
+        self.assertEqual(
+            os.path.join(package_tmp_dir, expected_package_name), returned_package)
         self.assertTrue(os.path.exists(returned_package))
-                  
+
         try:
             wf = WheelFile(returned_package)
             # sets up the expected hashes for all of the wheel directory.
             self.assertIsNone(wf.verify())
-            
+
             # Reading the files
             # if the hash doesn't match it will throw an exception.
             for o in wf.zipfile.infolist():
                 wf.zipfile.open(o).read()
-            
+
             wf.zipfile.close()
         finally:
             shutil.rmtree(package_tmp_dir)
 
-            
-    
     def test_raises_error_if_agent_dir_not_exists(self):
         '''
         This test passes under the following conditions:
             1. An AgentPackageError is thrown if the passed agent directory 
                doesen't exists.
         '''
-        # 
+        #
         fake_agent = '/tmp/Fake'
         if os.path.exists(fake_agent):
             shutil.rmtree(fake_agent, True)
-            
-        self.assertRaises(AgentPackageError, lambda: create_package(fake_agent))
-        
 
+        self.assertRaises(
+            AgentPackageError, lambda: create_package(fake_agent))
--- volttron/tests/sign-tests.py
+++ volttron/tests/sign-tests.py
@@ -9,24 +9,25 @@ import shutil
 
 from OpenSSL import crypto, SSL
 from volttron.restricted.key import create_self_signed_cert
-from volttron.restricted.auth import (sign_wheel, 
+from volttron.restricted.auth import (sign_wheel,
                                       verify_wheel_signature)
 from os.path import exists, join
 from wheel.install import WheelFile
 
 KEY_DIR = '/tmp/test_auth_key'
 KEY_ROOT = 'test'
-PRIVATE_KEY_NAME = KEY_ROOT+'.key'
-PUBLIC_KEY_NAME = KEY_ROOT+'.crt'
+PRIVATE_KEY_NAME = KEY_ROOT + '.key'
+PUBLIC_KEY_NAME = KEY_ROOT + '.crt'
+
 
 class TestAuth(unittest.TestCase):
-    
+
     @classmethod
     def setUpClass(cls):
         if not os.path.exists(KEY_DIR):
             os.makedirs(KEY_DIR)
-            assert(create_self_signed_cert(KEY_DIR, KEY_ROOT) == True)
-    
+            assert(create_self_signed_cert(KEY_DIR, KEY_ROOT) is True)
+
     @classmethod
     def tearDownClass(cls):
         if os.path.exists(KEY_DIR):
@@ -35,44 +36,46 @@ class TestAuth(unittest.TestCase):
     def test_self_signed_cert_are_created(self):
         self.assertTrue(exists(join(KEY_DIR, PRIVATE_KEY_NAME)))
         self.assertTrue(exists(join(KEY_DIR, PUBLIC_KEY_NAME)))
-    
+
     def test_unsigned_wheel(self):
         test_wheel = 'listeneragent-0.1-py2-none-any.whl'
         wheel_copy = join('/tmp', test_wheel)
         try:
             if exists(wheel_copy):
                 os.remove(wheel_copy)
-            
+
             with self.assertRaises(KeyError) as context:
-                verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME))
-            
+                verify_wheel_signature(
+                    wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME))
+
         finally:
             if exists(wheel_copy):
                 os.remove(wheel_copy)
-        
 
     def test_wrong_key(self):
-        
+
         test_wheel = 'listeneragent-0.1-py2-none-any.whl'
         wheel_copy = join('/tmp', test_wheel)
         try:
-            other_key_root= 'other'
+            other_key_root = 'other'
             other_key = create_self_signed_cert(KEY_DIR, other_key_root)
             if exists(wheel_copy):
                 os.remove(wheel_copy)
-            
+
             shutil.copy(join('fixtures', test_wheel), wheel_copy)
             self.assertTrue(exists(wheel_copy))
-            self.assertTrue(sign_wheel(wheel_copy, join(KEY_DIR, PRIVATE_KEY_NAME)))
-            self.assertTrue(verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME)))
-            
+            self.assertTrue(
+                sign_wheel(wheel_copy, join(KEY_DIR, PRIVATE_KEY_NAME)))
+            self.assertTrue(
+                verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME)))
+
             with self.assertRaises(Exception) as context:
-                verify_wheel_signature(wheel_copy, join(KEY_DIR, other_key_root+'.crt'))
-            
+                verify_wheel_signature(
+                    wheel_copy, join(KEY_DIR, other_key_root + '.crt'))
+
         finally:
             if exists(wheel_copy):
                 os.remove(wheel_copy)
-        
 
     def test_can_sign_wheel(self):
         test_wheel = 'listeneragent-0.1-py2-none-any.whl'
@@ -83,13 +86,14 @@ class TestAuth(unittest.TestCase):
 
             shutil.copy(join('fixtures', test_wheel), wheel_copy)
             self.assertTrue(exists(wheel_copy))
-            self.assertTrue(sign_wheel(wheel_copy, join(KEY_DIR, PRIVATE_KEY_NAME)))
-            self.assertTrue(verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME)))
+            self.assertTrue(
+                sign_wheel(wheel_copy, join(KEY_DIR, PRIVATE_KEY_NAME)))
+            self.assertTrue(
+                verify_wheel_signature(wheel_copy, join(KEY_DIR, PUBLIC_KEY_NAME)))
         finally:
             if exists(wheel_copy):
                 os.remove(wheel_copy)
-        
-        
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
