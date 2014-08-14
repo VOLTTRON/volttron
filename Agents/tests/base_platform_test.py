20f03e1a5c7c6c807d5379fb17631e19e74c2991
Craig Allwardt <Craig.Allwardt@pnnl.gov>
2014-08-14 16:11:01 -0700

    Put things in place so that we can build temp files for configuration and keep smap keys seperate.

--- Agents/tests/base_platform_test.py
+++ Agents/tests/base_platform_test.py
@@ -1,11 +1,13 @@
+
 import os
 import shutil
 import subprocess
 import sys
 import time
 import tempfile
-
 import unittest
+
+from contextlib import closing
 
 
 #All paths relative to proj-dir/volttron
@@ -19,39 +21,52 @@ VCTRL = "env/bin/volttron-ctl"
 # STOP_AGENT = "stop-agent"
 # START_AGENT = "start-agent"
 # BUILD_AGENT = "volttron/scripts/build-agent.sh"
-# CONFIG_FILE = "test-config.ini"
+CONFIG_FILE = "test-config.ini"
+SMAP_FILE = "test-smap.ini"
+
+PLATFORM_CONFIG = """
+"""
+
+TWISTED_CONFIG = """
+"""
+
+SMAP_KEY = ""
 
 class BasePlatformTest(unittest.TestCase):
-    
-    @classmethod
-    def writePConfig(cls):
-    
+
     @classmethod
     def setUpClass(cls):
         '''Setup platform here (only called on time per class)'''
         os.chdir('../../')
-        
+
         cls.tempdir = tempfile.mkdtemp()
-        
-        
-        
-        cls.p_process = subprocess.Popen([VSTART])
-#                                           "-c", CONFIG_FILE, "-v", "-l", "volttron.log"])
-        cls.t_process = subprocess.Popen(["twistd", "-n", "smap", "test-smap.ini"])
+
+        pconfig = os.path.join(cls.tmpdir, CONFIG_FILE)
+        with closing(open(pconfig, 'w')) as cfg:
+            cfg.write(PLATFORM_CONFIG)
+
+        tconfig = os.path.join(cls.tmpdir, SMAP_FILE)
+        with closing(open(tconfig, 'w')) as cfg:
+            cfg.write(TWISTED_CONFIG.format(SMAP_KEY))
+
+        lfile = os.path.join(cls.tmpdir, "volttron.log")
+
+        cls.p_process = subprocess.Popen([VSTART, "-c", pconfig, "-v", "-l", lfile])
+        cls.t_process = subprocess.Popen(["twistd", "-n", "smap", tconfig])
+        #cls.t_process = subprocess.Popen(["twistd", "-n", "smap", "test-smap.ini"])
         time.sleep(3)
-    
+
     @classmethod
     def tearDownClass(cls):
         '''Stop platform here'''
         if cls.p_process != None:
             cls.p_process.kill()
-        else: 
+        else:
             print "NULL"
-        
+
         if cls.t_process != None:
             cls.t_process.kill()
-        else: 
+        else:
             print "NULL"
-            
+
         shutil.rmtree(cls.tempdir, True)
-    
