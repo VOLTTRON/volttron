3698ed8e8bb9ee735e9b80fe9092165a16ba7184
Craig Allwardt <Craig.Allwardt@pnnl.gov>
2014-12-01 16:44:10 -0800

    Added absolute path resolution for testing.

--- volttron/tests/platform_test.py
+++ volttron/tests/platform_test.py
@@ -1,15 +1,30 @@
+import os
+import sys
 import unittest
+
+from os.path import dirname
+
 from platform_wrapper import PlatformWrapper
 
+_VOLTRON = dirname(os.path.join(os.path.abspath(dirname(__file__))))
+
 class PlatformTests(unittest.TestCase):
-    
+
     def setUp(self):
         self.platform = PlatformWrapper()
-        
+
     def test_platform_startup(self):
-        self.platform.startup_platform("base-platform-test.json")
+        self.platform.startup_platform(
+                    os.path.join(_VOLTRON, "base-platform-test.json"),
+                    use_twistd=True)
         self.assertIsNotNone(self.platform.p_process, "Platform process is none")
         self.assertIsNotNone(self.platform.t_process, "Twistd process is none")
+
+    def test_platform_startup_no_twistd(self):
+        self.platform.startup_platform(
+                    os.path.join(_VOLTRON, "base-platform-test.json"))
+        self.assertIsNotNone(self.platform.p_process, "Platform process is none")
+
 
 
     def tearDown(self):
