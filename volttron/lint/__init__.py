35adacf598ea51685a8a0b9fcae25b006e79fdf6
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2014-12-11 12:42:02 -0800

    Add README and copyright to volttron.lint.

--- volttron/lint/__init__.py
+++ volttron/lint/__init__.py
@@ -1,3 +1,76 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+
+# Copyright (c) 2013, Battelle Memorial Institute
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation
+# are those of the authors and should not be interpreted as representing
+# official policies, either expressed or implied, of the FreeBSD
+# Project.
+#
+# This material was prepared as an account of work sponsored by an
+# agency of the United States Government.  Neither the United States
+# Government nor the United States Department of Energy, nor Battelle,
+# nor any of their employees, nor any jurisdiction or organization that
+# has cooperated in the development of these materials, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness or any
+# information, apparatus, product, software, or process disclosed, or
+# represents that its use would not infringe privately owned rights.
+#
+# Reference herein to any specific commercial product, process, or
+# service by trade name, trademark, manufacturer, or otherwise does not
+# necessarily constitute or imply its endorsement, recommendation, or
+# favoring by the United States Government or any agency thereof, or
+# Battelle Memorial Institute. The views and opinions of authors
+# expressed herein do not necessarily state or reflect those of the
+# United States Government or any agency thereof.
+#
+# PACIFIC NORTHWEST NATIONAL LABORATORY
+# operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# under Contract DE-AC05-76RL01830
+#}}}
+
+'''
+volttron.lint -- a pylint plugin to mask spurious dynamic attribute errors.
+
+volttron.lint is a pylint plugin to mask spurious errors produced by pylint
+when objects with dynamic attributes are used. Dynamic attributes are those not
+defined directly on the object but are instead added by a metaclass or through
+calls to __getattribute__(). This module allows adding fake modules/classes
+which are added to the lookup mechanism to allow pylint to properly check
+dynamic attributes.
+
+Each fake module should be contained in a file in this directory named the same
+as the module it fakes plus a '.py' extension. Upon loading the plugin, a
+module transform function will be registered which will read the module file,
+if it exists, and create and add the appropriate objects and attributes to the
+namespace for that class. See the existing files for details.
+'''
 
 import os
 
@@ -6,21 +79,18 @@ from astroid.builder import AstroidBuilder
 
 
 def transform(module):
-    #print 'transform', module.name
+    '''Add fake locals to a module's namespace.'''
+    # Generate the path to the fake module
     dirname = os.path.dirname(__file__)
     path = os.path.join(dirname, module.name + '.py')
     if not os.path.exists(path):
         return
-    #print 'faking', module.name
+    # If the file exists, add fakes to the module's namespace
     fake = AstroidBuilder(MANAGER).file_build(path)
     for name, obj in fake.locals.iteritems():
-        try:
-            objlist = module.locals[name]
-        except KeyError:
-            module.locals[name] = obj
-        else:
-            objlist.extend(obj)
+        module.locals.setdefault(name, []).extend(obj)
 
 
 def register(linter):
+    '''Register the transform function during plugin registration.'''
     MANAGER.register_transform(nodes.Module, transform)
