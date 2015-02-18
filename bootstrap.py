94a6380173f41d26ddd9e6603a77af8df1acb447
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2015-02-18 12:09:58 -0800

    Added advanced control of bootstrap script, including update option.

--- bootstrap.py
+++ bootstrap.py
@@ -1,16 +1,41 @@
 from __future__ import print_function
 
+import argparse
+import errno
+import logging
 import os
 import subprocess
 import sys
 
 
-_path = os.path.dirname(__file__) or os.getcwd()
-_envdir = os.path.join(_path, 'env')
-_prompt = '(volttron) '
+_log = logging.getLogger(__name__)
+
+_WINDOWS = sys.platform.startswith('win')
 
 
-def stage1(directory=_envdir, prompt=_prompt):
+def check_call(args):
+    '''Run a subprocess exiting if an error occurs.'''
+    result = subprocess.call(args)
+    if result:
+        sys.exit(result)
+
+
+def shescape(args):
+    '''Return a sh shell escaped string composed of args.'''
+    return ' '.join('{1}{0}{1}'.format(arg.replace('"', '\\"'),
+                    '"' if ' ' in arg else '') for arg in args)
+
+
+def bootstrap_virtenv(dest, prompt='(volttron)',
+                      version=None, verbose=None):
+    '''Download latest virtualenv and create a virtual environment.
+
+    The virtual environment will be created in the given directory. The
+    shell prompt in the virtual environment can be overridden by setting
+    prompt and a specific version of virtualenv can be used by passing
+    the version string into version.
+    '''
+    # Imports used only for bootstrapping the environment
     import contextlib
     import re
     import shutil
@@ -19,35 +44,49 @@ def stage1(directory=_envdir, prompt=_prompt):
     import urllib2
 
     class EnvBuilder(object):
+        '''Virtual environment builder.
+
+        The resulting python executable will be set in the env_exe
+        attribute.
+        '''
+
+        __slots__ = ['version', 'prompt', 'env_exe']
+
         def __init__(self, version=None, prompt=None):
+            '''Allow overriding version and prompt.'''
             self.version = version
             self.prompt = prompt
             self.env_exe = None
 
         def _fetch(self, url):
+            '''Open url and return the response object (or bail).'''
+            _log.debug('Fetching %s', url)
             response = urllib2.urlopen(url)
             if response.getcode() != 200:
-                print('Server response:', response.code,
-                      response.msg, file=sys.stderr)
-                print('Error: Download failed!', file=sys.stderr)
+                _log.error('Server response is %s %s',
+                           response.code, response.msg)
+                _log.fatal('Download failed!')
                 sys.exit(1)
             return response
 
         def get_version(self):
-            print('Downloading virtualenv DOAP record')
-            doap_url = 'https://pypi.python.org/pypi?:action=doap&name=virtualenv'
+            '''Return the latest version from virtualenv DOAP record.'''
+            _log.info('Downloading virtualenv DOAP record')
+            doap_url = ('https://pypi.python.org/pypi'
+                        '?:action=doap&name=virtualenv')
             with contextlib.closing(self._fetch(doap_url)) as response:
                 doap_xml = response.read()
             self.version = re.search(
-                    r'<revision>([^<]*)</revision>', doap_xml).group(1)
+                r'<revision>([^<]*)</revision>', doap_xml).group(1)
             return self.version
 
         def download(self, directory):
+            '''Download the virtualenv tarball into directory.'''
             if self.version is None:
                 self.get_version()
-            url = ('https://pypi.python.org/packages/source/v/virtualenv/' +
+            url = ('https://pypi.python.org/packages/source/v/virtualenv/'
                    'virtualenv-{}.tar.gz'.format(self.version))
-            print('Downloading virtualenv')
+            _log.info('Downloading virtualenv %s', self.version)
             tarball = os.path.join(directory, 'virtualenv.tar.gz')
             with contextlib.closing(self._fetch(url)) as response:
                 with open(tarball, 'wb') as file:
@@ -55,74 +94,281 @@ def stage1(directory=_envdir, prompt=_prompt):
             with contextlib.closing(tarfile.open(tarball, 'r|gz')) as archive:
                 archive.extractall(directory)
 
-        def create(self, directory):
+        def create(self, directory, verbose=None):
+            '''Create a virtual environment in directory.'''
             tmpdir = tempfile.mkdtemp()
             try:
                 self.download(tmpdir)
                 args = [sys.executable]
-                args.append(os.path.join(tmpdir,
-                        'virtualenv-{}'.format(self.version), 'virtualenv.py'))
+                args.append(os.path.join(tmpdir, 'virtualenv-{}'.format(
+                    self.version), 'virtualenv.py'))
+                if verbose:
+                    args.append('--verbose')
+                elif verbose is not None:
+                    args.append('--quiet')
                 if self.prompt:
                     args.extend(['--prompt', prompt])
                 args.append(directory)
-                subprocess.check_call(args)
-                if sys.platform == 'win32':
-                    self.env_exe = os.path.join(directory, 'scripts', 'python.exe')
+                _log.debug('+ %s', shescape(args))
+                check_call(args)
+                if _WINDOWS:
+                    self.env_exe = os.path.join(
+                        directory, 'Scripts', 'python.exe')
                 else:
                     self.env_exe = os.path.join(directory, 'bin', 'python')
                 assert(os.path.exists(self.env_exe))
             finally:
                 shutil.rmtree(tmpdir, ignore_errors=True)
 
-    # Install the virtual environment.
-    builder = EnvBuilder(prompt=prompt)
-    builder.create(directory)
-    # Run this script within the virtual environment for stage2
-    subprocess.check_call([builder.env_exe, __file__])
+    _log.info('Creating virtual Python environment')
+    builder = EnvBuilder(prompt=prompt, version=version)
+    builder.create(dest, verbose)
+    return builder.env_exe
 
 
 def split_requirement(req):
+    '''Return package name (without version) and requirement tuple.'''
     for i, c in enumerate(req):
         if not c.isalnum() and c not in '.-_':
             return req[:i].lower(), req
     return req.lower(), req
 
 
-def stage2(directory=_path):
+def get_requirements(requirements_path=None):
+    '''Get a list for requirements from setup.py.
+
+    Optional requirements file may also be parsed.
+
+    Returns a dictionary with package names as key.
+    '''
     from setup import install_requires
-    requirements = dict(split_requirement(req) for req in install_requires)
-    requirements_txt = os.path.join(_path, 'requirements.txt')
-    if os.path.exists(requirements_txt):
-        with open(requirements_txt) as file:
-            requirements.update(
-                    dict(split_requirement(req.rstrip()) for req in file))
-    # Install packages provided only as eggs
-    args = [sys.executable, '-m', 'easy_install']
-    for name in ['bacpypes']:
-        args.append(requirements.get(name, name))
-    print(' '.join(args))
-    subprocess.check_call(args)
+    class ReqDict(dict):
+        def __missing__(self, key):
+            return self.get(key.lower(), key)
+    results = ReqDict(split_requirement(req) for req in install_requires)
+    if requirements_path and os.path.exists(requirements_path):
+        with open(requirements_path) as file:
+            results.update(dict(split_requirement(req) for req in
+                                (line.strip() for line in file)
+                                if req and not req.startswith('#')))
+    return results
+
+
+def call_module(args):
+    '''Call a python module with the given args in a separate process.
+
+    Exit if the return code is non-zero.
+    '''
+    _log.info('+ %s', shescape(args))
+    check_call([sys.executable, '-m'] + args)
+
+
+def pip_install(args, verbose=None, upgrade=False):
+    '''Call virtual environment `pip install`.'''
+    cmd = ['pip', 'install']
+    if verbose:
+        cmd.extend(['--verbose', '--global-option', '--verbose'])
+    elif verbose is None:
+        cmd.extend(['--global-option', '--quiet'])
+    else:
+        cmd.extend(['--quiet', '--global-option', '--quiet'])
+    if upgrade:
+        cmd.append('--upgrade')
+    cmd.extend(args)
+    call_module(cmd)
+
+
+def easy_install(args, verbose=None, upgrade=False):
+    '''Call virtual environment easy_install.'''
+    cmd = ['easy_install']
+    if verbose:
+        cmd.append('--verbose')
+    elif verbose is not None:
+        cmd.append('--quiet')
+    if upgrade:
+        cmd.append('--upgrade')
+    cmd.extend(args)
+    call_module(cmd)
+
+
+def install(verbose=False):
+    '''Install dependencies in setup.py and requirements.txt.'''
+    path = os.path.dirname(__file__) or '.'
+    _log.info('Installing required packages')
+    requirements_txt = os.path.join(path, 'requirements.txt')
+    requirements = get_requirements(requirements_txt)
+    # Upgrade pip to avoid warning upgrade messages
+    pip_install(['pip'], verbose, True)
+    # Get bacpypes with easy_install, because it is only provided as an egg
+    easy_install([requirements['BACpypes']], verbose)
+    # Build pyzmq separately to pass install options
+    pip_install(
+        ['--install-option', '--zmq=bundled', requirements['pyzmq']], verbose)
     # Install local packages and remaining dependencies
-    args = [sys.executable, '-m', 'pip', 'install',
-            '--global-option', '-q',
-            '-e', os.path.join(directory, 'lib', 'jsonrpc'),
-            '-e', os.path.join(directory, 'lib', 'clock'),
-            '-e', directory]
+    args = ['--editable', os.path.join(path, 'lib', 'jsonrpc'),
+            '--editable', os.path.join(path, 'lib', 'clock'),
+            '--editable', path]
     if os.path.exists(requirements_txt):
-        args.extend(['-r', requirements_txt])
-    print(' '.join(args))
-    subprocess.check_call(args)
+        args.extend(['--requirement', requirements_txt])
+    pip_install(args, verbose)
 
 
-def main(directory=_envdir, prompt=_prompt):
+def upgrade(packages, verbose=False):
+    '''Upgrade installed packages.
+
+    packages should be a list of packages to be upgraded or evaluate to
+    False to upgrade all packages. If the first element in packages is
+    '!', all packages except those in packages[1:] will be upgraded.
+    '''
+    from pip.utils import get_installed_distributions
+    path = os.path.dirname(__file__) or '.'
+    requirements = get_requirements(os.path.join(path, 'requirements.txt'))
+    if packages and packages[0] != '!':
+        packages = {name.lower(): requirements[name] for name in packages}
+    else:
+        exclude = packages[1:] if packages else []
+        exclude.extend(['volttron', 'flexible-jsonrpc', 'posix-clock'])
+        packages = {dist.project_name.lower(): requirements[dist.project_name]
+                    for dist in
+                        get_installed_distributions(include_editables=False)}
+        for name in exclude:
+            packages.pop(name.lower(), None)
+
+    _log.info('Updating packages')
+    # Update bacpypes with easy_install, because it is only provided as an egg
+    requirement = packages.pop('bacpypes', None)
+    if requirement:
+        easy_install([requirement], verbose, True)
+    # Update pip before others to limit upgrade warning message
+    requirement = packages.pop('pip', None)
+    if requirement:
+        pip_install([requirement], verbose, True)
+    # Update pyzmq separately to pass install options
+    requirement = packages.pop('pyzmq', None)
+    if requirement:
+        pip_install(['--install-option', '--zmq=bundled', requirement],
+                    verbose, True)
+    # Update remaining packages
+    if packages:
+        pip_install(packages.values(), verbose, True)
+
+
+class HelpFormatter(argparse.HelpFormatter):
+    '''Custom formatter to improve -u/--upgrade help.'''
+    def _format_action_invocation(self, action):
+        if action.dest == 'upgrade':
+            return '-u, --upgrade [[!] PKG...]'
+        return super(HelpFormatter, self)._format_action_invocation(action)
+
+
+def main(argv=sys.argv):
+    '''Script entry point.'''
     # Unfortunately, many dependencies are not yet available in Python3.
     if sys.version_info[:2] != (2, 7):
         sys.stderr.write('error: Python 2.7 is required\n')
         sys.exit(1)
+
+    # Build the parser
+    python = os.path.join('$VIRTUAL_ENV',
+                          'Scripts' if _WINDOWS  else 'bin', 'python')
+    if _WINDOWS:
+        python += '.exe'
+    parser = argparse.ArgumentParser(
+        description='Bootstrap and update a virtual Python environment '
+                    'for VOLTTRON development.',
+        usage='\n  bootstrap: python2.7 %(prog)s [OPTIONS]...'
+              '\n  update:    {} %(prog)s [OPTIONS]...'.format(python),
+        prog=os.path.basename(argv[0]),
+        formatter_class=HelpFormatter,
+        epilog='''
+            The first invocation of this script, which should be made
+            using the system, Python, will create a virtual Python
+            environment in a subdirectory named 'env' in the same directory
+            as this script or in the directory given by the --envdir option.
+            Subsequent invocations of this script should use the Python
+            executable installed in the virtual environment. If no arguments
+            are given with the -u/--upgrade option, all installed packages
+            will be upgraded. Packages may be limited, by listing the
+            packages that should be updated, or excluded, by including a
+            bang (!) as the first item in the list. Some shells may require
+            escaping the bang.'''
+
+    )
+    verbose = parser.add_mutually_exclusive_group()
+    verbose.add_argument(
+        '-q', '--quiet', dest='verbose', action='store_const', const=False,
+        help='produce less output')
+    verbose.add_argument(
+        '-v', '--verbose', action='store_const', const=True,
+        help='produce extra output')
+    bs = parser.add_argument_group('bootstrap options')
+    bs.add_argument(
+        '--envdir', default=None, metavar='VIRTUAL_ENV',
+        help='alternate location for virtual environment')
+    bs.add_argument(
+        '--force', action='store_true',
+        help='force installing in non-empty directory')
+    bs.add_argument(
+        '-o', '--only-virtenv', action='store_true',
+        help='create virtual environment and exit (skip install)')
+    bs.add_argument(
+        '--prompt', default='(volttron)', help='provide alternate prompt '
+        'in activated environment (default: %(default)s)')
+    bs.add_argument('--force-version', help=argparse.SUPPRESS)
+    up = parser.add_argument_group('update options')
+    up.add_argument(
+        '-u', '--upgrade', nargs='*', metavar='PKG',
+        help='upgrade installed packages')
+    path = os.path.dirname(__file__) or os.getcwd()
+    parser.set_defaults(envdir=os.path.join(path, 'env'))
+    options = parser.parse_args(argv[1:])
+
+    # Route errors to stderr, info and debug to stdout
+    error_handler = logging.StreamHandler(sys.stderr)
+    error_handler.setLevel(logging.WARNING)
+    error_handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+    info_handler = logging.StreamHandler(sys.stdout)
+    info_handler.setLevel(logging.DEBUG)
+    info_handler.setFormatter(logging.Formatter('%(message)s'))
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG if options.verbose else logging.INFO)
+    root.addHandler(error_handler)
+    root.addHandler(info_handler)
+
+    # Main script logic to perform bootstrapping or updating
     if hasattr(sys, 'real_prefix'):
-        stage2()
+        # The script was called from a virtual environment Python, so update
+        if options.upgrade is not None:
+            upgrade(options.upgrade, options.verbose)
+        else:
+            install(options.verbose)
     else:
-        stage1()
+        # The script was called from the system Python, so bootstrap
+        try:
+            # Refuse to create environment in existing, non-empty
+            # directory without the --force flag.
+            if os.listdir(options.envdir):
+                if not options.force:
+                    parser.print_usage(sys.stderr)
+                    print('{}: error: directory exists and is not empty: {}'
+                          .format(parser.prog, options.envdir), file=sys.stderr)
+                    print('Use the virtual Python to update or use '
+                          'the --force option to overwrite.', file=sys.stderr)
+                    parser.exit(1)
+                _log.warning('using non-empty environment directory: %s',
+                             options.envdir)
+        except OSError as exc:
+            if exc.errno != errno.ENOENT:
+                raise
+        env_exe = bootstrap_virtenv(options.envdir, options.prompt,
+                                    options.force_version, options.verbose)
+        if options.only_virtenv:
+            return
+        # Run this script within the virtual environment for stage2
+        args = [env_exe, __file__]
+        if options.verbose is not None:
+            args.append('--verbose' if options.verbose else '--quiet')
+        check_call(args)
 
 
 if __name__ == "__main__":
