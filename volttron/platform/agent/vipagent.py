3280767a4e11a9bb64f7a8374dea9c65157225e0
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2015-03-19 13:02:36 -0700

    Remove overuse of debug logging.

--- volttron/platform/agent/vipagent.py
+++ volttron/platform/agent/vipagent.py
@@ -227,44 +227,25 @@ class VIPAgent(object):
             for event, callback in self._meta.event_callbacks:
                 if event == trigger:
                     callback(self)
-        _log.debug('generating periodic callbacks')
         for definition in self._meta.periodics:
             callback = definition.callback(self)
             self._periodics.append(callback)
-        _log.debug('trigging setup events')
         _trigger_event('setup')
-        _log.debug('setup events complete')
-        _log.debug('connecting')
         self.vip_socket.connect(self.vip_address)
-        _log.debug('triggering connect events')
         _trigger_event('connect')
-        _log.debug('connect events complete')
-        _log.debug('starting periodics')
         # Start periodic callbacks
         for periodic in self._periodics:
             periodic.start()
-        _log.debug('triggering start events')
         _trigger_event('start')
-        _log.debug('start events complete')
-        _log.debug('entering main loop')
         try:
             self._vip_loop()
         finally:
-            _log.debug('main loop complete')
-            _log.debug('triggering stop events')
             _trigger_event('stop')
-            _log.debug('stop events complete')
-            _log.debug('stopping periodics')
             for periodic in self._periodics:
                 periodic.kill()
-            _log.debug('triggering disconnect events')
             _trigger_event('disconnect')
-            _log.debug('disconnect events complete')
-            _log.debug('disconnecting')
             self.vip_socket.disconnect(self.vip_address)
-            _log.debug('triggering finish events')
             _trigger_event('finish')
-            _log.debug('finish events complete')
 
     def _vip_loop(self):
         socket = self.vip_socket
