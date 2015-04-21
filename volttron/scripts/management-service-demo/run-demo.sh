4a969c2b0f74ffcf3f667c321441415cb0c6ce22
craig8 <craig.allwardt@pnnl.gov>
2015-04-21 16:12:07 -0700

    #33
    Demo running with 3 platforms now.  Added code to log where we are in
    the demo.

--- volttron/scripts/management-service-demo/run-demo.sh
+++ volttron/scripts/management-service-demo/run-demo.sh
@@ -1,15 +1,21 @@
+. demo-vars.sh
+
 ./demo-setup.sh
 ./start-platforms.sh
-VOLTTRON_HOME=~/v1home ./make-management
-VOLTTRON_HOME=~/v1home ./make-platform1
-VOLTTRON_HOME=~/v2home ./make-platform2
-VOLTTRON_HOME=~/v1home ./make-hello1
-VOLTTRON_HOME=~/v2home ./make-hello2
+VOLTTRON_HOME=$V1_HOME ./make-management
+VOLTTRON_HOME=$V1_HOME ./make-platform1
+VOLTTRON_HOME=$V2_HOME ./make-platform2
+VOLTTRON_HOME=$V3_HOME ./make-platform3
+#VOLTTRON_HOME=$V1_HOME ./make-hello1
+#VOLTTRON_HOME=$V2_HOME ./make-hello2
 
-VOLTTRON_HOME=~/v1home volttron-ctl start --tag manageragent
-VOLTTRON_HOME=~/v1home volttron-ctl start --tag platformagent
+echo "starting platform manager"
+VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag manageragent
+echo "starting platform agent 1"
+VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag platformagent
+echo "starting platform agent 2"
 VOLTTRON_HOME=~/v2home volttron-ctl start --tag platformagent
 
-
+echo "Launching browser"
 x-www-browser http://localhost:8080
 
