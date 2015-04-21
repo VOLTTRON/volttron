2c44a4e203bad14d25ab56c5f0051ba861bb3b8d
craig8 <craig.allwardt@pnnl.gov>
2015-04-21 15:46:01 -0700

    Added demo-vars for global variables and fixed the make-management and start-platforms.
    Agent homes are now in /tmp/v1home, /tmp/v2home and /tmp/v3home
    respectively for the demo.  They are assigned in demo-vars.sh
    
    #33 
    @jhaack

---
+++ volttron/scripts/management-service-demo/demo-vars.sh
@@ -0,0 +1,3 @@
+V1_HOME=/tmp/v1home
+V2_HOME=/tmp/v2home
+V3_HOME=/tmp/v3home
--- volttron/scripts/management-service-demo/start-platforms.sh
+++ volttron/scripts/management-service-demo/start-platforms.sh
@@ -1,3 +1,10 @@
-VOLTTRON_HOME=~/v1home volttron&
-VOLTTRON_HOME=~/v2home volttron&
+# bring in the vars that are to be available for this script
+. demo-vars.sh
+
+echo "Starting platform 1"
+VOLTTRON_HOME=$V1_HOME volttron -vv&
+echo "Starting platform 2"
+VOLTTRON_HOME=$V2_HOME volttron -vv&
+echo "Starting platform 3"
+VOLTTRON_HOME=$V3_HOME volttron -vv&
 
