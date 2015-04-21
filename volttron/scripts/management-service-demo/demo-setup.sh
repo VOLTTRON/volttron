fedbeb33f41c082c13b68df523ad1689e1364be2
craig8 <craig.allwardt@pnnl.gov>
2015-04-21 16:04:45 -0700

    Added creation of demo platform homes in temp dir.
    Stop platform now stops all 3 platforms.
    
    #33

--- volttron/scripts/management-service-demo/demo-setup.sh
+++ volttron/scripts/management-service-demo/demo-setup.sh
@@ -1,2 +1,19 @@
-mkdir ~/v1home
-mkdir ~/v2home
+. demo-vars.sh
+
+echo "(Re)creating Platform Directories:"
+echo "$V1_HOME, $V2_HOME, $V3_HOME"
+if [ -d "$V1_HOME" ]; then
+  rm $V1_HOME -rf
+fi
+
+if [ -d "$V2_HOME" ]; then
+  rm $V2_HOME -rf
+fi
+
+if [ -d "$V3_HOME" ]; then
+  rm $V3_HOME -rf
+fi
+
+mkdir $V1_HOME
+mkdir $V2_HOME
+mkdir $V3_HOME
--- volttron/scripts/management-service-demo/stop-platforms.sh
+++ volttron/scripts/management-service-demo/stop-platforms.sh
@@ -1,4 +1,10 @@
-VOLTTRON_HOME=~/v1home volttron-ctl shutdown --platform&
-VOLTTRON_HOME=~/v2home volttron-ctl shutdown --platform&
+. demo-vars.sh
+
+echo "Shutting down platform 1"
+VOLTTRON_HOME=$V1_HOME volttron-ctl shutdown --platform&
+echo "Shutting down platform 2"
+VOLTTRON_HOME=$V2_HOME volttron-ctl shutdown --platform&
+echo "Shutting down platform 3"
+VOLTTRON_HOME=$V3_HOME volttron-ctl shutdown --platform&
 
 
