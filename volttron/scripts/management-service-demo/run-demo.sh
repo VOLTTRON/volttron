8349fe770819d13283159319a46a36956ceaeb3a
craig8 <craig.allwardt@pnnl.gov>
2015-04-22 10:52:17 -0700

    Uncommented run agent code.

--- volttron/scripts/management-service-demo/run-demo.sh
+++ volttron/scripts/management-service-demo/run-demo.sh
@@ -16,19 +16,19 @@ echo "START make-platform2"
 VOLTTRON_HOME=$V2_HOME ./make-platform2
 echo "START make-platform3"
 VOLTTRON_HOME=$V3_HOME ./make-platform3
-#echo "START make-hello1"
-#VOLTTRON_HOME=$V1_HOME ./make-hello1
-#echo "START make-hello2"
+echo "START make-hello1"
+VOLTTRON_HOME=$V1_HOME ./make-hello1
+echo "START make-hello2"
 
 echo "starting platform manager"
 VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag manageragent
 
-#echo "starting platform agent 1"
-#VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag platformagent
+echo "starting platform agent 1"
+VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag platformagent
 echo "starting platform agent 2"
 VOLTTRON_HOME=$V2_HOME volttron-ctl start --tag platformagent
-#echo "starting platform agent 3"
-#VOLTTRON_HOME=$V3_HOME volttron-ctl start --tag platformagent
+echo "starting platform agent 3"
+VOLTTRON_HOME=$V3_HOME volttron-ctl start --tag platformagent
 
 
 echo "Launching browser"
