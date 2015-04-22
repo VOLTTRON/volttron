550c958a14054baaa9724db3542d502607715f0b
craig8 <craig.allwardt@pnnl.gov>
2015-04-22 07:50:58 -0700

    Updated service demo files to be generic placement in /tmp

--- volttron/scripts/management-service-demo/run-demo.sh
+++ volttron/scripts/management-service-demo/run-demo.sh
@@ -1,15 +1,34 @@
+. demo-vars.sh
+
 ./demo-setup.sh
-./start-platforms.sh
-VOLTTRON_HOME=~/v1home ./make-management
-VOLTTRON_HOME=~/v1home ./make-platform1
-VOLTTRON_HOME=~/v2home ./make-platform2
-VOLTTRON_HOME=~/v1home ./make-hello1
-VOLTTRON_HOME=~/v2home ./make-hello2
+echo "START start-platforms"
+VOLTTRON_HOME=$V1_HOME volttron -vv&
+echo "Starting platform 2"
+VOLTTRON_HOME=$V2_HOME volttron -vv&
+echo "Starting platform 3"
+VOLTTRON_HOME=$V3_HOME volttron -vv&
+echo "START make-management"
+VOLTTRON_HOME=$V1_HOME ./make-management
+echo "START make-platform1"
+VOLTTRON_HOME=$V1_HOME ./make-platform1
+echo "START make-platform2"
+VOLTTRON_HOME=$V2_HOME ./make-platform2
+echo "START make-platform3"
+VOLTTRON_HOME=$V3_HOME ./make-platform3
+#echo "START make-hello1"
+#VOLTTRON_HOME=$V1_HOME ./make-hello1
+#echo "START make-hello2"
+#VOLTTRON_HOME=$V2_HOME ./make-hello2
 
-VOLTTRON_HOME=~/v1home volttron-ctl start --tag manageragent
-VOLTTRON_HOME=~/v1home volttron-ctl start --tag platformagent
-VOLTTRON_HOME=~/v2home volttron-ctl start --tag platformagent
+echo "starting platform manager"
+VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag manageragent
+#echo "starting platform agent 1"
+#VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag platformagent
+echo "starting platform agent 2"
+VOLTTRON_HOME=$V2_HOME volttron-ctl start --tag platformagent
+#echo "starting platform agent 3"
+#VOLTTRON_HOME=$V3_HOME volttron-ctl start --tag platformagent
 
-
+echo "Launching browser"
 x-www-browser http://localhost:8080
 
--- volttron/scripts/management-service-demo/start-platforms.sh
+++ volttron/scripts/management-service-demo/start-platforms.sh
@@ -1,3 +1,23 @@
-VOLTTRON_HOME=~/v1home volttron&
-VOLTTRON_HOME=~/v2home volttron&
+. demo-vars.sh
 
+./demo-setup.sh
+echo "START start-platforms"
+VOLTTRON_HOME=$V1_HOME volttron -vv&
+echo "Starting platform 2"
+VOLTTRON_HOME=$V2_HOME volttron -vv&
+echo "Starting platform 3"
+VOLTTRON_HOME=$V3_HOME volttron -vv&
+echo "START make-management"
+VOLTTRON_HOME=$V1_HOME ./make-management
+echo "START make-platform1"
+VOLTTRON_HOME=$V1_HOME ./make-platform1
+echo "START make-platform2"
+VOLTTRON_HOME=$V2_HOME ./make-platform2
+echo "START make-platform3"
+VOLTTRON_HOME=$V3_HOME ./make-platform3
+echo "START platform manager"
+VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag manageragent
+echo "START platform agent 2"
+VOLTTRON_HOME=$V2_HOME volttron-ctl start --tag platformagent
+echo "START platform agent 3"
+VOLTTRON_HOME=$V3_HOME volttron-ctl start --tag platformagent
