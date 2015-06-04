88a04bdd37548401ab246ff93d7771d5394e5f5c
craig8 <craig.allwardt@pnnl.gov>
2015-06-03 18:50:42 -0700

    Added platform historians to the demo scripts.

--- volttron/scripts/management-service-demo/run-demo.sh
+++ volttron/scripts/management-service-demo/run-demo.sh
@@ -8,6 +8,8 @@ VOLTTRON_HOME=$V1_HOME ./make-hello1
 echo "START make-hello2"
 VOLTTRON_HOME=$V2_HOME ./make-hello2
 
+./make-platform-historians
+
 echo "Launching browser"
 x-www-browser http://localhost:8080 &
 
