a396b2d3520d417197285fab239488a69dff17dc
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2015-06-09 11:06:50 -0700

    Modify periodic decorators to accept wait argument.
    Unfortunately, Python says a decorator like @Core.periodic(5).wait(3) is
    a syntax error. So now it will have to be like @Core.periodic(5,
    wait=3).

--- volttron/platform/vipagent/core.py
+++ volttron/platform/vipagent/core.py
@@ -89,23 +89,13 @@ class Periodic(object):   # pylint: disable=invalid-name
     period seconds while the agent is executing its run loop.
     '''
 
-    def __init__(self, period, *args, **kwargs):
+    def __init__(self, period, args=None, kwargs=None, wait=0):
         '''Store period (seconds) and arguments to call method with.'''
         assert period > 0
         self.period = period
-        self.args = args
-        self.kwargs = kwargs
-        self.timeout = 0
-
-    def wait(self, seconds=None):
-        '''Wait before calling for the first time.
-
-        Wait period seconds, if seconds is None. Returns the decorator
-        instance.
-        '''
-        assert seconds is None or seconds >= 0
-        self.timeout = seconds
-        return self
+        self.args = args or ()
+        self.kwargs = kwargs or {}
+        self.timeout = wait
 
     def __call__(self, method):
         '''Attach this object instance to the given method.'''
@@ -326,18 +316,14 @@ class Core(object):
 
     @dualmethod
     def periodic(self, period, func, args=None, kwargs=None, wait=0):
-        if args is None:
-            args = ()
-        if kwargs is None:
-            kwargs = {}
-        greenlet = Periodic(period, *args, **kwargs).wait(wait).get(func)
+        greenlet = Periodic(period, args, kwargs, wait).get(func)
         self.greenlet.link(lambda glt: greenlet.kill())
         greenlet.start()
         return greenlet
 
     @periodic.classmethod
-    def periodic(cls, period, *args, **kwargs):   # pylint: disable=no-self-argument
-        return Periodic(period, *args, **kwargs)
+    def periodic(cls, period, args=None, kwargs=None, wait=0):   # pylint: disable=no-self-argument
+        return Periodic(period, args, kwargs, wait)
 
     @classmethod
     def receiver(cls, signal):
