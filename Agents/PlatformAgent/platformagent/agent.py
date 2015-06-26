9b48e18519bf6b2ec3990a9aaad17f73dffd1ac8
Jereme Haack <Jereme.Haack@pnnl.gov>
2015-06-26 15:29:56 -0700

    Fixed manager call #111

--- Agents/PlatformAgent/platformagent/agent.py
+++ Agents/PlatformAgent/platformagent/agent.py
@@ -201,7 +201,7 @@ def platform_agent(config_path, **kwargs):
         #TODO: Make configurable
         @Core.periodic(30)
         def update_sibling_address_cache(self):
-            for manager in self.managers:
+            for manager in self._managers:
                 try:
                     print (manager[0],manager[1]) 
                     agent = Agent(address=manager[0])
