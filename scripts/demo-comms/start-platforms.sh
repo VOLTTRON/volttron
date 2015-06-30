4a2629f41a5962f02b7f8d4b1f5e95f4a5de68ec
Jereme Haack <Jereme.Haack@pnnl.gov>
2015-06-30 16:44:57 -0700

    fix path on script

--- scripts/demo-comms/start-platforms.sh
+++ scripts/demo-comms/start-platforms.sh
@@ -2,9 +2,9 @@
 . vars.sh
 
 echo "Starting platform 1: $V1_HOME"
-VOLTTRON_HOME=$V1_HOME volttron -vv -l $V1_HOME/volttron.log &
+VOLTTRON_HOME=$V1_HOME volttron -vv -l $V1_HOME/volttron.log --vip-address=tcp://127.0.0.2:8081&
 echo "Starting platform 2: $V2_HOME"
-VOLTTRON_HOME=$V2_HOME volttron -vv -l $V2_HOME/volttron.log &
+VOLTTRON_HOME=$V2_HOME volttron -vv -l $V2_HOME/volttron.log --vip-address=tcp://127.0.0.1:8081&
 
 echo "Making volttron central on $V1_HOME"
 VOLTTRON_HOME=$V1_HOME ../pack_install.sh ../../Agents/VolttronCentralAgent/ \
@@ -14,7 +14,7 @@ echo "START volttron central"
 VOLTTRON_HOME=$V1_HOME volttron-ctl start --tag volttroncentral
 
 echo "Make platform1 on $V1_HOME"
-VOLTTRON_HOME=$VOLTTRON_HOME ../pack_install.sh ../../Agents/PlatformAgent/ \
+VOLTTRON_HOME=$V1_HOME ../pack_install.sh ../../Agents/PlatformAgent/ \
 ./platform-config1 platformagent
 
 echo "Start make-platform1"
