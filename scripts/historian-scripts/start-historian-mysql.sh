54edeeac6d90cd2828fe9f183e3d6531c27dd4e7
C. Allwardt <craig.allwardt@pnnl.gov>
2015-07-31 15:02:35 -0700

    Added start_historian to the core and updated start-historian-mysql to use it

--- scripts/historian-scripts/start-historian-mysql.sh
+++ scripts/historian-scripts/start-historian-mysql.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env bash
 
-export VOLTTRON_HOME=$HOME/.volttron
-
-START="volttron-ctl start"
-
-if [ ! -e "./Agents" ]; then
+if [ ! -e "./applications" ]; then
     echo "Please execute from root of volttron repository."
     exit 0
 fi
 
+export HIST="services/core/SQLHistorian"
+export HIST_CONFIG="$HIST/config.mysql.platform.historian"
+SCRIPTS_CORE="./scripts/core"
 
-# This is the directory that contains the scripts
-HIST_TEST_DIR=`dirname $0`
-HIST="Agents/SQLHistorianAgent"
-HIST_TAG="hist"
-
-SCRIPTS="./scripts"
-$SCRIPTS/remove_agent.sh $HIST_TAG
-
-if [ "$1" = "full" ]
-then
-    $HIST_TEST_DIR/start-platform.sh
-fi
-
-SCRIPTS="./scripts"
-PACK="$SCRIPTS/pack_install.sh"
-
-echo $PACK $HIST $HIST/config.mysql.platform.historian $HIST_TAG
-# Install and start HIST.
-$PACK $HIST $HIST/config.mysql.platform.historian $HIST_TAG
-$START --tag $HIST_TAG
+$SCRIPTS_CORE/start_historian.sh $1
 
