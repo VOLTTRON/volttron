1156a01e48717d1f63160bca759b39decbb20fd5
Jereme Haack <Jereme.Haack@pnnl.gov>
2015-08-12 11:54:43 -0700

    Added some scripts to make it easy to set vip address

--- scripts/core/pack_install.sh
+++ scripts/core/pack_install.sh
@@ -32,18 +32,28 @@ if [ -z "$VOLTTRON_HOME" ]; then
   echo "VOLTTRON_HOME UNSET setting to $VOLTTRON_HOME";
 fi
 
+echo "VOLTTRON_HOME=$VOLTTRON_HOME"
+
+COMMAND_ARGS=""
+
+if [ ! -z "$VIP_ADDRESS" ]; then
+  COMMAND_ARGS=$COMMAND_ARGS --vip-addres $VIP_ADDRESS
+  echo "Using VIP_ADDRESS: $VIP_ADDRESS";
+fi
+
+
 # volttron-pkg package $1
 
-WHEEL=$(volttron-pkg package $1 | awk -F": " '{ print $2 }')
+WHEEL=$(volttron-pkg $COMMAND_ARGS package $1 | awk -F": " '{ print $2 }')
 
 if [ ! -e "$WHEEL" ]; then
   echo "$WHEEL doesn't exist"
   exit 0
 fi
 
-VOLTTRON_HOME=$VOLTTRON_HOME volttron-pkg configure "$WHEEL" "$2"
+VOLTTRON_HOME=$VOLTTRON_HOME volttron-pkg $COMMAND_ARGS configure "$WHEEL" "$2"
 
-VOLTTRON_HOME=$VOLTTRON_HOME volttron-ctl install "$3=$WHEEL"
+VOLTTRON_HOME=$VOLTTRON_HOME volttron-ctl $COMMAND_ARGS install "$3=$WHEEL"
 
 
 
