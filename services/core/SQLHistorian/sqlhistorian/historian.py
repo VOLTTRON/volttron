0a098fe878f004e8532cad2857ed59d32ca12628
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2015-08-14 15:41:30 -0700

    Allow vip_main() to send default identity.

--- services/core/SQLHistorian/sqlhistorian/historian.py
+++ services/core/SQLHistorian/sqlhistorian/historian.py
@@ -91,7 +91,7 @@ def historian(config_path, **kwargs):
     assert databaseType is not None
     params = connection.get('params', None)
     assert params is not None
-    identity = config.get('identity', None)
+    identity = config.get('identity', kwargs.get('identity'))
 
     if databaseType == 'sqlite':
         from .db.sqlitefuncts import SqlLiteFuncts as DbFuncts
@@ -198,7 +198,7 @@ def historian(config_path, **kwargs):
                 self.core.stop()
 
     SQLHistorian.__name__ = 'SQLHistorian'
-    return SQLHistorian(identity=identity, **kwargs)
+    return SQLHistorian(**kwargs)
 
 
 
