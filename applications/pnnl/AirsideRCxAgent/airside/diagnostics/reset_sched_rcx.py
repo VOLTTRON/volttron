3fb0dc26b8ba11a4b01f00e2c9da33e8dabb137b
lute362 <robert.lutes@pnnl.gov>
2015-09-09 16:37:16 -0700

    Update for AHU diagnostics.

---
+++ applications/pnnl/AirsideRCxAgent/airside/diagnostics/reset_sched_rcx.py
@@ -0,0 +1,261 @@
+'''
+Copyright (c) 2014, Battelle Memorial Institute
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+
+This material was prepared as an account of work sponsored by an
+agency of the United States Government.  Neither the United States
+Government nor the United States Department of Energy, nor Battelle,
+nor any of their employees, nor any jurisdiction or organization
+that has cooperated in the development of these materials, makes
+any warranty, express or implied, or assumes any legal liability
+or responsibility for the accuracy, completeness, or usefulness or
+any information, apparatus, product, software, or process disclosed,
+or represents that its use would not infringe privately owned rights.
+
+Reference herein to any specific commercial product, process, or
+service by trade name, trademark, manufacturer, or otherwise does
+not necessarily constitute or imply its endorsement, recommendation,
+r favoring by the United States Government or any agency thereof,
+or Battelle Memorial Institute. The views and opinions of authors
+expressed herein do not necessarily state or reflect those of the
+United States Government or any agency thereof.
+
+PACIFIC NORTHWEST NATIONAL LABORATORY
+operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+under Contract DE-AC05-76RL01830
+'''
+import numpy as np
+import datetime
+import re
+import logging
+
+SCHED_VALIDATE = 'Schedule-Reset ACCx'
+DUCT_STC_RCX3 = 'No Static Pressure Reset Dx'
+SA_TEMP_RCX3 = 'No Supply-air Temperature Reset Dx'
+SCHED_RCX = 'Operational Schedule Dx'
+DX = '/diagnostic message'
+DATA = '/data/'
+ST = 'state'
+
+class SchedResetRcx(object):
+    '''Schedule, supply-air temperature, and duct static pressure auto-detect
+    diagnostics for AHUs or RTUs.
+    '''
+    def __init__(self, unocc_time_threshold, unocc_stp_threshold,
+                 monday_sch, tuesday_sch, wednesday_sch, thursday_sch,
+                 friday_sch, saturday_sch, sunday_sch, data_window,
+                 no_required_data, stpr_reset_threshold, sat_reset_threshold,
+                 analysis):
+
+        self.act_sch = []
+        self.fanstat_values = []
+        self.schedule = {}
+        self.stcpr_arr = []
+        self.stcpr_stpt_arr = []
+        self.sat_stpt_arr = []
+        self.timestamp = []
+        self.sched_time = []
+        self.dx_time = None
+
+        def date_parse(date):
+            date = re.sub('[:;]', ',', date)
+            return [int(item) for item in (x.strip() for x in date.split(','))]
+
+        self.analysis = analysis
+        self.monday_sch = date_parse(monday_sch)
+        self.tuesday_sch = date_parse(tuesday_sch)
+        self.wednesday_sch = date_parse(wednesday_sch)
+        self.thursday_sch = date_parse(thursday_sch)
+        self.friday_sch = date_parse(friday_sch)
+        self.saturday_sch = date_parse(saturday_sch)
+        self.sunday_sch = date_parse(sunday_sch)
+
+        self.schedule = {0: self.monday_sch, 1: self.tuesday_sch,
+                         2: self.wednesday_sch, 3: self.thursday_sch,
+                         4: self.friday_sch, 5: self.saturday_sch,
+                         6: self.sunday_sch}
+        self.pre_msg = ('Current time is in the scheduled hours '
+                        'unit is operating correctly.')
+
+        # Application thresholds (Configurable)
+        self.data_window = float(data_window)
+        self.no_required_data = no_required_data
+        self.unocc_time_threshold = float(unocc_time_threshold)
+        self.unocc_stp_threshold = float(unocc_stp_threshold)
+        self.stpr_reset_threshold = float(stpr_reset_threshold)
+        self.sat_reset_threshold = float(sat_reset_threshold)
+
+    def sched_rcx_alg(self, cur_time, stcpr_data, stcpr_sp_data,
+                      sat_stpt_data, fan_stat_data, dx_result,
+                      sched_val):
+        '''Check schedule status and unit operational status.'''
+        def clear_old():
+            '''Clear old data'''
+            self.sat_stpt_arr = []
+            self.stcpr_arr = []
+            self.stcpr_stpt_arr = []
+            self.fanstat_values = []
+            self.sched_time = []
+            self.dx_time = None
+
+            if stcpr_sp_val is not None:
+                self.sat_stpt_arr.append(sat_stpt_val)
+                self.stcpr_stpt_arr.append(stcpr_sp_val)
+            if fan_stat is not None:
+                self.fanstat_values.append(fan_stat)
+                self.stcpr_arr.extend(duct_stp)
+            self.timestamp = [self.timestamp[-1]]
+
+        fan_stat = None
+        stcpr_sp_val = None
+        act_sch = self.schedule[cur_time.weekday()]
+        run = False
+        if self.timestamp and self.timestamp[-1].date() != cur_time.date():
+            self.dx_time = self.timestamp[-1].date()
+            run = True
+        if((cur_time.hour < act_sch[0] or
+           (cur_time.hour == act_sch[0] and cur_time.minute < act_sch[1])) or
+           (cur_time.hour > act_sch[2] or
+           (cur_time.hour == act_sch[2] and cur_time.minute < act_sch[3]))):
+            if not run:
+                self.stcpr_arr.extend(stcpr_data)
+                self.fanstat_values.append(int(max(fan_stat_data)))
+                self.sched_time.append(cur_time)
+            fan_stat = int(max(fan_stat_data))
+            duct_stp = stcpr_data
+        else:
+            if int(max(fan_stat_data)):
+                if not run:
+                    self.stcpr_stpt_arr.append(np.mean(stcpr_sp_data))
+                    self.sat_stpt_arr.append(np.mean(sat_stpt_data))
+                stcpr_sp_val = np.mean(stcpr_sp_data)
+                sat_stpt_val = np.mean(sat_stpt_data)
+        self.timestamp.append(cur_time)
+
+        data = {}
+        for key, value in sched_val.items():
+            tag = SCHED_VALIDATE + DATA + key
+            data.update({tag: value})
+
+        if run and len(self.sched_time) >= self.no_req_data and len(self.sat_stpt_arr) >= self.no_req_data:
+            dx_result = self.unocc_fan_operation(dx_result)
+            dx_result = self.no_static_pr_reset(dx_result)
+            dx_result = self.no_sat_sp_reset(dx_result)
+            data.update({SCHED_VALIDATE + DATA + ST: 1})
+            dx_result.insert_table_row(self.analysis, data)
+            clear_old()
+        elif run:
+            dx_msg = 61.2
+            dx_table = {
+                SCHED_RCX + DX:  dx_msg
+            }
+            dx_result.insert_table_row(self.analysis, dx_table)
+            data.update({SCHED_VALIDATE + DATA + ST: 2})
+            dx_result.insert_table_row(self.analysis, data)
+            clear_old()
+        else:
+            data.update({SCHED_VALIDATE + DATA + ST: 0})
+            dx_result.insert_table_row(self.analysis, data)
+        return dx_result
+
+    def unocc_fan_operation(self, result):
+        '''If the AHU/RTU is operating during unoccupied periods inform the
+        building operator.
+        '''
+        fanstat_on = [i for i in self.fanstat_values if int(i) == 1]
+        if self.fanstat_values:
+            percent_on = (len(fanstat_on)/len(self.fanstat_values)) * 100.0
+        else:
+            percent_on = 0
+        if self.stcpr_arr:
+            avg_duct_stpr = np.mean(self.stcpr_arr)
+        else:
+            avg_duct_stpr = 0
+        if percent_on > self.unocc_time_threshold:
+            msg = 'Supply fan is on during unoccupied times.'
+            dx_msg = 63.1
+        else:
+            if avg_duct_stpr < self.unocc_stp_threshold:
+                msg = 'No problems detected.'
+                dx_msg = 60.0
+            else:
+                msg = ('Fan status show the fan is off but the duct static '
+                       'pressure is high, check the functionality of the '
+                       'pressure sensor.')
+                dx_msg = 64.2
+        for item in self.sched_time:
+            dx_table = {
+                SCHED_RCX + DX:  dx_msg
+            }
+            result.insert_table_row(self.analysis, dx_table)
+        result.log(msg, logging.INFO)
+        return result
+
+    def no_static_pr_reset(self, result):
+        '''Auto-RCx  to detect whether a static pressure set point
+
+        reset is implemented.
+        '''
+        if not self.stcpr_stpt_arr:
+            return result
+        stp_diff = (max(self.stcpr_stpt_arr) - min(self.stcpr_stpt_arr))
+
+        if stp_diff < self.stpr_reset_threshold:
+            msg = ('No duct static pressure reset detected. A duct static '
+                   'pressure set point reset can save significant energy.')
+            dx_msg = 71.1
+        else:
+            msg = 'No problem detected.'
+            dx_msg = 70.0
+        dx_table = {
+            DUCT_STC_RCX3 + DX:  dx_msg
+        }
+        result.insert_table_row(self.analysis, dx_table)
+        result.log(msg, logging.INFO)
+        return result
+
+    def no_sat_sp_reset(self, result):
+        '''Auto-RCx  to detect whether a supply-air temperature set point
+
+        reset is implemented.
+        '''
+        if not self.sat_stpt_arr:
+            return result
+        satemp_diff = max(self.sat_stpt_arr) - min(self.sat_stpt_arr)
+        if satemp_diff <= self.sat_reset_threshold:
+            msg = ('A supply-air temperature reset was not detected. '
+                   'This can result in excess energy consumption.')
+            dx_msg = 81.1
+        else:
+            msg = 'No problems detected for this diagnostic.'
+            dx_msg = 80.0
+        dx_table = {
+            SA_TEMP_RCX3 + DX:  dx_msg
+        }
+        result.insert_table_row(self.analysis, dx_table)
+        result.log(msg, logging.INFO)
+        return result
---
+++ applications/pnnl/AirsideRCxAgent/airside/diagnostics/satemp_rcx.py
@@ -0,0 +1,289 @@
+'''
+Copyright (c) 2014, Battelle Memorial Institute
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+
+This material was prepared as an account of work sponsored by an
+agency of the United States Government.  Neither the United States
+Government nor the United States Department of Energy, nor Battelle,
+nor any of their employees, nor any jurisdiction or organization
+that has cooperated in the development of these materials, makes
+any warranty, express or implied, or assumes any legal liability
+or responsibility for the accuracy, completeness, or usefulness or
+any information, apparatus, product, software, or process disclosed,
+or represents that its use would not infringe privately owned rights.
+
+Reference herein to any specific commercial product, process, or
+service by trade name, trademark, manufacturer, or otherwise does
+not necessarily constitute or imply its endorsement, recommendation,
+r favoring by the United States Government or any agency thereof,
+or Battelle Memorial Institute. The views and opinions of authors
+expressed herein do not necessarily state or reflect those of the
+United States Government or any agency thereof.
+
+PACIFIC NORTHWEST NATIONAL LABORATORY
+operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+under Contract DE-AC05-76RL01830
+'''
+import numpy as np
+import datetime
+import logging
+
+SA_VALIDATE = 'Supply-air Temperature ACCx'
+SA_TEMP_RCX = 'Supply-air Temperature Control Loop Dx'
+SA_TEMP_RCX1 = 'Low Supply-air Temperature Dx'
+SA_TEMP_RCX2 = 'High Supply-air Temperature Dx'
+DX = '/diagnostic message'
+ST = 'state'
+CORRECT_SAT = 'Suggested SAT setpoint'
+DATA = '/data/'
+
+
+class SupplyTempRcx(object):
+    '''Air-side HVAC Self-Correcting Diagnostic: Detect and correct
+    supply-air temperature problems.
+    '''
+    def __init__(self, data_window, no_req_data, auto_correctflag,
+                 stpt_allowable_dev, rht_on_thr, high_dmpr_thr, pc_dmpr_thr,
+                 pc_rht_thr, min_sat_stpt, sat_retuning, rht_valve_thr,
+                 max_sat_stpt, analysis, sat_stpt_cname):
+        self.ts = []
+        self.sat_stpt_arr = []
+        self.satemp_arr = []
+        self.rht_arr = []
+        self.rht = []
+        self.percent_rht = []
+        self.percent_dmpr = []
+
+        # Common RCx parameters
+        self.analysis = analysis
+        self.sat_stpt_cname = sat_stpt_cname
+        self.data_window = float(data_window)
+        self.no_req_data = no_req_data
+        self.auto_correctflag = bool(auto_correctflag)
+        self.stpt_allowable_dev = float(stpt_allowable_dev)
+        self.rht_on_thr = float(rht_on_thr)
+        self.pc_rht_thr = float(pc_rht_thr)
+        self.dgr_sym = u'\N{DEGREE SIGN}'
+
+        # Low SAT RCx thresholds
+        self.rht_valve_thr = float(rht_valve_thr)
+        self.max_sat_stpt = float(max_sat_stpt)
+
+        # High SAT RCx thresholds
+        self.high_dmpr_thr = float(high_dmpr_thr)
+        self.pc_dmpr_thr = float(pc_dmpr_thr)
+        self.min_sat_stpt = float(min_sat_stpt)
+        self.sat_retuning = float(sat_retuning)
+
+    def sat_rcx(self, current_time, sat_data, sat_stpt_data,
+                rht_data, zn_dmpr, dx_result,
+                sat_override_check, validate):
+        '''Check supply-air temperature RCx pre-requisites
+
+        and assemble the supply-air temperature analysis data set.
+        '''
+        self.satemp_arr.append(np.mean(sat_data))
+        self.rht_arr.append(np.mean(rht_data))
+        self.sat_stpt_arr.append(np.mean(sat_stpt_data))
+        tot_rht = sum(1 if val > self.rht_on_thr else 0 for val in rht_data)
+        count_rht = len(rht_data)
+        tot_dmpr = sum(1 if val > self.high_dmpr_thr else 0 for val in zn_dmpr)
+        count_damper = len(zn_dmpr)
+
+        self.percent_rht.append(tot_rht/count_rht)
+        self.percent_dmpr.append(tot_dmpr/count_damper)
+        self.ts.append(current_time)
+        e_time = self.ts[-1] - self.ts[0]
+        e_time = e_time.total_seconds()/60
+        e_time = e_time if e_time > 0.0 else 1.0
+
+        data = {}
+        for key, value in validate.items():
+            tag = SA_VALIDATE + DATA + key
+            data.update({tag: value})
+
+        if e_time >= self.data_window and len(self.ts) >= self.no_req_data:
+            avg_sat_stpt = np.mean(self.sat_stpt_arr)
+            zipper = (self.sat_stpt_arr, self.satemp_arr)
+            stpt_tracking = [abs(x - y) for x, y in zip(*zipper)]
+            stpt_tracking = np.mean(stpt_tracking)/avg_sat_stpt*100
+
+            if stpt_tracking > self.stpt_allowable_dev:
+                msg = ('Supply-air temperature is deviating significantly '
+                       'from the supply-air temperature set point.')
+                dx_msg = 31.1
+                dx_table = {
+                    SA_TEMP_RCX + DX: dx_msg
+                }
+            else:
+                msg = 'No problem detected.'
+                dx_msg = 30.0
+                dx_table = {
+                    SA_TEMP_RCX + DX: dx_msg
+                }
+            dx_result.insert_table_row(self.analysis, dx_table)
+            dx_result.log(msg, logging.INFO)
+            if e_time > 75:
+                dx_result.insert_table_row(self.analysis,
+                                           {SA_TEMP_RCX1 + DX: 46.2})
+                dx_result.insert_table_row(self.analysis,
+                                           {SA_TEMP_RCX2 + DX: 56.2})
+                data.update({SA_VALIDATE + DATA + ST: 2})
+                dx_result.insert_table_row(self.analysis, data)
+                return dx_result
+            dx_result = self.low_sat(dx_result, avg_sat_stpt,
+                                     sat_override_check)
+            dx_result = self.high_sat(dx_result, avg_sat_stpt,
+                                      sat_override_check)
+            self.percent_rht = []
+            self.percent_dmpr = []
+            self.rht_arr = []
+            data.update({SA_VALIDATE + DATA + ST: 1})
+            dx_result.insert_table_row(self.analysis, data)
+            return dx_result
+        data.update({SA_VALIDATE + DATA + ST: 0})
+        dx_result.insert_table_row(self.analysis, data)
+        return dx_result
+
+    def low_sat(self, result, avg_sat_stpt, sat_override_check):
+        '''Diagnostic to identify and correct low supply-air temperature
+        (correction by modifying SAT set point)
+        '''
+        avg_zones_rht = np.mean(self.percent_rht)*100
+        rht_avg = np.mean(self.rht_arr)
+        if (rht_avg > self.rht_valve_thr and
+                avg_zones_rht > self.pc_rht_thr):
+            if avg_sat_stpt is not None and not sat_override_check:
+                if self.auto_correctflag:
+                    if avg_sat_stpt <= self.max_sat_stpt:
+                        sat_stpt = avg_sat_stpt + self.sat_retuning
+                        result.command(self.sat_stpt_cname, sat_stpt)
+                        sat_stpt = '%s' % float('%.2g' % sat_stpt)
+                        msg = ('The SAT has been detected to be too low. '
+                               'The SAT set point has been increased to: '
+                               '{}{drg}F'.format(self.dgr_sym, sat_stpt))
+                        dx_msg = 41.1
+                    else:
+                        result.command(self.sat_stpt_cname,
+                                       self.max_sat_stpt)
+                        sat_stpt = '%s' % float('%.2g' % self.max_sat_stpt)
+                        sat_stpt = str(sat_stpt)
+                        msg = ('The SAT was detected to be too low. '
+                               'Auto-correction has increased the SAT set '
+                               'point to the maximum configured SAT set '
+                               'point: {}{}F)'.format(self.dgr_sym, sat_stpt))
+                        dx_msg = 42.1
+                else:
+                    # Create diagnostic message for fault
+                    # condition without auto-correction
+                    msg = ('The SAT has been detected to be too low but '
+                           'auto-correction is not enabled.')
+                    dx_msg = 43.1
+            elif not sat_override_check:
+                msg = 'The SAT has been detected to be too low.'
+                dx_msg = 44.1
+            else:
+                msg = ('The SAT has been detected to be too low but '
+                       'auto-correction cannot be performed because the SAT '
+                       'set-point is in an override state.')
+                dx_msg = 45.1
+        else:
+            msg = 'No problem detected'
+            dx_msg = 40.0
+        dx_table = {
+            SA_TEMP_RCX1 + DX: dx_msg
+        }
+        result.insert_table_row(self.analysis, dx_table)
+        result.log(msg, logging.INFO)
+        return result
+
+    def high_sat(self, result, avg_sat_stpt, sat_override_check):
+        '''Diagnostic to identify and correct high supply-air temperature
+
+        (correction by modifying SAT set point)
+        '''
+        avg_zones_rht = np.mean(self.percent_rht)*100
+        avg_zn_dmpr = np.mean(self.percent_dmpr)*100
+
+        if avg_zn_dmpr > self.pc_dmpr_thr and avg_zones_rht < self.pc_rht_thr:
+            if avg_sat_stpt is not None and not sat_override_check:
+                if self.auto_correctflag:
+                    sat_stpt = avg_sat_stpt - self.sat_retuning
+                    # Create diagnostic message for fault condition
+                    # with auto-correction
+                    if sat_stpt >= self.min_sat_stpt:
+                        result.command(self.sat_stpt_cname, sat_stpt)
+                        sat_stpt = '%s' % float('%.2g' % sat_stpt)
+                        msg = ('The SAT has been detected to be too high. The '
+                               'SAT set point has been increased to: '
+                               '{}{}F'.format(self.dgr_sym, sat_stpt))
+                        dx_msg = 51.1
+                    else:
+                        # Create diagnostic message for fault condition
+                        # where the maximum SAT has been reached
+                        result.command(
+                            self.sat_stpt_cname, self.min_sat_stpt)
+                        sat_stpt = '%s' % float('%.2g' % self.min_sat_stpt)
+                        msg = ('The SAT was detected to be too high, '
+                               'auto-correction has increased the SAT to the '
+                               'minimum configured SAT: {}{}F'
+                               .format(self.dgr_sym, sat_stpt))
+                        dx_msg = 52.1
+                else:
+                    # Create diagnostic message for fault condition
+                    # without auto-correction
+                    msg = ('The SAT has been detected to be too high but '
+                           'auto-correction is not enabled.')
+                    dx_msg = 53.1
+            elif not sat_override_check:
+                msg = 'The SAT has been detected to be too high.'
+                dx_msg = 54.1
+            else:
+                msg = ('The SAT has been detected to be too high but '
+                       'auto-correction cannot be performed because the '
+                       'SAT set point is in an override state.')
+                dx_msg = 55.1
+        else:
+            msg = 'No problem detected.'
+            dx_msg = 50.0
+        dx_table = {
+            SA_TEMP_RCX2 + DX: dx_msg
+        }
+        result.insert_table_row(self.analysis, dx_table)
+        result.log(msg, logging.INFO)
+        self.sat_stpt_arr = []
+        self.satemp_arr = []
+        self.ts = []
+#         temp1 = []
+#         temp2 = []
+#         for message in range(0, len(Application.pre_requiste_messages) - 1):
+#             if SCHED_RCx in Application.pre_requiste_messages[message]:
+#                 temp1.append(Application.pre_requiste_messages[message])
+#                 temp2.append(Application.pre_msg_time[message])
+#         Application.pre_requiste_messages = temp1
+#         Application.pre_msg_time = temp2
+        return result
