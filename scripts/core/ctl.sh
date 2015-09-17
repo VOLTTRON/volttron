2af0038bcee9a43321ca1634a038db26d4bceeb9
Jereme Haack <Jereme.Haack@pnnl.gov>
2015-09-16 17:24:35 -0700

    Change script to use ipc

--- scripts/core/ctl.sh
+++ scripts/core/ctl.sh
@@ -1,22 +1,25 @@
-#Script for more easily dealing with a secured platform
-#Replace values for the variables with those appropriate for your deployment
-#And then use this to call volttron-ctl commands
-
-#VOLTTRON_HOME will be ~/.volttron unless you have specifically changed it. Use the modifed value if you have 
-#VIP_SOCKET is what your platform is setup to use for vip-address in its config file
-#Curve Key is the public key output during platform start, for instance use the portion in parens:
-#2015-08-17 10:26:22,387 () volttron.platform.main INFO: public key: '+=c#-/bCg6DsR1wcxN0^[^9H-v>/5.R.iC==M}ND' (zKmY2tcbDbB6ZHElJpDcBfIIzctjMR81py5-7c8k5E4)
-#Public and Secret keys are generated using volttron-ctl keypair. The public key should have credentials in auth.json
-
+# This script provides a shortcut for issuing VOLTTRON commands when you have 
+# setup an externally facing vip address, encryption, and authorization
 
 #Modify these values for your deployment
-export VOLTTRON_HOME=~/.volttron
-VIP_SOCKET=tcp://127.0.0.1:9999
-SERVER_KEY=<Server public key>
-PUBLIC_KEY=<Public portion of keypair>
-SECRET_KEY=<Secret portion of keypair>
 
-export VIP_ADDRESS="$VIP_SOCKET?serverkey=$SERVER_KEY&publickey=$PUBLIC_KEY&secretkey=$SECRET_KEY"
+if [ -z "$VOLTTRON_HOME" ]; then
+  VOLTTRON_HOME=$HOME/.volttron
+  echo "VOLTTRON_HOME UNSET setting to $VOLTTRON_HOME";
+fi
+
+echo "VOLTTRON_HOME=$VOLTTRON_HOME"
+
+if [ -z "$VIP_ADDRESS" ]; then
+  VIP_ADDRESS="ipc://@$VOLTTRON_HOME/run/vip.socket"
+  COMMAND_ARGS="$COMMAND_ARGS --vip-address $VIP_ADDRESS"
+  echo "Using VIP_ADDRESS: $VIP_ADDRESS";
+
+fi
+
+
+
+export VIP_ADDRESS="$VIP_ADDRESS"
 
 volttron-ctl $1 --vip-address $VIP_ADDRESS
 
