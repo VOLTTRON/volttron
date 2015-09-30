f3f7243e82fa3b86208b314be2391230a62daf31
C. Allwardt <craig.allwardt@pnnl.gov>
2015-09-29 17:39:37 -0700

    It is now that the analysis and devices should publish the same format

--- volttron/platform/agent/base_historian.py
+++ volttron/platform/agent/base_historian.py
@@ -243,42 +243,22 @@ class BaseHistorianAgent(Agent):
         self.capture_data(peer, sender, bus, topic, headers, message, device)
         
     def capture_analysis_data(self, peer, sender, bus, topic, headers, message):
-        '''Capture device data and submit it to be published by a historian.
+        '''Capture analaysis data and submit it to be published by a historian.
         
-        Filter out only the */all topics for publishing to the historian.
+        Filter out all but the all topics
         '''
         
         if topic.endswith("/all") or '/all/' in topic:
-#             _log.debug("Unmatched topic: {}".format(topic))
             return
-        
-        # Because of the above if we know that all is in the topic so
-        # we strip it off to get the base device
-#         parts = topic.split('/')
+                
         parts = topic.split('/')
-        device = '/'.join(parts[1:-1]) #'/'.join(reversed(parts[2:]))
-        
-        _log.debug("found topic {}".format(topic))
-        
-        if topic.endswith('Timestamp'):
-            pass
-        
-        try:
-            value = float(message[0])
-        except:
-            value = message[0]
-        # Because message is a single point we need to have the point in the
-        # message
-        real_message = {parts[-1]: value}
-        real_message = [jsonapi.dumps(real_message), jsonapi.dumps({})]
-        
-        self.capture_data(peer, sender, bus, topic, headers, real_message, device)
+        # strip off the first part of the topic.
+        device = '/'.join(parts[1:-1])
+                
+        self.capture_data(peer, sender, bus, topic, headers, message, device)
         
     def capture_data(self, peer, sender, bus, topic, headers, message, device):
         
-        if topic.startswith('analysis'):
-            pass
-        #peer, sender, bus, topic, headers, message
         timestamp_string = headers.get(headers_mod.DATE)
         timestamp, my_tz = process_timestamp(timestamp_string)
         
@@ -286,12 +266,13 @@ class BaseHistorianAgent(Agent):
             # 2.0 agents compatability layer makes sender == pubsub.compat so 
             # we can do the proper thing when it is here
             if sender == 'pubsub.compat':
-                values = jsonapi.loads(message[0])
+                message = jsonapi.loads(message[0])
+                
+            if isinstance(message, dict):
+                values = message
             else:
-                if isinstance(message, dict):
-                    values = message
-                else:
-                    values = message[0]
+                values = message[0]
+                
         except ValueError as e:
             _log.error("message for {topic} bad message string: {message_string}".format(topic=topic,
                                                                                      message_string=message[0]))
@@ -308,10 +289,12 @@ class BaseHistorianAgent(Agent):
             # 2.0 agents compatability layer makes sender == pubsub.compat so 
             # we can do the proper thing when it is here
             if sender == 'pubsub.compat':
-                meta = jsonapi.loads(message[1])
-            else:
-                if not isinstance(message, dict):
-                    meta = message[1]
+                if isinstance(message[1], str):
+                    meta = jsonapi.loads(message[1])
+            
+            if not isinstance(message, dict):
+                meta = message[1]
+                
         except ValueError as e:
             _log.warning("meta data for {topic} bad message string: {message_string}".format(topic=topic,
                                                                                      message_string=message[0]))
@@ -324,7 +307,7 @@ class BaseHistorianAgent(Agent):
             source = 'scrape'
         _log.debug("Queuing {topic} from {source} for publish".format(topic=topic,
                                                                       source=source))
-
+        
         for key, value in values.iteritems():
             point_topic = device + '/' + key
             self._event_queue.put({'source': source,
