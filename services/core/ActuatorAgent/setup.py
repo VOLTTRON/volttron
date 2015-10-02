bcd68ba982c0c923e88c6460cd7dc85c2bbf42df
Robert Lutes <robert.lutes@pnnl.gov>
2015-10-02 16:08:13 -0700

    Revert agent versions to 0.1.

--- services/core/ActuatorAgent/setup.py
+++ services/core/ActuatorAgent/setup.py
@@ -63,7 +63,7 @@ package = packages[0]
 
 setup(
     name = package + 'agent',
-    version = "3.0",
+    version = "0.1",
     install_requires = ['volttron'],
     packages = packages,
     entry_points = {
