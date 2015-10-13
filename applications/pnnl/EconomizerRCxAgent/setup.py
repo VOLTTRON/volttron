db98328329e8626291e275b4963833a9ce0d6161
C. Allwardt <craig.allwardt@pnnl.gov>
2015-10-13 09:04:31 -0700

    Bumped economizer version

--- applications/pnnl/EconomizerRCxAgent/setup.py
+++ applications/pnnl/EconomizerRCxAgent/setup.py
@@ -63,7 +63,7 @@ package = packages[0]
 
 setup(
     name = package + 'agent',
-    version = "0.1",
+    version = '3.1',
     install_requires = ['volttron'],
     packages = packages,
     entry_points = {
