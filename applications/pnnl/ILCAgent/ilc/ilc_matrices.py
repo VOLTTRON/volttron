56be386291e0e0cbbd6fe53d4c034a3a49a0c2b8
Robert Lutes <robert.lutes@pnnl.gov>
2015-11-11 12:56:01 -0800

    add matrices file for ILC

---
+++ applications/pnnl/ILCAgent/ilc/ilc_matrices.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+'''
+Copyright (c) 2015, Battelle Memorial Institute
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+
+This material was prepared as an account of work sponsored by an
+agency of the United States Government.  Neither the United States
+Government nor the United States Department of Energy, nor Battelle,
+nor any of their employees, nor any jurisdiction or organization
+that has cooperated in the development of these materials, makes
+any warranty, express or implied, or assumes any legal liability
+or responsibility for the accuracy, completeness, or usefulness or
+any information, apparatus, product, software, or process disclosed
+or represents that its use would not infringe privately owned rights.
+
+Reference herein to any specific commercial product, process, or
+service by trade name, trademark, manufacturer, or otherwise does
+not necessarily constitute or imply its endorsement, recommendation,
+r favoring by the United States Government or any agency thereof,
+or Battelle Memorial Institute. The views and opinions of authors
+expressed herein do not necessarily state or reflect those of the
+United States Government or any agency thereof.
+
+PACIFIC NORTHWEST NATIONAL LABORATORY
+operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+under Contract DE-AC05-76RL01830
+'''
+import os
+import sys
+import operator
+import logging
+import math
+from xlrd import open_workbook
+import numpy as np
+from volttron.platform.agent import utils
+MATRIX_ROWSTRING = "%20s\t%12.2f%12.2f%12.2f%12.2f%12.2f"
+CRITERIA_LABELSTRING = "\t\t\t%12s%12s%12s%12s%12s"
+DEVICE_ROWSTRING = "%20s%15.2f%12.2f%12.2f%12.2f%12.2f%12.2f%12.2f"
+DEVICE_LABELSTRING = "\t\t\t%12s%12s%12s%12s%12s%12s%12s"
+LABELS = ['NO_curtailing', 'deltaT_zone_delta', 'Room_rtu',
+          'Power_rtu', 'deltaT_zone_sp']
+
+utils.setup_logging()
+_log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.debug,
+                    format='%(asctime)s   %(levelname)-8s %(message)s',
+                    datefmt='%m-%d-%y %H:%M:%S')
+
+
+def open_file():
+    '''Create file input window for user.'''
+    from Tkinter import Tk
+    from tkFileDialog import askopenfilename
+    file_path = None
+    supported_files = ['xlsx', 'xls']
+    tk = Tk()
+    tk.withdraw()
+    file_path = \
+        askopenfilename(defaultextension='.xlsx',
+                        title='choose excel file for AHP',
+                        initialfile='',
+                        parent=tk,
+                        initialdir=os.path.realpath(__file__))
+    _, filextension = os.path.splitext(file_path)
+    if ([ext for ext in supported_files if ext == filextension] and
+            filextension != ''):
+        file_path = ('File Selected is not a supported '
+                     'type (xlsx or xls)')
+        return file_path
+    if (file_path != '' and os.path.exists(file_path) and
+            os.path.isfile(file_path)):
+        file_path = file_path
+    tk.destroy()
+    return file_path
+
+
+def extract_criteria_matrix(excel_file, sheet):
+    '''Function to extract the criteria matrix from
+
+    the CriteriaMatrix sheet of the excel spreadsheet
+    '''
+    # Open the excel file
+    wb = open_workbook(excel_file)
+
+    # Access the "CriteriaMatrix" sheet
+    sheet = wb.sheet_by_name(sheet)
+
+    criteria_labels = []
+    criteria_matrix = []
+    # Matrix starts at row 3 (column headers, which are
+    # duplicated by column A) and runs until "Column Sum"
+    for row in range(3, sheet.nrows):
+        if(sheet.cell(row, 0).value == ""):
+            break
+
+        criteria_labels.append(sheet.cell(row, 0).value)
+    criteria_labels.pop()
+    # NOTE: the number of rows and number of columns should match
+    # Iterate over the rows and columns of the spreadsheet loading
+    # the numbers as floating point values into a list of lists.
+
+    for row in range(3, 3 + len(criteria_labels)):
+        temp_row = []
+        for col in range(1, 1 + len(criteria_labels)):
+            temp_row.append(float(sheet.cell(row, col).value))
+        criteria_matrix.append(temp_row)
+
+    return criteria_labels, criteria_matrix
+
+
+def calc_column_sums(criteria_matrix):
+    '''Calculate the column sums for the criteria matrix.'''
+    j = 0
+    csum = []
+    while j < len(criteria_matrix[0]):
+        col = [float(row[j]) for row in criteria_matrix]
+        csum.append(sum(col))
+        j += 1
+    return csum
+
+
+def normalize_matrix(criteria_matrix, col_sums):
+    '''Normalizes the members of criteria matrix using the vector
+
+    col_sums. Returns the normalized matrix, and the sums of each
+    row of the matrix.
+    '''
+    normalized_matrix = []
+    rowsums = []
+    i = 0
+    while i < len(criteria_matrix):
+        j = 0
+        norm_row = []
+        while j < len(criteria_matrix[0]):
+            norm_row.append(
+                criteria_matrix[i][j]/(col_sums[j] if col_sums[j] != 0
+                                       else 1))
+            j += 1
+        rowsum = sum(norm_row)
+        # norm_row.append(rowsum/j)
+        rowsums.append(rowsum/j)
+        normalized_matrix.append(norm_row)
+        i += 1
+    # display_matrix([rowsums,], CRITERIA_LABELSTRING, criteria_LABELS,
+    # ['Priority vector'], MATRIX_ROWSTRING)
+    return normalized_matrix, rowsums
+
+
+# Validates the criteria matrix to ensure that the inputs are internally consistent
+# Returns a True if the matrix is valid, and False if it is not.
+def validate_input(pairwise_matrix, col_sums, display=False,
+                   criteria_LABELS="", CRITERIA_LABELSTRING="",
+                   MATRIX_ROWSTRING="", display_dest=sys.stdout):
+    '''Validates the criteria matrix to ensure that the inputs are
+
+    internally consistent. Returns a True if the matrix is valid,
+    and False if it is not.
+    '''
+    # Calculate row products and take the 5th root
+    _log.info("Validating matrix")
+    random_index = [0, 0, 0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49]
+    roots = []
+    for row in pairwise_matrix:
+        roots.append(math.pow(reduce(operator.mul, row, 1), 1.0/5))
+    display_matrix([roots, ], CRITERIA_LABELSTRING,
+                   criteria_LABELS, ['5th root of product'],
+                   MATRIX_ROWSTRING, display_dest) \
+        if display else ""
+
+    # Sum the vector of products
+    root_sum = sum(roots)
+    # Calculate the priority vector
+    priority_vec = []
+    for item in roots:
+        priority_vec.append(item / root_sum)
+
+    display_matrix([priority_vec, ], CRITERIA_LABELSTRING,
+                   criteria_LABELS, ['Priority Vector'],
+                   MATRIX_ROWSTRING, display_dest) \
+        if display else ""
+
+    # Calculate the priority row
+    priority_row = []
+    for i in range(0, len(col_sums)):
+        priority_row.append(col_sums[i] * priority_vec[i])
+
+    display_matrix([priority_row, ], CRITERIA_LABELSTRING,
+                   criteria_LABELS, ['Priority Row'],
+                   MATRIX_ROWSTRING, display_dest) \
+        if display else ""
+
+    # Sum the priority row
+    priority_row_sum = sum(priority_row)
+    if display:
+        print >> display_dest, "Priority row sum: ", priority_row_sum
+
+    # Calculate the consistency index
+    consistency_index = \
+        (priority_row_sum - len(col_sums))/(len(col_sums) - 1)
+
+    if display:
+        print >> display_dest, "Consistency Index:", consistency_index
+
+    # Calculate the consistency ratio
+    consistency_ratio = consistency_index / random_index[len(col_sums)]
+
+    if display:
+        print >> display_dest, "Consistency Ratio:", consistency_ratio
+    return consistency_ratio < 0.2
+
+
+def input_rtu(zn_temp, zn_tempsp, prev_temp, rtu_pwr,
+              zn_type, stg_val, rtu_labels):
+    i = 0
+    csum = []
+    data = []
+    zone_dt = [round(1/max((a - b), 0.1), 2) for a, b in
+               zip(zn_temp, zn_tempsp)]
+    zn_dtsp = [round(max(a - b, 0), 2) for a, b in
+               zip(prev_temp, zn_temp)]
+    data.append(
+        [val/sum(zone_dt) if sum(zone_dt) != 0 else 0 for val in zone_dt])
+    data.append(
+        [val/sum(zn_dtsp) if sum(zn_dtsp) != 0 else 0 for val in zn_dtsp])
+    data.append(
+        [val/sum(rtu_pwr) if sum(rtu_pwr) != 0 else 0 for val in rtu_pwr])
+    data.append(
+        [val/sum(zn_type) if sum(zn_type) != 0 else 0 for val in zn_type])
+    data.append(
+        [val/sum(stg_val) if sum(stg_val) != 0 else 0 for val in stg_val])
+    print data
+    while i < len(data[0]):
+        row = [float(col[i]) for col in data]
+        csum.append(row)
+        i += 1
+    input_rtus = csum
+
+    # display_devicematrix(in_matrix, DEVICE_LABELSTRING, rtu_LABELS,
+    # DEVICE_ROWSTRING)
+    return input_rtus
+
+
+def build_score(_matrix, weight, LABELS):
+    '''Calculates the curtailment score using the normalized matrix m,
+
+    and the weights vector returns a sorted vector of weights for each
+    device that is a candidate for curtailment.
+    '''
+    score_matrix = []
+    scores = []
+    for i in range(0, len(_matrix) - 1):
+        criteria_sum = 0
+        for j in range(0, len(weight) - 1):
+            criteria_sum += _matrix[i][j] * weight[j]
+        score_matrix.append([LABELS[i], criteria_sum])
+        scores.append(criteria_sum)
+    # display_matrix([scores,], DEVICE_LABELSTRING, RTU_LABELS,
+    # ['Priority RTU'], DEVICE_ROWSTRING)
+    return scores, sorted(score_matrix, key=lambda l: l[1], reverse=True)
+
+
+def display_matrix(_matrix, LABELString, xLABELS, yLABELS,
+                   rowstring, display_func=sys.stdout):
+    '''Function to diplay the critieria matrix.'''
+    # Display header
+    xLABELS = [item.encode('utf-8') for item in xLABELS]
+
+    print >> display_func, LABELString % tuple(xLABELS)
+    i = 0
+    while i < len(yLABELS):
+        print >> display_func, \
+                 rowstring % ((yLABELS[i],) + tuple(_matrix[i]))
+        i += 1
+
+
+def rtus_scores(scores, P_blg, p_tg, p_rtu):
+    scores[p_rtu < 1.0] = 0   # exclude off-RTUs from priority order
+    priority = map(lambda x: x + 1,
+                   sorted(range(len(scores)),
+                          key=lambda k: scores[k],
+                          reverse=True))
+    curtail = np.size(np.nonzero(np.cumsum(p_rtu) < (P_blg - p_rtu))) + 1
+    print >> sys.stdout, "Curtailed RTU list :",  priority[0:curtail]
+    return priority[0:curtail]
+
+
+def rtus_ctrl(scores, p_blg, p_tag, ctrl_t, ctrl_tsp, hdb, p_rtu):
+    p_tg = (p_tag * 3 - p_blg) / 2   # depend on AHP running periods
+    priority = False
+    if (p_blg > p_tag):
+        p_cur = p_blg - p_tag
+        print >> sys.stdout, "Curtailed demand control: ", p_cur
+        priority = rtus_scores(scores, p_blg, p_tg, p_rtu)
+    return priority
