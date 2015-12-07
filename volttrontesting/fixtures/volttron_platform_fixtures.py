249d27767f3eddc495101d1f780f8c962e1bc663
Craig <craig.allwardt@pnnl.gov>
2015-12-07 15:44:40 -0800

    task #253 Updated volttron instance fixtures

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -1,16 +1,30 @@
+import os
 import pytest
+from volttrontesting.utils.platformwrapper import PlatformWrapper
 
 @pytest.fixture(scope="module")
 def volttron_instance_1(request):
     print('getting instance 1')
+    vh = '/tmp/instance1home'
+    if os.path.exists(vh):
+        os.removedirs(vh)
+    os.makedirs(vh)
+    wrapper = PlatformWrapper("/tmp/instance1home")
     def fin():
+        wrapper.shutdown_platform(cleanup=True)
         print('teardown instance 1')
-    return {'result': 'volttron platform instance'}
+    return wrapper
 
 
 @pytest.fixture(scope="module")
 def volttron_instance_2(request):
     print('getting instance 2')
+    vh = '/tmp/instance2home'
+    if os.path.exists(vh):
+        os.removedirs(vh)
+    os.makedirs(vh)
+    wrapper = PlatformWrapper("/tmp/instance2home")
     def fin():
+        wrapper.shutdown_platform(cleanup=True)
         print ('teardown instance 2')
-    return {'result':'volttron platform instance'}
+    return wrapper
