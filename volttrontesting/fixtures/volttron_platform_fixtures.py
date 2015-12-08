dac8d9f83e27644284d51a36b791af9ddef9d0a1
Craig <craig.allwardt@pnnl.gov>
2015-12-07 16:12:48 -0800

    task #253 added instance configuration fixtures.

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -1,14 +1,26 @@
 import os
+import json
 import pytest
 from volttrontesting.utils.platformwrapper import PlatformWrapper
 
 @pytest.fixture(scope="module")
-def volttron_instance_1(request):
+def instancce_1_config():
+    return {"vip-address": "tcp://127.0.0.1:2296"}
+
+@pytest.fixture(scope="module")
+def instancce_2_config():
+    return {"vip-address": "tcp://127.0.0.2:2296"}
+
+@pytest.fixture(scope="module")
+def volttron_instance_1(request, instancce_1_config):
     print('getting instance 1')
     vh = '/tmp/instance1home'
-    if os.path.exists(vh):
-        os.removedirs(vh)
-    os.makedirs(vh)
+    if not os.path.exists(vh):
+        os.makedirs(vh)
+
+    with open(os.path.join(vh, 'config'), 'w') as fout:
+        fout.write(json.dumps(instancce_1_config))
+
     wrapper = PlatformWrapper("/tmp/instance1home")
     def fin():
         wrapper.shutdown_platform(cleanup=True)
@@ -17,12 +29,14 @@ def volttron_instance_1(request):
 
 
 @pytest.fixture(scope="module")
-def volttron_instance_2(request):
+def volttron_instance_2(request, instancce_2_config):
     print('getting instance 2')
     vh = '/tmp/instance2home'
-    if os.path.exists(vh):
-        os.removedirs(vh)
-    os.makedirs(vh)
+    if not os.path.exists(vh):
+        os.makedirs(vh)
+
+    with open(os.path.join(vh, 'config'), 'w') as fout:
+        fout.write(json.dumps(instancce_2_config))
     wrapper = PlatformWrapper("/tmp/instance2home")
     def fin():
         wrapper.shutdown_platform(cleanup=True)
