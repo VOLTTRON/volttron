9e3b829d3728f00c7484e4df9a1bb3f17f3e1e3d
Craig <craig.allwardt@pnnl.gov>
2015-12-09 11:37:26 -0800

    Modified fixture to de-duplicate code

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -4,29 +4,22 @@ import pytest
 from volttrontesting.utils.platformwrapper import PlatformWrapper
 
 @pytest.fixture(scope="module")
-def instancce_1_config():
+def instance_1_config():
     return {"vip-address": "tcp://127.0.0.1:22916"}
 
 @pytest.fixture(scope="module")
-def instancce_2_config():
+def instance_2_config():
     return {"vip-address": "tcp://127.0.0.2:22916"}
 
+def build_wrapper(platform_config, **kwargs):
+    wrapper = PlatformWrapper()
+    wrapper.startup_platform(platform_config, **kwargs)
+    return wrapper
+
 @pytest.fixture(scope="module")
-def volttron_instance_1(request, instancce_1_config):
-    print('getting instance 1')
-    vh = '/tmp/instance1home'
-    if not os.path.exists(vh):
-        os.makedirs(vh)
-
-    config_dir = os.path.join(vh, 'config')
-    with open(os.path.join(vh, 'config'), 'w') as fout:
-        fout.write(json.dumps(instancce_1_config))
-        fout.close()
-
-    wrapper = PlatformWrapper("/tmp/instance1home")
-    print("CONFIG DIR: ", config_dir)
-    print("EXISTS?: ", os.path.exists(config_dir))
-    wrapper.startup_platform(config_dir)
+def volttron_instance_1(request, instance_1_config):
+    wrapper = build_wrapper(instance_1_config)
+    
     def fin():
         wrapper.shutdown_platform(cleanup_temp=True)
         print('teardown instance 1')
@@ -34,20 +27,11 @@ def volttron_instance_1(request, instancce_1_config):
 
 
 @pytest.fixture(scope="module")
-def volttron_instance_2(request, instancce_2_config):
+def volttron_instance_2(request, instance_2_config):
     print('getting instance 2')
-    vh = '/tmp/instance2home'
-    if not os.path.exists(vh):
-        os.makedirs(vh)
-
-    config_dir = os.path.join(vh, 'config')
-    with open(config_dir, 'w') as fout:
-        fout.write(json.dumps(instancce_2_config))
-        fout.close()
-        
-    wrapper = PlatformWrapper("/tmp/instance2home")
-    wrapper.startup_platform(config_dir)
+    wrapper = build_wrapper(instance_2_config)
+    
     def fin():
         wrapper.shutdown_platform(cleanup_temp=True)
-        print ('teardown instance 2')
+        print('teardown instance 2')
     return wrapper
