6a84e63e653ab2316cef9f7982260333bd339c80
C. Allwardt <craig.allwardt@pnnl.gov>
2015-12-14 16:36:50 -0800

    task #253 Renamed instance_1 and instance_2 to
    instance1 and instance2 respectively.

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -4,34 +4,38 @@ import pytest
 from volttrontesting.utils.platformwrapper import PlatformWrapper
 
 @pytest.fixture(scope="module")
-def instance_1_config():
+def instance1_config():
     return {"vip-address": "tcp://127.0.0.1:22916"}
 
 @pytest.fixture(scope="module")
-def instance_2_config():
+def instance2_config():
     return {"vip-address": "tcp://127.0.0.2:22916"}
 
-def build_wrapper(platform_config, **kwargs):
+def build_wrapper(vip_address, **kwargs):
     wrapper = PlatformWrapper()
-    wrapper.startup_platform(platform_config, **kwargs)
+    print('BUILD_WRAPPER: {}'.format(vip_address))
+    wrapper.startup_platform(vip_address=vip_address, **kwargs)
     return wrapper
 
 @pytest.fixture(scope="module")
-def volttron_instance_1(request, instance_1_config):
-    wrapper = build_wrapper(instance_1_config)
+def volttron_instance1(request, instance1_config):
+    print("building instance 1")
+    wrapper = build_wrapper("tcp://127.0.0.1:22916")
 
-    def fin():
+    def cleanup():
         wrapper.shutdown_platform(True)
         print('teardown instance 1')
+    request.addfinalizer(cleanup)
     return wrapper
 
 
 @pytest.fixture(scope="module")
-def volttron_instance_2(request, instance_2_config):
-    print('getting instance 2')
-    wrapper = build_wrapper(instance_2_config)
+def volttron_instance2(request, instance2_config):
+    print("building instance 2")
+    wrapper = build_wrapper("tcp://127.0.0.2:22916")
 
-    def fin():
+    def cleanup():
         wrapper.shutdown_platform(True)
         print('teardown instance 2')
+    request.addfinalizer(cleanup)
     return wrapper
