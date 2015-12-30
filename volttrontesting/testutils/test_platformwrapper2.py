b5319fcb1ca1afe84799de6df517e951c317b4d9
Craig <craig.allwardt@pnnl.gov>
2015-12-30 00:54:16 -0800

    Updated to use a common random port generator.  Fixed so that the tests run successfully (without hanging).

--- volttrontesting/testutils/test_platformwrapper2.py
+++ volttrontesting/testutils/test_platformwrapper2.py
@@ -3,6 +3,7 @@ import pytest
 import time
 import warnings
 
+from volttrontesting.fixtures.volttron_platform_fixtures import get_rand_port
 from volttrontesting.utils.platformwrapper import PlatformWrapper
 from volttron.platform.vip.agent import Agent, PubSub, Core
 
@@ -14,7 +15,8 @@ def test_can_cleanup_installed_listener():
         warnings.warn('No psutil module present for this test')
         return
     wrapper = PlatformWrapper()
-    address="tcp://127.0.0.1:4848"
+
+    address="tcp://127.0.0.1:{}".format(get_rand_port())
     wrapper.startup_platform(address)
 
     assert wrapper is not None
@@ -24,10 +26,10 @@ def test_can_cleanup_installed_listener():
         start=False)
     assert auuid is not None
     started = wrapper.start_agent(auuid)
-    assert isinstance(started[0], int)
-    assert psutil.pid_exists(started[0])
-    
+    assert isinstance(started, int)
+    assert psutil.pid_exists(started)
+
     wrapper.shutdown_platform()
     # give operating system enough time to update pids.
     gevent.sleep(0.1)
-    assert not psutil.pid_exists(started[0])
+    assert not psutil.pid_exists(started)
