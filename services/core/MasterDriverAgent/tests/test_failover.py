fa817796d3c596638a2ec9bb54c04895513a028e
Mike Roup <michael.roup@pnnl.gov>
2016-01-20 16:59:17 -0800

    Cleanup test_failover

--- services/core/MasterDriverAgent/tests/test_failover.py
+++ services/core/MasterDriverAgent/tests/test_failover.py
@@ -4,7 +4,6 @@ import time
 
 from volttron.platform.vip.agent import Agent, PubSub, Core
 
-from volttrontesting.fixtures.volttron_platform_fixtures import build_wrapper
 
 MAX_WAIT_TIME = 20
 
@@ -12,30 +11,12 @@ messages = {}
 def onmessage(peer, sender, bus, topic, headers, message):
     messages[topic] = message
 
-@pytest.fixture
-def primary_volttron(request, instance1_config):
-    wrapper = build_wrapper(instance1_config['vip-address'])
 
-    def cleanup():
-        print('Shutting down instance: {}'.format(wrapper.volttron_home))
-        wrapper.shutdown_platform(True)
-    request.addfinalizer(cleanup)
-    return wrapper
-
-@pytest.fixture
-def secondary_volttron(request, instance2_config):
-    wrapper = build_wrapper(instance2_config['vip-address'])
-
-    def cleanup():
-        print('Shutting down instance: {}'.format(wrapper.volttron_home))
-        wrapper.shutdown_platform(True)
-    request.addfinalizer(cleanup)
-    return wrapper
-
-
-def test_failover(primary_volttron, secondary_volttron):
+def test_failover(volttron_instance1, volttron_instance2):
     global messages
     agent_dir = "services/core/MasterDriverAgent"
+    primary_volttron = volttron_instance1
+    secondary_volttron = volttron_instance2
 
     # find out how long it takes to get data from primary instance
     assert primary_volttron is not None
@@ -43,8 +24,6 @@ def test_failover(primary_volttron, secondary_volttron):
 
     primary_listener = primary_volttron.build_agent()
     primary_listener.vip.pubsub.subscribe(peer='pubsub', prefix='', callback=onmessage)
-
-    print(agent_dir + "/tests/config0")
 
     primary_master = primary_volttron.install_agent(agent_dir=agent_dir, config_file=agent_dir + "/tests/config0")
     assert primary_master is not None
@@ -54,9 +33,7 @@ def test_failover(primary_volttron, secondary_volttron):
     while not messages.keys() and time.time() < time_start + MAX_WAIT_TIME:
         gevent.sleep(1)
         primary_msg_delay += 1
-        print "PRIMARY {}".format(primary_msg_delay)
 
-    print(messages.keys())
     assert messages.keys()
 
     primary_volttron.stop_agent(primary_master)
@@ -64,9 +41,7 @@ def test_failover(primary_volttron, secondary_volttron):
     assert(not primary_volttron.is_agent_running(primary_master))
 
     
-
     # find out how long it takes to get data from secondary instance
-
     assert secondary_volttron is not None
     assert secondary_volttron.is_running()
 
@@ -81,12 +56,10 @@ def test_failover(primary_volttron, secondary_volttron):
     while not messages.keys() and time.time() < time_start + MAX_WAIT_TIME:
         gevent.sleep(1)
         secondary_msg_delay += 1
-        print "SECONDARY {}".format(secondary_msg_delay)
 
     assert messages.keys()
-    print("tried to delay", messages)
 
-    # they shouldn't take the same ammount of time
+    # there should be a delay beween publishes
     assert (secondary_msg_delay - primary_msg_delay) > 0
 
     assert True
