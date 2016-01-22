9d4d22624e45ce3f7ac65bc5b4164515865cf22d
Mike Roup <michael.roup@pnnl.gov>
2016-01-22 14:16:18 -0800

    Move failover test setup to class

--- services/core/MasterDriverAgent/tests/test_failover.py
+++ services/core/MasterDriverAgent/tests/test_failover.py
@@ -4,62 +4,93 @@ import time
 
 from volttron.platform.vip.agent import Agent, PubSub, Core
 
-
+agent_dir = "services/core/MasterDriverAgent"
 MAX_WAIT_TIME = 20
 
-messages = {}
-def onmessage(peer, sender, bus, topic, headers, message):
-    messages[topic] = message
+class Failover(object):
+    def __init__(self, instance, config):
+        self.instance = instance
+        self.uuid_listener = self.build_listener()
+        self.uuid_master = self.build_master(config)
+        self.messages = {}
+
+    def _save_message(self, peer, sender, bus, topic, headers, message):
+        self.messages[topic] = message
+
+    def build_listener(self):
+        uuid = self.instance.build_agent()
+        uuid.vip.pubsub.subscribe(peer='pubsub', prefix='', callback=self._save_message)
+        return uuid
+
+    def build_master(self, config):
+        uuid = self.instance.install_agent(agent_dir=agent_dir, config_file=agent_dir + config)
+        assert uuid is not None
+        return uuid
+
+    def stop_master(self):
+        self.instance.stop_agent(self.uuid_master)
+
+    def time_delay(self):
+        m = self.messages
+        m.clear()
+        assert not m.keys()
+
+        publish_delay = 0
+        time_start = time.time()
+        while not m.keys() and time.time() < time_start + MAX_WAIT_TIME:
+            gevent.sleep(1)
+            publish_delay += 1
+
+        return publish_delay
+
+    def is_publishing(self):
+        return self.time_delay() < MAX_WAIT_TIME
 
 
-def test_failover(volttron_instance1, volttron_instance2):
-    global messages
-    agent_dir = "services/core/MasterDriverAgent"
-    primary_volttron = volttron_instance1
-    secondary_volttron = volttron_instance2
+@pytest.fixture
+def failover_primary(request, volttron_instance1):
+    p = Failover(volttron_instance1,"/tests/config0")
+    def cleanup():
+        p.instance.stop_agent(p.uuid_master)
+        p.instance.remove_agent(p.uuid_master)
+    request.addfinalizer(cleanup)
+    return p
 
-    # find out how long it takes to get data from primary instance
-    assert primary_volttron is not None
-    assert primary_volttron.is_running()
 
-    primary_listener = primary_volttron.build_agent()
-    primary_listener.vip.pubsub.subscribe(peer='pubsub', prefix='', callback=onmessage)
+@pytest.fixture
+def failover_secondary(request, volttron_instance2):
+    s = Failover(volttron_instance2,"/tests/config1")
+    def cleanup():
+        s.instance.stop_agent(s.uuid_master)
+        s.instance.remove_agent(s.uuid_master)
+    request.addfinalizer(cleanup)
+    return s
 
-    primary_master = primary_volttron.install_agent(agent_dir=agent_dir, config_file=agent_dir + "/tests/config0")
-    assert primary_master is not None
 
-    time_start = time.time()
-    primary_msg_delay = 0
-    while not messages.keys() and time.time() < time_start + MAX_WAIT_TIME:
-        gevent.sleep(1)
-        primary_msg_delay += 1
+def test_failover_publish_delay(failover_primary, failover_secondary):
+    primary = failover_primary
+    secondary = failover_secondary
 
-    assert messages.keys()
+    primary_msg_delay = primary.time_delay()
+    assert primary.messages.keys()
 
-    primary_volttron.stop_agent(primary_master)
-    messages.clear()
-    assert(not primary_volttron.is_agent_running(primary_master))
+    secondary_msg_delay = secondary.time_delay()
+    assert secondary.messages.keys()
 
-    
-    # find out how long it takes to get data from secondary instance
-    assert secondary_volttron is not None
-    assert secondary_volttron.is_running()
+    # there should be a five second delay beween publishes
+    assert abs(secondary_msg_delay - primary_msg_delay) == 5
+    assert True
 
-    secondary_listener = secondary_volttron.build_agent()
-    secondary_listener.vip.pubsub.subscribe(peer='pubsub', prefix='', callback=onmessage)
 
-    secondary_master = secondary_volttron.install_agent(agent_dir=agent_dir, config_file=agent_dir + "/tests/config1")
-    assert secondary_master is not None
+def test_failure_does_not_cascade(failover_primary, failover_secondary):
+    primary = failover_primary
+    secondary = failover_secondary
 
-    time_start = time.time()
-    secondary_msg_delay = 0
-    while not messages.keys() and time.time() < time_start + MAX_WAIT_TIME:
-        gevent.sleep(1)
-        secondary_msg_delay += 1
+    assert primary.is_publishing()
+    assert secondary.is_publishing()
 
-    assert messages.keys()
+    primary.stop_master()
+    assert not primary.is_publishing()
 
-    # there should be a delay beween publishes
-    assert (secondary_msg_delay - primary_msg_delay) > 0
-
+    assert secondary.is_publishing()
     assert True
