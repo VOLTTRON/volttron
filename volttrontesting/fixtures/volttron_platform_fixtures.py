2a9634393b2dddf20c17ce88843859278a833620
Craig <craig.allwardt@pnnl.gov>
2016-01-22 23:11:35 -0800

    Made PEP-8 compliant.

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -3,6 +3,7 @@ import json
 import pytest
 from volttrontesting.utils.platformwrapper import PlatformWrapper
 
+
 def get_rand_port():
     from random import randint
     port = randint(5000, 6000)
@@ -10,27 +11,32 @@ def get_rand_port():
         port = randint(5000, 6000)
     return port
 
+
 def is_port_open(port):
     import socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex(('127.0.0.1',port))
     return result == 0
 
+
 @pytest.fixture(scope="module")
 def instance1_config():
     port = get_rand_port()
     return {"vip-address": "tcp://127.0.0.1:{}".format(port)}
+
 
 @pytest.fixture(scope="module")
 def instance2_config():
     port = get_rand_port()
     return {"vip-address": "tcp://127.0.0.1:{}".format(port)}
 
+
 def build_wrapper(vip_address, **kwargs):
     wrapper = PlatformWrapper()
     print('BUILD_WRAPPER: {}'.format(vip_address))
     wrapper.startup_platform(vip_address=vip_address, **kwargs)
     return wrapper
+
 
 @pytest.fixture(scope="module")
 def volttron_instance1(request, instance1_config):
@@ -53,6 +59,7 @@ def volttron_instance2(request, instance2_config):
         wrapper.shutdown_platform(True)
     request.addfinalizer(cleanup)
     return wrapper
+
 
 @pytest.fixture(scope="function")
 def volttron_instance1_encrypt(request):
