2435bc347254019bef1b519e752144e9291e4454
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-02-23 09:03:22 -0800

    Added printing of the logfile if necessary for the test methods.
    Conflicts:
    	volttrontesting/fixtures/volttron_platform_fixtures.py

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -3,6 +3,13 @@ import json
 import pytest
 from volttrontesting.utils.platformwrapper import PlatformWrapper
 
+PRINT_LOG_ON_SHUTDOWN = True
+
+def print_log(volttron_home):
+    if PRINT_LOG_ON_SHUTDOWN:
+        with open(volttron_home+"/volttron.log") as fin:
+            print(fin.read())
+
 
 def get_rand_port():
     from random import randint
