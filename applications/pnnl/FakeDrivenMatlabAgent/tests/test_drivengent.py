6f1dba9e22d63c70b576d00bf12db3931835ae41
Poorva <poorva1209@gmail.com>
2016-04-14 13:44:32 -0700

    #Task #319 removed comments

--- applications/pnnl/FakeDrivenMatlabAgent/tests/test_drivengent.py
+++ applications/pnnl/FakeDrivenMatlabAgent/tests/test_drivengent.py
@@ -43,55 +43,6 @@ config_wh = {
     }
 }
 
-
-
-# @pytest.fixture(scope="module",
-#                 params=['volttron_2', 'volttron_3'])
-# def publish_agent(request, volttron_instance1):
-#      1: Start a fake agent to publish to message bus
-#     if request.param == 'volttron_2':
-#         agent = PublishMixin(
-#             volttron_instance1.opts['publish_address'])
-#     else:
-#         agent = volttron_instance1.build_agent()
-# 
-#      2: add a tear down method to stop sqlhistorian agent and the fake
-#      agent that published to message bus
-#     def stop_agent():
-#         print("In teardown method of module")
-#         if isinstance(agent, Agent):
-#             agent.core.stop()
-# 
-#     request.addfinalizer(stop_agent)
-#     return agent
-
-# Fixtures for setup and teardown of drivenmatlabagent agent
-# @pytest.fixture(scope="module",volttron_instance1,params=[])
-# def drivenmatlabagent(request, volttron_instance1):
-#     print("** Setting up test_drivenagent module **")
-#     # Make database connection
-#     print("request param", request.param)
-#     
-#     # Install and start drivenagent agent
-#     agent_uuid = volttron_instance1.install_agent(
-#         agent_dir="application/pnnl/DrivenMatlabAgent",
-#         config_file=request.param,
-#         start=True)
-#     print("agent id: ", agent_uuid)
-# 
-#     # Tear down method to stop drivenmatlabagent agent and the fake
-#     # agent that published to message bus
-#     def stop_agent():
-#         print("In teardown method of module")
-#         #volttron_instance1.stop_agent(agent_uuid)
-# 
-#     request.addfinalizer(stop_agent)
-#     return request.param@pytest.fixture(scope="module",volttron_instance1,params=[])
-
-def onmessage(self, peer, sender, bus, topic, headers, message):
-    print(message)
-
-    
 @pytest.mark.dev
 def test_drivenmatlabagent(volttron_instance1):
     print("** Setting up test_drivenagent module **")
@@ -136,28 +87,3 @@ def test_drivenmatlabagent(volttron_instance1):
     value = driven_agent.vip.rpc.call('platform.actuator', 'get_point', path).get()
     print('The set point value is '+str(value))
     assert value == 1 
-    
-    
-    
-# #Subscribe to command_topic
-# agent.vip.pubsub.subscribe(peer = 'pubsub',
-#            prefix = command_topic,
-#            callback = onmessage).get(timeout=5)
-# 
-# now = datetime.datetime.now().isoformat(' ')
-# devices_topic = 'devices/PNNL/2400_STEVENS/HEATER1/all'
-# headers = {'Date': now,
-# 'max_compatible_version': u'', 
-# 'min_compatible_version': '3.0'}
-# message = [{'temperature': 57.0}, 
-# {'temperature': {'units': 'Farenheit', 
-#              'type': 'float', 
-#              'tz': 'US/Pacific'}}]
-# 
-# agent.vip.pubsub.publish('pubsub', 
-#          devices_topic, 
-#          headers, 
-#          message).get(timeout=5)
-# gevent.sleep(2)
-
-
