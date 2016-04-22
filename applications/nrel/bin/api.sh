bcc46aae3f51f3fd9216487319176f0244b851e0
Vaidhynathan, Deepthi <deepthi.vaidhynathan@nrel.gov>
2016-04-22 14:34:22 -0600

    shell

--- applications/nrel/bin/api.sh
+++ applications/nrel/bin/api.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 rm volttron.log
 rm status.log
 
@@ -8,4 +8,4 @@ volttron-ctl start --tag API_test_radiothermostatrelay
 volttron-ctl start --tag schouse_controller
 volttron-ctl start --tag vtime_now
 
-volttron-ctl status 
+volttron-ctl status
