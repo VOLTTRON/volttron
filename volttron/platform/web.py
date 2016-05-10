99318c4a1fb8a2ab71a52ede83d848168a4e03e1
volttron central <craig.allwardt@pnnl.gov>
2016-05-09 17:12:20 -0700

    Removed logging from web.py cuz it did not work as expected

--- volttron/platform/web.py
+++ volttron/platform/web.py
@@ -418,8 +418,7 @@ class MasterWebService(Agent):
         self.registeredroutes.append((re.compile('^/$'), 'callable',
                                       self._redirect_index))
         port = int(port)
-        server = pywsgi.WSGIServer((hostname, port), self.app_routing,
-                                   log=logging.getLogger("wsgi"))
+	server = pywsgi.WSGIServer((hostname, port), self.app_routing)
         server.serve_forever()
 
 
