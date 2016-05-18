7a16daf64464e0f3fdfe19a8e85f1eb424322243
Kyle Monson <kyle.monson@pnnl.gov>
2016-05-18 14:55:38 -0700

    Fixed agent test startup scripts.

--- scripts/launch_proxy.sh
+++ scripts/launch_proxy.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
 # Manually launch the bacnet proxy agent. Useful for debugging as running this way will dump driver logging data directly to the console.
 pushd ../services/core/BACnetProxy
-AGENT_CONFIG=bacnet-proxy.agent python -m bacnet_proxy.agent
+if [ -z "$VOLTTRON_HOME" ]; then
+    export VOLTTRON_HOME=~/.volttron
+fi
+export AGENT_CONFIG=bacnet-proxy.agent
+python -m bacnet_proxy.agent
 popd
 
