03cc00bd7af6aebdc5d45c76a316b48a30b618b1
Craig <craig.allwardt@pnnl.gov>
2016-05-20 04:53:46 -0400

    Bugfix/vc2 (#563)
    * task 529 Pinned charts are saved to the database, so they show up in dashboard when vc first loads. Unpinning a chart removes it from tthe dashboard and database. Changing settings such as chart type refresh rate are saved to database iff the chart is pinned or gets pinned. Pulled some files from feature/web. Pulled enhanced control-button component from vc-config-devices. Removed code and features not in 3.5 release, such as edit-chart-form and combo-box. Added onclickoutside feature to control buttons, so taptips go away when click outside the control.
    
    *  Fixed #543 Added None check from the store for keys that aren't present.
    
    * task #529 Added get_setting_keys check to loadCharts. Removed =null default values from new-register.
    
    * task #535 Added loading flag to prevent double-clicking top node when first expanding it.
    
    * task #545 Now orphan tooltips are hidden when clicking outside the chart.
    
    * task #548 Fixed issue where points with special characters in their names don't get added to charts.
    
    * Fixed #543 Added py.test for adding null value which removes the key.
    Added a null value send to new-register.html
    
    * Documentation update
    
    *  #554 Significant gains are made from caching the agent list data.
     Registry now holds both device and agent_list data.
    
    * Fixed #553 Added return statement.
    
    *  #534 Redesigning to use pubsub for registration of agent.
     Moved some test code out of test_webapi module to a vctestutility.
    
    * Changed topic that agents will publish to.
    
    * #534 Broke out the handle registration through pubsub method
    added reconnect to platforms back in.
    
    * #534 Now restarting either vc or vcp doesn't give multiple entries.
    
    * #554 Added spawn later to startup of platform agent so delays don't happen.
    with the ui and refresheing of state.
    Fixed issue with get versus get_platform in the registry class.
    
    * #534 No duplicates when starting/stoping reinstalling agents now.
    
    * Auto create data directory if it doesn't exist.

--- services/core/VolttronCentral/tests/test_webapi.py
+++ services/core/VolttronCentral/tests/test_webapi.py
@@ -6,57 +6,10 @@ import sys
 from volttron.platform.messaging.health import STATUS_GOOD
 from volttrontesting.utils.utils import poll_gevent_sleep
 from zmq.utils import jsonapi
-
-
-class FailedToGetAuthorization(Exception):
-    pass
-
-
-class APITester(object):
-    def __init__(self, url, username='admin', password='admin'):
-        self._url = url
-        self._username = username
-        self._password = password
-        self._auth_token = self.get_auth_token()
-
-    def do_rpc(self, method, use_auth_token=True, **params):
-        data = {
-            'jsonrpc': '2.0',
-            'method': method,
-            'params': params,
-            'id': '1'
-        }
-        if use_auth_token:
-            data['authorization'] = self._auth_token
-        print('posting data')
-        print(data)
-        return requests.post(self._url, json=data)
-
-    def get_auth_token(self):
-        response = self.do_rpc(
-            'get_authorization', use_auth_token=False,
-            username=self._username, password=self._password)
-        if not response:
-            raise FailedToGetAuthorization
-        validate_response(response)
-        return json.loads(response.content)['result']
-
-    def inspect(self, platform_uuid, agent_uuid):
-        return self.do_rpc('platforms.uuid.{}.agents.uuid.{}.'
-                           'inspect'.format(platform_uuid, agent_uuid))
-
-    def register_instance(self, addr, name=None):
-        return self.do_rpc('register_instance', discovery_address=addr,
-                           display_name=name)
-
-    def list_platforms(self):
-        return self.do_rpc('list_platforms')
-
-    def list_agents(self, platform_uuid):
-        return self.do_rpc('platforms.uuid.' + platform_uuid + '.list_agents')
-
-    def unregister_platform(self, platform_uuid):
-        return self.do_rpc('unregister_platform', platform_uuid=platform_uuid)
+from vctestutils import (APITester, FailedToGetAuthorization,
+                         check_multiple_platforms, validate_response,
+                         authenticate, do_rpc, validate_at_least_one,
+                         each_result_contains)
 
 
 @pytest.fixture(scope="function")
@@ -80,90 +33,8 @@ def web_api_tester(request, vc_instance, pa_instance):
     return tester
 
 
-def do_rpc(method, params=None, auth_token=None, rpc_root=None):
-    """ A utility method for calling json rpc based funnctions.
-
-    :param method: The method to call
-    :param params: the parameters to the method
-    :param auth_token: A token if the user has one.
-    :param rpc_root: Root of jsonrpc api.
-    :return: The result of the rpc method.
-    """
-
-    assert rpc_root, "Must pass a jsonrpc url in to the function."
-
-    json_package = {
-        'jsonrpc': '2.0',
-        'id': '2503402',
-        'method': method,
-    }
-
-    if auth_token:
-        json_package['authorization'] = auth_token
-
-    if params:
-        json_package['params'] = params
-
-    data = jsonapi.dumps(json_package)
-
-    return requests.post(rpc_root, data=data)
-
-
-def authenticate(jsonrpcaddr, username, password):
-    """ Authenticate a user with a username and password.
-
-    :param jsonrpcaddr:
-    :param username:
-    :param password:
-    :return a tuple with username and auth token
-    """
-
-    print('RPCADDR: ', jsonrpcaddr)
-    response = do_rpc("get_authorization", {'username': username,
-                                            'password': password},
-                      rpc_root=jsonrpcaddr)
-
-    validate_response(response)
-    jsonres = response.json()
-
-    return username, jsonres['result']
-
-
-def check_multiple_platforms(platformwrapper1, platformwrapper2):
-    assert platformwrapper1.bind_web_address
-    assert platformwrapper2.bind_web_address
-    assert platformwrapper1.bind_web_address != \
-        platformwrapper2.bind_web_address
-
-
-def each_result_contains(result_list, fields):
-    for result in result_list:
-        assert all(field in result.keys() for field in fields)
-
-
-def validate_at_least_one(response):
-    validate_response(response)
-    result = response.json()['result']
-    assert len(result) > 0
-    return result
-
-
-def validate_response(response):
-    """ Validate that the message is a json-rpc response.
-
-    :param response:
-    :return:
-    """
-    assert response.ok
-    rpcdict = response.json()
-    print('RPCDICT', rpcdict)
-    assert rpcdict['jsonrpc'] == '2.0'
-    assert rpcdict['id']
-    assert 'error' in rpcdict.keys() or 'result' in rpcdict.keys()
-
-
 @pytest.mark.vc
-@pytest.mark.xfail(reason='Not sure why this is failing.')
+#@pytest.mark.xfail(reason='Not sure why this is failing.')
 def test_auto_register_platform(vc_instance):
     vc, vcuuid, jsonrpc = vc_instance
 
@@ -189,6 +60,25 @@ def test_auto_register_platform(vc_instance):
         return len(response.json()['result']) > 0
 
     assert poll_gevent_sleep(6, redo_request)
+    response = tester.do_rpc("list_platforms")
+    assert len(response.json()['result']) > 0
+    jsondata = response.json()
+    # Specific platform not the same as vcp on the platform
+    platform_uuid = jsondata['result'][0]['uuid']
+    # Remove the agent.
+    vc.remove_agent(pauuid)
+    assert vc.list_agents() == 1
+    newpauuid = vc.install_agent(agent_dir=adir, config_file=adir + "config")
+    assert newpauuid != pauuid
+    assert poll_gevent_sleep(6, redo_request)
+    response = tester.do_rpc("list_platforms")
+    jsondata = response.json()
+    # Specific platform not the same as vcp on the platform
+    platform_uuid2= jsondata['result'][0]['uuid']
+    assert platform_uuid == platform_uuid2
+
+
+
 
 
 @pytest.mark.vc
@@ -240,6 +130,7 @@ def test_vc_settings_store(vc_instance):
     resp = tester.do_rpc('get_setting_keys')
     assert kv['key'] not in resp.json()['result']
 
+
 @pytest.mark.vc
 def test_register_instance(vc_instance, pa_instance):
 
@@ -263,8 +154,7 @@ def test_register_instance(vc_instance, pa_instance):
     assert result['status'] == 'SUCCESS'
 
     # list platforms
-    response = do_rpc("list_platforms", [pa_wrapper.bind_web_address],
-                      auth, vc_jsonrpc)
+    response = do_rpc("list_platforms", None, auth, vc_jsonrpc)
     validate_response(response)
     platforms = response.json()['result']
     assert len(platforms) == 1
