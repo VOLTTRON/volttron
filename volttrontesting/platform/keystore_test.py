a9da1cdb3a4c66d50f0c877456a116b8e0f8c2e5
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-06-08 18:35:26 -0700

    Revert "Revert "Revert "merged bootstrap"""
    This reverts commit 5488209ae64bf43d8367adecc7962837f510b253.

--- volttrontesting/platform/keystore_test.py
+++ volttrontesting/platform/keystore_test.py
@@ -1,4 +1,3 @@
-import json
 import os
 import tempfile
 
@@ -10,7 +9,7 @@ host_pair1 = {'addr': 'tcp://127.0.0.1:1234', 'key': 'ABCDEFG'}
 host_pair2 = {'addr': 'tcp://192.168.0.2:1234', 'key': '123456789'}
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def keystore_instance1(tmpdir_factory):
     path = str(tmpdir_factory.mktemp('keys').join('keys.json'))
     print('KEYSTORE PATH:', path)
@@ -23,7 +22,7 @@ def keystore_instance1(tmpdir_factory):
 def test_keystore_generated_when_created(tmpdir_factory):
     kspath = str(tmpdir_factory.mktemp('keys').join('keys.json'))
     ks = keystore.KeyStore(kspath)
-    assert os.stat(kspath).st_mode & 0o777 == 0o600
+    assert os.stat(kspath).st_mode & 0o777 == 0o660
     assert ks.secret()
     assert ks.public()
 
@@ -90,17 +89,3 @@ def test_known_hosts_update_entry(known_hosts_instance1):
 
     known_hosts_instance1.add(host_pair1['addr'], new_key)
     assert known_hosts_instance1.serverkey(host_pair1['addr']) == new_key
-
-
-@pytest.mark.keystore
-def test_invalid_unicode_key(keystore_instance1):
-    """
-    Bypass KeyStore API and directly edit key to make it unicode.
-    The keys should always be ASCII characters. This is a precaution
-    against a corrupted key store.
-    """
-    with open(keystore_instance1.filename) as fp:
-        keystore_json = json.load(fp)
-    keystore_json['public'] = u'\u0100'
-    keystore_instance1.update(keystore_json)
-    assert keystore_instance1.public() is None
