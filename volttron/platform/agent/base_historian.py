8de0ac488f9019c6ff8c93f2d75b8bad02fe117d
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-06-13 11:47:44 -0700

    Fixed: #618 Analysis is now getting /all appended to it. This allows us to use the same device data code for both analysis and device.

--- volttron/platform/agent/base_historian.py
+++ volttron/platform/agent/base_historian.py
@@ -411,7 +411,6 @@ class BaseHistorianAgent(Agent):
         _log.debug(
             "Queuing {topic} from {source} for publish".format(topic=topic,
                                                                source=source))
-        _log.debug(data)
         for point, item in data.iteritems():
             #             ts_path = location + '/' + point
             if 'Readings' not in item or 'Units' not in item:
@@ -473,9 +472,13 @@ class BaseHistorianAgent(Agent):
         Filter out all but the all topics
         """
 
-        if topic.endswith("/all") or '/all/' in topic:
-            return
+	# topic now is going to always end in all.
+        if not topic.endswith('/'):
+            topic = topic + '/'
 
+	if not topic.endswith('all'):
+	   topic = topic + 'all'
+        
         # Anon the topic if necessary.
         topic = self._get_topic(topic)
         parts = topic.split('/')
@@ -493,7 +496,6 @@ class BaseHistorianAgent(Agent):
         timestamp = get_aware_utc_now()
         if timestamp_string is not None:
             timestamp, my_tz = process_timestamp(timestamp_string, topic)
-        _log.debug("### In capture_data timestamp str {} ".format(timestamp))
         try:
             _log.debug(
                 "### In capture_data Actual message {} ".format(message))
@@ -502,7 +504,6 @@ class BaseHistorianAgent(Agent):
             if sender == 'pubsub.compat':
                 # message = jsonapi.loads(message[0])
                 message = compat.unpack_legacy_message(headers, message)
-                _log.debug("### message after compat {}".format(message))
 
             if isinstance(message, dict):
                 values = message
@@ -547,7 +548,6 @@ class BaseHistorianAgent(Agent):
         # Anon the topic if necessary.
         topic = self._get_topic(topic)
         timestamp_string = headers.get('time')
-        _log.debug("TIMESTMAMP_STRING: {}".format(timestamp_string))
         if timestamp_string is None:
             _log.error(
                 "message for {topic} missing timetamp".format(topic=topic))
@@ -1043,7 +1043,7 @@ class BaseQueryHistorianAgent(Agent):
                           "key2": value2,
                           ...}
             }
-         
+
         Timestamps must be strings formatted by
         :py:func:`volttron.platform.agent.utils.format_timestamp`.
 
