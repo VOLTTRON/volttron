b4802414dc6980d615fb74d6d3a2cbad5d122ae8
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-06-22 13:36:36 -0400

    XXX! use bytes() to pass a pathname to inotify_add_watch. May not be the right place to do it wiht

--- volttron/platform/lib/inotify/__init__.py
+++ volttron/platform/lib/inotify/__init__.py
@@ -160,7 +160,7 @@ class _inotify(object):
             self._fd = None
 
     def add_watch(self, pathname, mask=IN_ALL_EVENTS):
-        wd = inotify_add_watch(self.fileno(), pathname, mask | IN_IGNORED)
+        wd = inotify_add_watch(self.fileno(), bytes(pathname,encoding="utf-8"), mask | IN_IGNORED)
         with self._lock:
             self._watch_names[pathname] = wd
             self._watch_wds[wd] = (pathname, mask)
