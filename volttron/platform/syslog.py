6473d19e5bb2eb9f20c5e381aeea8108acf28ed8
Poorva <poorva1209@gmail.com>
2016-06-29 10:46:49 -0700

    added syslog agent to platform

---
+++ volttron/platform/syslog.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+
+# Copyright (c) 2015, Battelle Memorial Institute
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation
+# are those of the authors and should not be interpreted as representing
+# official policies, either expressed or implied, of the FreeBSD
+# Project.
+#
+# This material was prepared as an account of work sponsored by an
+# agency of the United States Government.  Neither the United States
+# Government nor the United States Department of Energy, nor Battelle,
+# nor any of their employees, nor any jurisdiction or organization that
+# has cooperated in the development of these materials, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness or any
+# information, apparatus, product, software, or process disclosed, or
+# represents that its use would not infringe privately owned rights.
+#
+# Reference herein to any specific commercial product, process, or
+# service by trade name, trademark, manufacturer, or otherwise does not
+# necessarily constitute or imply its endorsement, recommendation, or
+# favoring by the United States Government or any agency thereof, or
+# Battelle Memorial Institute. The views and opinions of authors
+# expressed herein do not necessarily state or reflect those of the
+# United States Government or any agency thereof.
+#
+# PACIFIC NORTHWEST NATIONAL LABORATORY
+# operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# under Contract DE-AC05-76RL01830
+
+# }}}
+
+import datetime
+import sys
+import logging
+
+from gevent.fileobject import FileObject
+from volttron.platform.agent.utils import watch_file
+from volttron.platform.messaging.health import STATUS_BAD, STATUS_GOOD, Status
+from volttron.platform.vip.agent import Agent, Core, PubSub, RPC
+from volttron.platform.messaging import (headers as headers_mod, topics)
+from volttron.platform.agent import utils
+from volttron.platform.messaging.utils import normtopic
+
+from dateutil.parser import parse
+
+
+utils.setup_logging()
+_log = logging.getLogger(__name__)
+
+__version__ = "0.1"
+
+
+class SyslogAgent(Agent):
+    """
+    The `SyslogAgent` is a agent that publishes /var/log/syslog content
+    on message bus whenever there is change on that file.
+    """
+
+    def __init__(self, config_path, **kwargs):
+        """ Configures the `SyslogAgent`
+
+        Validates that the outfile parameter in the config file is specified
+        and sets up the agent.
+
+        @param config_path: path to the configuration file for this agent.
+        @param kwargs:
+        @return:
+        """
+        kwargs.pop('identity')
+        super(SyslogAgent, self).__init__(identity='syslog', **kwargs)
+        self.syslog_file_path = '/home/volttron/git/volttron/examples/SyslogAgent/syslog/temp'
+        self.topic = 'platform/syslog'
+        self.f = open(self.syslog_file_path, 'r')
+        
+    @Core.receiver('onstart')
+    def starting(self, sender, **kwargs):
+        #TODO: SEEK_END
+        self.f.seek(0,2)
+        self.core.spawn(watch_file, self.syslog_file_path, self.read_syslog_file)
+        
+    def read_syslog_file(self):
+        _log.info('loading syslog file %s', self.syslog_file_path)
+        for line in self.f.readlines():
+            self.publish_syslog(line.strip())
+        
+    def publish_syslog(self, line):
+        print('publishing syslog line {}'.format(line))
+        headers = {
+                    headers_mod.CONTENT_TYPE: headers_mod.CONTENT_TYPE.JSON,
+                    #headers_mod.DATE: str(self.received_input_datetime),
+                }
+        message = {'timestamp':line[:15],
+                    'line': line[15:]}
+        print(message)
+        self.vip.pubsub.publish(peer="pubsub", topic=self.topic,
+                                headers=headers, message=message)
+
+def main(argv=sys.argv):
+    '''Main method called to start the agent.'''
+    utils.vip_main(SyslogAgent)
+
+
+if __name__ == '__main__':
+    # Entry point for script
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        pass
