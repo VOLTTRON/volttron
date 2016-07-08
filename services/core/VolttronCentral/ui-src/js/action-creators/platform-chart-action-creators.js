5feb1a54e98ce150f3cd8fb3187164c42441fd7b
Joe Chapman <joseph.chapman@pnnl.gov>
2016-07-08 16:50:34 -0700

    task #659 Added setting and saving max/min y axis for charts. Added buttons to change max/min y axis.

--- services/core/VolttronCentral/ui-src/js/action-creators/platform-chart-action-creators.js
+++ services/core/VolttronCentral/ui-src/js/action-creators/platform-chart-action-creators.js
@@ -25,13 +25,27 @@ var platformChartActionCreators = {
 			chartType: chartType
 		});
 	},
-	changeRefreshRate: function (rate, chartKey) {
-		dispatcher.dispatch({
-			type: ACTION_TYPES.CHANGE_CHART_REFRESH,
-			rate: rate,
-			chartKey: chartKey
-		});
-	},
+    changeRefreshRate: function (rate, chartKey) {
+        dispatcher.dispatch({
+            type: ACTION_TYPES.CHANGE_CHART_REFRESH,
+            rate: rate,
+            chartKey: chartKey
+        });
+    },
+    setMin: function (min, chartKey) {
+        dispatcher.dispatch({
+            type: ACTION_TYPES.CHANGE_CHART_MIN,
+            min: min,
+            chartKey: chartKey
+        });
+    },
+    setMax: function (max, chartKey) {
+        dispatcher.dispatch({
+            type: ACTION_TYPES.CHANGE_CHART_MAX,
+            max: max,
+            chartKey: chartKey
+        });
+    },
 	refreshChart: function (series) {
 
 		var authorization = authorizationStore.getAuthorization();
