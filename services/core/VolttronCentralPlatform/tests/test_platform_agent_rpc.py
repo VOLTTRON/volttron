58ed1e9039011860880a5c6e491566fe1515ae0d
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-08-18 08:55:18 -0700

    Added tests for the rpc functions that vc will ultimately be called on it.

---
+++ services/core/VolttronCentralPlatform/tests/test_platform_agent_rpc.py
@@ -0,0 +1,77 @@
+import gevent
+from volttron.platform.agent.known_identities import VOLTTRON_CENTRAL_PLATFORM
+from volttron.platform.jsonrpc import json_validate_response
+from volttron.platform.messaging.health import STATUS_GOOD
+from volttrontesting.utils.webapi import WebAPI
+
+import pytest
+from volttrontesting.utils.core_service_installs import add_volttron_central, \
+    add_volttron_central_platform
+from volttrontesting.utils.platformwrapper import start_wrapper_platform
+from volttrontesting.utils.utils import poll_gevent_sleep
+
+vcp = None
+
+
+@pytest.fixture(scope="module")
+def setup_platform(request, get_volttron_instances):
+    """ Creates a single instance of VOLTTRON with a VOLTTRON Central Platform
+
+    The VOLTTRON Central Platform agent is not registered with a VOLTTRON
+    Central Platform.
+    """
+    global vcp
+    vcp = get_volttron_instances(1)
+
+    assert vcp
+    assert vcp.is_running()
+    vcp_uuid = add_volttron_central_platform(vcp)
+
+    assert vcp_uuid, "Invalid vcp uuid returned"
+    assert vcp.is_agent_running(vcp_uuid), "vcp wasn't running!"
+
+    return vcp
+
+
+@pytest.mark.vcp
+def test_list_agents(setup_platform):
+    assert vcp.is_running()
+
+    connection = vcp.build_connection(peer=VOLTTRON_CENTRAL_PLATFORM)
+
+    assert VOLTTRON_CENTRAL_PLATFORM in connection.peers()
+
+    agent_list = connection.call("list_agents")
+
+    assert agent_list and len(agent_list) == 1
+
+
+@pytest.mark.vcp
+def test_can_inspect_agent(setup_platform):
+    connection = vcp.build_connection(peer=VOLTTRON_CENTRAL_PLATFORM)
+
+    output = connection.call('inspect')
+    methods = output['methods']
+    assert 'list_agents' in methods
+    assert 'start_agent' in methods
+    assert 'stop_agent' in methods
+    assert 'restart_agent' in methods
+    assert 'agent_status' in methods
+    assert 'get_device' in methods
+    assert 'get_devices' in methods
+    assert 'route_request' in methods
+    assert 'manage' in methods
+    assert 'unmanage' in methods
+    assert 'get_health' in methods
+    assert 'reconfigure' in methods
+    assert 'get_instance_uuid' in methods
+    assert 'get_manager_key' in methods
+    assert 'list_agent_methods' in methods
+
+
+@pytest.mark.vcp
+def test_can_call_rpc_method(setup_platform):
+    connection = vcp.build_connection(peer=VOLTTRON_CENTRAL_PLATFORM)
+
+    health = connection.call('get_health')
+    assert health['status'] == STATUS_GOOD
