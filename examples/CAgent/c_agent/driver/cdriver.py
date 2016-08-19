248f5373c6be714accc5d2300a2ed5619dc83eb6
Mike Roup <michael.roup@pnnl.gov>
2016-08-19 14:15:48 -0700

    update c driver for driver revert

--- examples/CAgent/c_agent/driver/cdriver.py
+++ examples/CAgent/c_agent/driver/cdriver.py
@@ -63,11 +63,10 @@ need to be somewhere it can be found by this file.
 from StringIO import StringIO
 from csv import DictReader
 
-from master_driver.interfaces import BaseInterface, BaseRegister
+from master_driver.interfaces import BasicRevert, BaseInterface, BaseRegister
 
 from ctypes import *
 
-SO_FILENAME = "libfoo.so"
 
 def so_lookup_function(shared_object, function_name):
     """Attempt to find a symbol in the loaded shared object
@@ -91,7 +90,7 @@ class CRegister(BaseRegister):
         super(CRegister, self).__init__("byte", read_only, pointName, units, description = '')
 
 
-class Interface(BaseInterface):
+class Interface(BasicRevert, BaseInterface):
     """Simple interface that calls c code.
     Function names are constructed based on register
     point names for brevity. Few if any APIs will
@@ -100,10 +99,10 @@ class Interface(BaseInterface):
     def __init__(self, **kwargs):
         super(Interface, self).__init__(**kwargs)
 
-        cdll.LoadLibrary(SO_FILENAME)
-        self.shared_object = CDLL(SO_FILENAME)
-
     def configure(self, config_dict, registry_config_str):
+        so_filename = config_dict['shared_object']
+        cdll.LoadLibrary(so_filename)
+        self.shared_object = CDLL(so_filename)
         self.parse_config(registry_config_str)
 
     def get_point(self, point_name):
@@ -113,7 +112,7 @@ class Interface(BaseInterface):
 
         return so_get_point()
 
-    def set_point(self, point_name, value):
+    def _set_point(self, point_name, value):
         register = self.get_register_by_name(point_name)
         if register.read_only:
             raise  IOError("Trying to write to a point configured read only: "+point_name)
@@ -122,7 +121,7 @@ class Interface(BaseInterface):
                                           "set_" + register.point_name)
         so_set_point(value)
 
-    def scrape_all(self):
+    def _scrape_all(self):
         result = {}
         read_registers = self.get_registers_by_type("byte", True)
         write_registers = self.get_registers_by_type("byte", False)
