750a8aec69a5b1e31ebd8ae9b498b6f49b83bc3b
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-08-21 20:46:02 -0700

    Create's 4 instances with a master driver and forwarder on one.

--- scripts/4platforms/create.py
+++ scripts/4platforms/create.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import subprocess
 from subprocess import Popen
 import os
@@ -6,6 +7,9 @@ import psutil
 import shutil
 import sys
 import tempfile
+
+
+vc_address = None
 
 
 def setup_platform_1(env, home):
@@ -48,25 +52,7 @@ def setup_platform_1(env, home):
     ]
 
     for i in range(len(agents)):
-        cfgfile = "/tmp/config"
-
-        with open(cfgfile, 'w') as cfgout:
-            json.dump(configs[i], cfgout)
-
-        cmd = ["volttron-pkg", "package", agents[i]]
-
-        output = subprocess.check_output(cmd)
-        filename = output.split(":")[1].strip()
-
-        cmd = ["volttron-pkg", "configure", filename, cfgfile]
-        output = subprocess.check_output(cmd)
-
-        cmd = ["volttron-ctl", "install", filename, "--tag", tags[i],
-               "--vip-identity", vip[i]]
-        output = subprocess.check_output(cmd, env=env)
-
-        cmd = ["volttron-ctl", "start", "--tag", tags[i]]
-        output = subprocess.check_output(cmd, env=env)
+        install_agent(env, agents[i], configs[i], tags[i], vip[i])
 
 
 def setup_platform_2(env, home):
@@ -89,28 +75,104 @@ def setup_platform_2(env, home):
     ]
 
     for i in range(len(agents)):
-        cfgfile = "/tmp/config"
+        install_agent(env, agents[i], configs[i], tags[i], vip[i])
 
-        with open(cfgfile, 'w') as cfgout:
-            json.dump(configs[i], cfgout)
 
-        cmd = ["volttron-pkg", "package", agents[i]]
+def setup_platform_3(env, home):
+    """ Setup the third platform.
 
-        output = subprocess.check_output(cmd)
-        filename = output.split(":")[1].strip()
+    This platform will hold the following agents with identities in ()
 
-        cmd = ["volttron-pkg", "configure", filename, cfgfile]
-        output = subprocess.check_output(cmd)
+    - VOLTTRON Central Platform (platform.agent)
+    - A forwarding agent that should forward to the volttron central instance.
 
-        cmd = ["volttron-ctl", "install", filename, "--tag", tags[i],
-               "--vip-identity", vip[i]]
-        output = subprocess.check_output(cmd, env=env)
+    This platform should be automatically registered with volttron-central on
+    the startup of the volttron central platform agent.
+    """
 
-        cmd = ["volttron-ctl", "start", "--tag", tags[i]]
-        output = subprocess.check_output(cmd, env=env)
+    agents = ["services/core/{}".format(p) for p in ["VolttronCentralPlatform",
+                                                     "ForwardHistorian"]]
+    tags = ['vcp', 'forwarder']
+    vip = ['platform.agent', 'forward.historian']
+
+    configs = [
+        {},
+        {'destination-vip': get_destination_address(vc_address)}
+    ]
+
+    for i in range(len(agents)):
+        install_agent(env, agents[i], configs[i], tags[i], vip[i])
+
+    install_fake_masterdriver(env, home)
+
+
+def install_fake_masterdriver(env, home):
+    lines = [
+        "Point Name, Volttron Point Name, Units, Units Details, Writable, Starting Value, Type, Notes",
+        "Heartbeat, Heartbeat, On / Off, On / Off, TRUE, 0, boolean, Point for heartbeat toggle",
+        "EKG, EKG, waveform, waveform, TRUE, 1, float, Sine wave for baseline output"]
+
+    registrycfg = os.path.join(os.path.dirname(__file__), "registry.csv")
+    registrycfg = os.path.abspath(registrycfg)
+    with open(registrycfg, 'w') as fout:
+        for line in lines:
+            fout.write("{}\n".format(line))
+
+    drivercfg = {
+        "driver_config": {},
+        "campus": "MyFakeCampus",
+        "building": "SomeBuilding",
+        "unit": "MyFakeDevice",
+        "driver_type": "fakedriver",
+        "registry_config": registrycfg,
+        "interval": 5,
+        "timezone": "US/Pacific",
+        "heart_beat_point": "Heartbeat"
+    }
+    driverfile = os.path.join(os.path.dirname(__file__), "driver.cfg")
+    driverfile = os.path.abspath(driverfile)
+    with open(driverfile, 'w') as fout:
+        json.dump(drivercfg, fout, indent=4)
+
+    masterdrivercfg = {
+        "agentid": "master_driver",
+        "driver_config_list": [driverfile]
+    }
+
+    masterdriver = os.path.join(os.path.dirname(__file__), "masterdriver.cfg")
+    masterdriver = os.path.abspath(masterdriver)
+    with open(masterdriver, 'w') as fout:
+        json.dump(masterdrivercfg, fout, indent=4)
+
+    install_agent(env, "services/core/MasterDriverAgent", masterdriver,
+                  "driver")
+
+
+def install_agent(env, agent_dir, cfgdict, tag, identity=None):
+    cfgfile = "/tmp/config"
+
+    with open(cfgfile, 'w') as cfgout:
+        json.dump(cfgdict, cfgout)
+
+    cmd = ["volttron-pkg", "package", agent_dir]
+
+    output = subprocess.check_output(cmd)
+    filename = output.split(":")[1].strip()
+
+    cmd = ["volttron-pkg", "configure", filename, cfgfile]
+    output = subprocess.check_output(cmd)
+
+    cmd = ["volttron-ctl", "install", filename, "--tag", tag]
+    if identity:
+        cmd.extend(["--vip-identity", identity])
+    output = subprocess.check_output(cmd, env=env)
+
+    cmd = ["volttron-ctl", "start", "--tag", tag]
+    output = subprocess.check_output(cmd, env=env)
 
 
 def main():
+    global vc_address
     vhomes = ["/tmp/v{}".format(i+1) for i in range(4)]
     envs = []
     pidfile = os.path.join(os.path.dirname(__file__), "pids")
@@ -131,7 +193,7 @@ def main():
                 cmd.append(
                     "--bind-web-address=http://127.0.0.{}:8080".format(i))
 
-            cmd.append('--instance-name="volttron-platform-{}"'.format(i))
+            cmd.append('--instance-name=volttron-platform-{}'.format(i))
 
             env = os.environ.copy()
             envs.append(env)
@@ -144,11 +206,16 @@ def main():
 
             pidout.write("{}\n".format(process.pid))
 
-    setup_functions = [setup_platform_1, setup_platform_2]
+    setup_functions = [setup_platform_1, setup_platform_2, setup_platform_3]
 
     for i in range(len(setup_functions)):
         setup_functions[i](envs[i], vhomes[i])
 
+
+def get_destination_address(vc_address):
+    resp = requests.get("{}/discovery/".format(vc_address))
+    d = resp.json()
+    return "{vip-address}?serverkey={serverkey}".format(**d)
 
 if __name__=='__main__':
 
