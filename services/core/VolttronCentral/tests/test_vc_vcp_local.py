645dd1e401b8ee6c8a9c969b3ffbb3adc48d7931
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-08-25 15:12:20 -0700

    Added tests for the output of webapi.

--- services/core/VolttronCentral/tests/test_vc_vcp_local.py
+++ services/core/VolttronCentral/tests/test_vc_vcp_local.py
@@ -53,4 +53,21 @@ def test_discovered(setup_first):
     webapi = WebAPI(wrapper.bind_web_address)
     platforms = webapi.call('list_platforms')
     assert len(platforms) == 1
+    p1 = platforms[0]
+    expected_keys = ("name", "uuid", "health")
+    for k in expected_keys:
+        assert k in p1, "Key: {} not found in platform return value".format(k)
+        assert p1[k], "Key: {} on platform was not valid".format(k)
+
+    assert wrapper.vip_address == p1['name']
+
+    health = p1["health"]
+    expected_keys = ("status", "context", "last_updated")
+    for k in expected_keys:
+        assert k in health, \
+            "Key: {} not found on heaalth return value".format(k)
+
+    assert health['last_updated']
+    assert 'GOOD' == health['status']
+    assert None is health['context']
 
