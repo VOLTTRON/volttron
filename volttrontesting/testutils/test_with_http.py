b703d8485c1aa996d3373b1d54c0710235e7a36e
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-08-25 21:50:27 -0700

    Added a skip_if function for encryption versus no encryption.

--- volttrontesting/testutils/test_with_http.py
+++ volttrontesting/testutils/test_with_http.py
@@ -1,16 +1,17 @@
 import pytest
 import requests
+import gevent
 
 from volttrontesting.utils.platformwrapper import start_wrapper_platform
-
-
-def setup_instance(wrapper):
-    if not wrapper.is_running():
-        start_wrapper_platform(wrapper, with_http=True, with_tcp=True)
+from volttrontesting.utils.skip_if import skip_if_not_encrypted
 
 
 @pytest.mark.wrapper
 def test_can_start_webserver(get_volttron_instances):
     wrapper = get_volttron_instances(1, False)
-    setup_instance(wrapper)
+
+    skip_if_not_encrypted(get_volttron_instances.param == 'encrypted')
+    start_wrapper_platform(wrapper, with_http=True, with_tcp=True)
+
+    gevent.sleep(0.5)
     assert requests.get(wrapper.discovery_address).ok
