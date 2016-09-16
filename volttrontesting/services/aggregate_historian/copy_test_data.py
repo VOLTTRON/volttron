e00241a91a6aac446ca13f2cda3c091df5b91050
schandrika <chandrika@pnnl.gov>
2016-09-16 16:06:09 -0700

    task #565 - scripts and configs for testing against a copy of production mongodb data

--- volttrontesting/services/aggregate_historian/copy_test_data.py
+++ volttrontesting/services/aggregate_historian/copy_test_data.py
@@ -5,6 +5,7 @@ except:
     raise Exception("Required: pymongo")
 from datetime import datetime
 from bson.objectid import ObjectId
+from pymongo import ReplaceOne
 
 source_params = {
             "host": "vc-db1.pnl.gov",
@@ -23,6 +24,14 @@ dest_params = {
             "authSource": "admin"
         }
 
+local_dest_params = {
+            "host": "localhost",
+            "port": 27017,
+            "database": "mongo_test",
+            "user": "test",
+            "passwd": "test"
+        }
+
 
 def connect_mongodb(connection_params):
     print ("setup mongodb")
@@ -31,6 +40,7 @@ def connect_mongodb(connection_params):
         mongo_conn_str = mongo_conn_str+ '?authSource={authSource}'
     params = connection_params
     mongo_conn_str = mongo_conn_str.format(**params)
+    print (mongo_conn_str)
     mongo_client = pymongo.MongoClient(mongo_conn_str)
     db = mongo_client[connection_params['database']]
     return db
@@ -69,14 +79,14 @@ def copy(source_params, dest_params, start_date, end_date):
     #     records.append(record)
     # print("total records {}".format(len(records)))
     # dest_db[dest_tables['topics_table']].insert_many(
-    #     records, ordered=False, bypass_document_validation=True)
-
+    #     records)
+    #
     # records = []
     # for record in source_db[source_tables['meta_table']].find():
     #     records.append(record)
     # print("total records {}".format(len(records)))
     # dest_db[dest_tables['meta_table']].insert_many(
-    #     records, ordered=False, bypass_document_validation=True)
+    #     records)
 
     # This is probably the most inefficient way of doing a copying a subset
     # of a
@@ -86,37 +96,56 @@ def copy(source_params, dest_params, start_date, end_date):
     # https://jira.mongodb.org/browse/SERVER-13201
     # Aggregation is the fastest way to get a subset of data from a collection,
     # next would be map reduce. map reduce can write output to another db
-    # but it
-    # would only generate doc of schema
-    #  id:<object id>, value:<search/mapreduce result>
+    # but it would only generate doc of schema
+    # id:<object id>, value:<search/mapreduce result>
 
+    dest_db[dest_tables['data_table']].create_index(
+        [('ts', pymongo.ASCENDING),
+         ('topic_id', pymongo.ASCENDING)],
+        unique=True, background=False)
     records = []
     i = 0
     print ("start obj:{}".format(ObjectId.from_datetime(start_date)))
     print ("end obj:{}".format(ObjectId.from_datetime(end_date)))
-    # cursor = source_db[source_tables['data_table']].find(
-    #     {'$and':
-    #         [{'_id': {'$gte': ObjectId.from_datetime(start_date)}},
-    #          {'_id': {'$lte': ObjectId.from_datetime(end_date)}}]})
     cursor = source_db[source_tables['data_table']].find(
         {'$and':
-            [{'_id': {'$gte': ObjectId('56d4db800000000000000000')}},
-             {'_id': {'$lte': ObjectId('574cd4000000000000000000')}}]})
+            [{'_id': {'$gte': ObjectId.from_datetime(start_date)}},
+             {'_id': {'$lte': ObjectId.from_datetime(end_date)}}]})
     print ("Record count from cursor {}".format(cursor.count()))
     for record in cursor:
         i += 1
-        records.append(record)
+        records.append(
+                    ReplaceOne(
+                        {'ts':record['ts'], 'topic_id':record['topic_id']},
+                        {'ts': record['ts'], 'topic_id': record['topic_id'],
+                                'value': record['value']},
+                        upsert=True))
         if i == 2000:
             print("total records {}".format(len(records)))
-            dest_db[dest_tables['data_table']].insert_many(
-            records, ordered=False, bypass_document_validation=True)
+            dest_db[dest_tables['data_table']].bulk_write(records)
             i =0
-            records =[]
+            records = []
+
+
+
+    # cursor = dest_db[dest_tables['data_table']].aggregate([
+    #     {"$group": {
+    #         "_id": {"ts": "$ts", "topic_id": "$topic_id"},
+    #         "duplicates": {"$push": "$_id"},
+    #         "count": {"$sum": 1}
+    #     }},
+    #     {"$match": {"count": {"$gt": 1}}}
+    # ])
+    # for doc in cursor:
+    #     doc.dups.shift()
+    #     dest_db[dest_tables['data_table']].remove(
+            # {"_id": {"$in":  doc.duplicates}})
 
 
 if __name__ == '__main__':
 
-    copy(source_params, dest_params, datetime.strptime('01Mar2016', '%d%b%Y'),
-         datetime.strptime('31May2016', '%d%b%Y'))
+    copy(source_params, local_dest_params, datetime.strptime('31Mar2016',
+                                                         '%d%b%Y'),
+         datetime.strptime('01May2016', '%d%b%Y'))
 
 
