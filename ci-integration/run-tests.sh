30a446b0376fd4f137295cead36d65588f7e2164
jhaack <jhaack>
2016-09-27 11:36:52 -0700

    #754 Further broke up tests

--- ci-integration/run-tests.sh
+++ ci-integration/run-tests.sh
@@ -14,12 +14,29 @@ exit_code=0
 # Break up the tests to work around the issue in #754. Breaking them up allows 
 # the files to be closed with the individual pytest processes
 
-testdirs= "docs" "examples" "scripts" "services/core" "volttron" "volttrontesting"
+# testdirs=("examples" "services/core/*" "volttron" "volttrontesting/*")
 
-for dir in testdirs
-do
-  echo $dir
-  py.test -v $dir
+#echo ${testdirs[@]}
+
+#for D in services/core/*; do
+#    if [ -d "${D}" ]; then
+#        testdirs=("#testdirs[@]}" $D)
+#    fi
+#done
+#echo "HIIIIIIIIIIIIIII"
+#echo ${testdirs[@]}
+
+filedirs="volttrontesting/platform"
+testdirs="docs examples scripts volttron"
+splitdirs="services/core"
+
+echo "File tests"
+for dir in $filedirs; do
+  echo "File test for dir: $dir"
+  for testfile in $dir/*.py; do
+    echo "Using testfile: $testfile"
+    pytest -v $testfile
+
   tmp_code=$?
   exit_code=$tmp_code
   echo $exit_code
@@ -31,6 +48,45 @@ do
       fi
     fi
   fi
+
+   done
+done
+
+for dir in $testdirs; do
+  pytest -v $testdirs
+
+  tmp_code=$?
+  exit_code=$tmp_code
+  echo $exit_code
+  if [ $tmp_code -ne 0 ]; then
+    if [ $tmp_code -ne 5 ]; then
+      if [ ${FAST_FAIL} ]; then
+        echo "Fast failing!"
+        exit $tmp_code
+      fi
+    fi
+  fi
+
+   done
+done
+
+for dir in $splitdirs; do
+
+for D in "$dir/*"; do
+    if [ -d "${D}" ]; then
+        py.test -v ${D}
+        tmp_code=$?
+        if [ $tmp_code -ne 0 ]; then
+          if [ $tmp_code -ne 5 ]; then
+            if [ ${FAST_FAIL} ]; then
+              echo "Fast failing!"
+              exit $tmp_code
+            fi
+            exit_code=$tmp_code
+          fi
+        fi
+    fi
+done
 done
 
 exit $exit_code
