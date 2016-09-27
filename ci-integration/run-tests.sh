d3e4a507e8e55b5c678b9bc17f3b35931f11d282
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-09-26 23:01:35 -0700

    Keep exit code if the code is not 0 so that it truly represents a failure

--- ci-integration/run-tests.sh
+++ ci-integration/run-tests.sh
@@ -8,20 +8,46 @@ export CI=travis
 pip install mock
 pip install pytest pytest-timeout
 
+exit_code=0
+
 # Break up the tests to work around the issue in #754. Breaking them up allows 
 # the files to be closed with the individual pytest processes
 py.test -v docs
+if [ $? != 0 ]; then
+  exit_code=$?
+fi
+
 py.test -v examples
+if [ $? != 0 ]; then
+  exit_code=$?
+fi
+
 py.test -v scripts
+if [ $? != 0 ]; then
+  exit_code=$?
+fi
+
+p
 for D in services/core/*; do
     if [ -d "${D}" ]; then
         py.test -v ${D}
+        if [ $? != 0 ]; then
+            exit_code=$?
+        fi
     fi
 done
 py.test -v volttron
+if [ $? != 0 ]; then
+  exit_code=$?
+fi
 
 for D in volttrontesting/*; do
     if [ -d "${D}" ]; then
         py.test -v ${D}
+        if [ $? != 0 ]; then
+            exit_code=$?
+        fi
     fi
 done
+
+exit $exit_code
