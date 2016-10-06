08493e5184d051802843c21e5891fcd98d13a03c
Mike Roup <michael.roup@pnnl.gov>
2016-10-06 11:31:08 -0700

    enhance simple failover tests

--- services/core/FailoverAgent/tests/test_simple_failover.py
+++ services/core/FailoverAgent/tests/test_simple_failover.py
@@ -29,7 +29,7 @@ simple_secondary_config = {
 SLEEP_TIME = 3
 
 uuid_primary = None
-
+uuid_secondary = None
 
 def all_agents_running(instance):
     agents = instance.list_agents()
@@ -38,10 +38,11 @@ def all_agents_running(instance):
 
 
 @pytest.fixture
-def simple_failover(get_volttron_instances):
+def simple_failover(request, get_volttron_instances):
     global simple_primary_config
     global simple_secondary_config
     global uuid_primary
+    global uuid_secondary
 
     primary, secondary = get_volttron_instances(2)
 
@@ -52,9 +53,9 @@ def simple_failover(get_volttron_instances):
     secondary.allow_all_connections()
 
     # configure primary
-    uuid = primary.install_agent(agent_dir="examples/ListenerAgent",
-                                 vip_identity="listener",
-                                 start=False)
+    listener_primary = primary.install_agent(agent_dir="examples/ListenerAgent",
+                                             vip_identity="listener",
+                                             start=False)
 
     simple_primary_config["remote_vip"] = secondary.vip_address
     simple_primary_config["remote_serverkey"] = secondary.serverkey
@@ -62,19 +63,29 @@ def simple_failover(get_volttron_instances):
                                          config_file=simple_primary_config)
 
     # configure secondary
-    uuid = secondary.install_agent(agent_dir="examples/ListenerAgent",
-                                   vip_identity="listener",
-                                   start=False)
+    listener_secondary = secondary.install_agent(agent_dir="examples/ListenerAgent",
+                                                 vip_identity="listener",
+                                                 start=False)
 
     simple_secondary_config["remote_vip"] = primary.vip_address
     simple_secondary_config["remote_serverkey"] = primary.serverkey
-    secondary.install_agent(agent_dir="services/core/FailoverAgent",
-                            config_file=simple_secondary_config)
+    uuid_secondary = secondary.install_agent(agent_dir="services/core/FailoverAgent",
+                                             config_file=simple_secondary_config)
 
     gevent.sleep(SLEEP_TIME)
     assert all_agents_running(primary)
     assert not all_agents_running(secondary)
 
+    def cleanup():
+        primary.stop_agent(uuid_primary)
+        primary.stop_agent(listener_primary)
+        primary.shutdown_platform()
+
+        secondary.stop_agent(uuid_secondary)
+        secondary.stop_agent(listener_secondary)
+        secondary.shutdown_platform()
+
+    request.addfinalizer(cleanup)
     return primary, secondary
 
 
@@ -95,9 +106,41 @@ def test_simple_failover(simple_failover):
     assert all_agents_running(primary)
     assert not all_agents_running(secondary)
 
-def test_secondary_on_primary_crash(simple_failover):
+
+def test_primary_on_secondary_crash(simple_failover):
+    global uuid_secondary
     primary, secondary = simple_failover
 
+    secondary.skip_cleanup = True
+    secondary.shutdown_platform()
+    gevent.sleep(SLEEP_TIME)
+    assert all_agents_running(primary)
+
+    secondary.startup_platform(secondary.vip_address, encrypt=secondary.encrypt)
+    secondary.start_agent(uuid_secondary)
+
+    gevent.sleep(SLEEP_TIME)
+    assert all_agents_running(primary)
+    assert not all_agents_running(secondary)
+    secondary.skip_cleanup = False
+
+
+def test_secondary_on_primary_crash(simple_failover):
+    global uuid_primary
+    primary, secondary = simple_failover
+
+    primary.skip_cleanup = True
     primary.shutdown_platform()
     gevent.sleep(SLEEP_TIME)
     assert all_agents_running(secondary)
+
+    primary.startup_platform(primary.vip_address, encrypt=primary.encrypt)
+
+    # primary.startup_platform(vip_address, **args)
+    primary.start_agent(uuid_primary)
+
+    gevent.sleep(SLEEP_TIME)
+    assert all_agents_running(primary)
+    assert not all_agents_running(secondary)
+
+    primary.skip_cleanup = False
