3ad8e94b9fe58a00cc09941059c321fb7dbc092e
schandrika <chandrika@pnnl.gov>
2016-10-27 12:20:08 -0700

    task #991 added create statements for new tables used by historian

--- services/core/SQLHistorian/sqlite-create.sql
+++ services/core/SQLHistorian/sqlite-create.sql
@@ -9,3 +9,12 @@ CREATE INDEX IF NOT EXISTS data_idx ON data (ts ASC);
 CREATE TABLE IF NOT EXISTS topics (topic_id INTEGER PRIMARY KEY, 
                                  topic_name TEXT NOT NULL,
                                  UNIQUE(topic_name));
+
+CREATE TABLE IF NOT EXISTS meta
+    (topic_id INTEGER PRIMARY KEY,
+     metadata TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS volttron_table_definitions(
+    table_id TEXT PRIMARY KEY,
+    table_name TEXT NOT NULL,
+    table_prefix TEXT);
