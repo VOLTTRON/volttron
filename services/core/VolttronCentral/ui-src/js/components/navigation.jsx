68e59a1667450fc9475638ccee43f64d322ea516
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-10-27 13:10:22 -0400

    Add OpenADR tab

--- services/core/VolttronCentral/ui-src/js/components/navigation.jsx
+++ services/core/VolttronCentral/ui-src/js/components/navigation.jsx
@@ -25,7 +25,7 @@ var Navigation = React.createClass({
         var navItems;
 
         if (this.state.loggedIn) {
-            navItems = ['Dashboard', 'Platforms', 'Charts'].map(function (navItem) {
+            navItems = ['Dashboard', 'Platforms', 'Charts', 'OADR'].map(function (navItem) {
                 var route = navItem.toLowerCase();
 
                 return (
