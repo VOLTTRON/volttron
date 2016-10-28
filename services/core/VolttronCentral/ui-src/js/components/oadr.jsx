1279928bff2bf0b7520e0c481d1e5000bd5c3bbf
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-10-27 23:38:40 -0400

    Formatting niceties.

--- services/core/VolttronCentral/ui-src/js/components/oadr.jsx
+++ services/core/VolttronCentral/ui-src/js/components/oadr.jsx
@@ -12,14 +12,20 @@ var StatusForm = require('../components/status-indicator');
 var DeregisterPlatformConfirmation = require('../components/deregister-platform-confirmation');
 
 var OADR = React.createClass({
+    getInitialState: function () {
+        return getStateFromStores();
+    },
 
     render: function(){
     return (
     <div className="view">
+                <div className="absolute_anchor">
     <h2>Open ADR</h2>
     This space for: listing each VEN, with time since last contact,
-    reports, and events. 
-    </div>);
+    reports, and events.
+      </div>
+    </div>
+    );
     }
 });
 
