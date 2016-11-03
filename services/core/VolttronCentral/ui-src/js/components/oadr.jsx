a53e11ed4b795fcd073c32c07cae3d8241a1309a
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-11-02 23:25:24 -0400

    Comments on how to proceed.

--- services/core/VolttronCentral/ui-src/js/components/oadr.jsx
+++ services/core/VolttronCentral/ui-src/js/components/oadr.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-
+var $ = require('jquery');
 var React = require('react');
 var Router = require('react-router');
 
@@ -19,6 +19,11 @@ var DeregisterPlatformConfirmation = require('../components/deregister-platform-
 // 2. Each VEN and when it last had contact with it. 
 // 3. The latest oadrReports on from all parties.
 // 4. Whether any Events are active. 
+//
+// The entry point into the Oadr agent is with /oadr/.
+// IT already has a method there to return its state.
+// What is missing is enough React smarts to display it.
+// platform-charg-action-createros.js has a clue as to how to do this.
 
 var OADR = React.createClass({
     render: function(){
