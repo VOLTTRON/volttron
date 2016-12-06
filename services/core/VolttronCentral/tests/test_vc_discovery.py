bbea2614e275cf77b6d13eb0c234d89fa2741040
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-12-06 13:02:39 -0800

    Extend the timeout period for discovery

--- services/core/VolttronCentral/tests/test_vc_discovery.py
+++ services/core/VolttronCentral/tests/test_vc_discovery.py
@@ -71,6 +71,7 @@ def both_with_vc_vcp(request):
 
 
 @pytest.mark.vc
+@pytest.mark.timeout(300)
 def test_autoregister_external(vc_vcp_platforms):
     gevent.sleep(15)
     vc, vcp = vc_vcp_platforms
@@ -88,6 +89,7 @@ def test_autoregister_external(vc_vcp_platforms):
 
 
 @pytest.mark.vc
+@pytest.mark.timeout(300)
 def test_autoregister_local(both_with_vc_vcp):
     gevent.sleep(15)
 
