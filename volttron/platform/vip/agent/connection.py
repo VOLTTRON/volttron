ea6b6591db804ad97aba7b809ee0efbe00b7a8b5
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-12-10 11:34:42 -0800

    Brought some of the changes that were made to connection and agent object into this branch.

--- volttron/platform/vip/agent/connection.py
+++ volttron/platform/vip/agent/connection.py
@@ -82,10 +82,8 @@ class Connection(object):
     """
     def __init__(self, address, peer=None, publickey=None,
                  secretkey=None, serverkey=None, volttron_home=None, **kwargs):
-        cnstring = "Connecting to address: {} using publickey credential: {} " \
-                   "serverkey: {}"
-
-        _log.debug(cnstring.format(address, publickey, serverkey))
+        _log.debug("Connection: {}, {}, {}, {}, {}"
+                   .format(address, peer, publickey, secretkey, serverkey))
         self._address = address
         self._peer = peer
         self._serverkey = None
@@ -102,6 +100,7 @@ class Connection(object):
             parsed = urlparse.urlparse(address)
             if parsed.scheme == 'tcp':
                 qs = urlparse.parse_qs(parsed.query)
+                _log.debug('QS IS: {}'.format(qs))
                 if 'serverkey' in qs:
                     self._serverkey = qs.get('serverkey')
                 else:
@@ -166,11 +165,16 @@ class Connection(object):
     def server(self):
         if self._greenlet is None:
             _log.debug('Spawning greenlet')
-
             event = gevent.event.Event()
             self._greenlet = gevent.spawn(self._server.core.run, event)
-            if not event.wait(timeout=DEFAULT_TIMEOUT):
-                raise RuntimeError("Unable to connect to target platform")
+
+            try:
+                with gevent.Timeout(DEFAULT_TIMEOUT):
+                    event.wait()
+            except gevent.Timeout:
+                self.kill()
+                self._greenlet = None
+                raise
 
             self._connected_since = get_aware_utc_now()
             if self.peer not in self._server.vip.peerlist().get(timeout=2):
@@ -232,8 +236,7 @@ class Connection(object):
         raise ValueError("peer not specified on class or as method argument.")
 
     def kill(self, *args, **kwargs):
-        try:
-            if self._greenlet is not None:
-                self._greenlet.kill(*args, **kwargs)
-        finally:
+        if self._greenlet is not None:
+            self._greenlet.kill(*args, **kwargs)
+            del(self._greenlet)
             self._greenlet = None
