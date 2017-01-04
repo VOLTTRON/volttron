588342d16e89a084664b957e07be86f8c7d1accb
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2017-01-04 07:27:55 -0500

    Component for displaying global scheduler's view of the world.

---
+++ services/core/VolttronCentral/ui-src/js/components/gs.jsx
@@ -0,0 +1,79 @@
+'use strict';
+
+var $ = require('jquery');
+var React = require('react');
+//var Router = require('react-router');
+var xhr = require('../lib/xhr');
+
+var modalActionCreators = require('../action-creators/modal-action-creators');
+var statusIndicatorActionCreators = require('../action-creators/status-indicator-action-creators');
+var platformsStore = require('../stores/platforms-store');
+var RegisterPlatformForm = require('../components/register-platform-form');
+var StatusForm = require('../components/status-indicator');
+var DeregisterPlatformConfirmation = require('../components/deregister-platform-confirmation');
+//
+// TODO: 
+//
+// The interface here should report as follows:
+//
+// 1. That the OpenADR agent is up.
+// 2. Each VEN and when it last had contact with it. 
+// 3. The latest oadrReports on from all parties.
+// 4. Whether any Events are active. 
+//
+// The entry point into the Oadr agent is with /oadr/.
+// IT already has a method there to return its state.
+// What is missing is enough React smarts to display it.
+// platform-charg-action-createros.js has a clue as to how to do this.
+
+function gs() {
+    var page = {};
+    page.promise = new xhr.Request({
+        method: 'GET',
+        url: '/gs/status',
+	contentType:'application/json',
+	timeout:600000,
+    }).finally(function() {
+	page.completed = Date.now();
+    }).
+	then(function(response) {
+	    page.response = response;
+	    const element = (
+		    <div>
+      <h1>Global Scheduler</h1> {response.content}
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+	React.render(
+    	element,
+    	document.getElementById('gs')
+  	);	
+
+	})
+        .catch(xhr.Error, function (error) {
+            page.error = error;
+	});
+    }
+    
+
+
+var GS = React.createClass({
+    render: function(){
+    flame();
+    return (
+    <div className="view">
+                <div className="absolute_anchor">
+    <h2>Global scheduler status</h2>
+    This space for: listing each VEN, with time since last contact,
+    reports, and events.
+     {new Date().toLocaleTimeString()}
+Test <span id="gs" >TEST</span>
+    
+      </div>
+      
+    </div>
+    );
+    }
+});
+
+module.exports = GS;
