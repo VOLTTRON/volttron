e2a6f1f418a0a415f3020bec1c02d9bc90a6be08
Kyle Monson <kyle.monson@pnnl.gov>
2017-02-06 13:34:01 -0800

    Generalized external data agent.

--- services/core/ExternalJSONData/agent/agent.py
+++ services/core/ExternalJSONData/agent/agent.py
@@ -59,12 +59,13 @@
 
 from __future__ import absolute_import
 
-import datetime
 import logging
 import sys
+import csv
+from ast import literal_eval
+from StringIO import StringIO
 import requests
 from requests.auth import HTTPBasicAuth
-import json
 from volttron.platform.messaging.utils import Topic
 
 from volttron.platform.vip.agent import Agent, Core
@@ -83,7 +84,7 @@ __version__ = '1.0'
 PUBLISH_TOPIC = Topic("{base}//{source}//{key}")
 
 
-def json_agent(config_path, **kwargs):
+def external_data_agent(config_path, **kwargs):
     config = utils.load_config(config_path)
 
     interval = config.get('interval', 300)
@@ -95,27 +96,37 @@ def json_agent(config_path, **kwargs):
                           key=None)
     sources = config.get("sources", [])
 
-    return ExternalJSON(interval, default_user, default_password, sources=sources, topic=topic, **kwargs)
+    return ExternalData(interval, default_user, default_password, sources=sources, topic=topic, **kwargs)
 
 
-
-class ExternalJSON(Agent):
+class ExternalData(Agent):
     """Gathers and publishes JSON data available via a web api.
     """
-    def __init__(self, interval, default_user, default_password, sources=[], topic=PUBLISH_TOPIC, **kwargs):
-        super(ExternalJSON, self).__init__(**kwargs)
+    def __init__(self, interval, default_user, default_password, sources, global_topic_prefix, **kwargs):
+        super(ExternalData, self).__init__(**kwargs)
         self.interval = interval
         self.default_user = default_user
         self.default_password = default_password
-        self.sources = sources
-        self.topic = topic
 
-        self._validate_sources()
+        self.periodic_greenlet = None
 
-    def _validate_sources(self):
+        self.default_config = {"interval": interval,
+                               "global_topic_prefix": global_topic_prefix,
+                               "default_user": default_user,
+                               "default_password": default_password}
+
+        self.default_config["sources"] = self.sources = self._validate_sources(sources, global_topic_prefix)
+
+        self.vip.config.set_default("config", self.default_config)
+        self.vip.config.subscribe(self._configure, actions=["NEW", "UPDATE"], pattern="config")
+
+    def _validate_sources(self, old_sources, global_topic_prefix):
         # Simple validation of sources
+        topic = PUBLISH_TOPIC(base=global_topic_prefix,
+                              source=None,
+                              key=None)
         new_sources = []
-        for source in self.sources:
+        for source in old_sources:
             url = source.get("url")
             key_column = source.get("key")
             source_topic = source.get("topic", "")
@@ -125,41 +136,59 @@ class ExternalJSON(Agent):
                 continue
 
             if key_column is None:
-                topic = self.topic(source=source_topic, key="")
-                if not topic:
+                topic_str = topic(source=source_topic, key="")
+                if not topic_str:
                     _log.error("{url} configured to publish without topic. Removing source".format(url=url))
                     continue
 
             new_sources.append(source)
 
-        self.sources = new_sources
+        return new_sources
 
-    @Core.receiver('onstart')
-    def onstart(self,sender, **kwargs):
-        self.core.periodic(self.interval,self.publish_data)
+    def _configure(self, config_name, action, contents):
+        config = self.default_config.copy()
+        config.update(contents)
 
-    def publish_data(self):
+        _log.debug("Configuring CSV agent")
 
-        now = utils.get_aware_utc_now()
-        now = utils.format_timestamp(now)
-        headers = {
-            headers_mod.DATE: now,
-            headers_mod.TIMESTAMP: now
-        }
+        global_topic_prefix = config.get('global_topic_prefix', "")
+
+        self.sources = self._validate_sources(config.get("sources", []), global_topic_prefix)
+
+        self.topic = PUBLISH_TOPIC(base=global_topic_prefix,
+                                  source=None,
+                                  key=None)
+
+        self.default_user = config.get('default_user')
+        self.default_password = config.get('default_password')
+
+        try:
+            interval = float(config.get("interval", 300.0))
+        except ValueError:
+            _log.error("Error setting scrape interval, reverting to default of 300 seconds")
+            interval = 300.0
+
+        if self.periodic_greenlet is not None:
+            self.periodic_greenlet.kill()
+
+        self.periodic_greenlet = self.core.periodic(interval, self._publish_data)
+
+
+    def _publish_data(self):
         for source in self.sources:
+            now = utils.get_aware_utc_now()
+            now = utils.format_timestamp(now)
+            headers = {
+                headers_mod.DATE: now,
+                headers_mod.TIMESTAMP: now
+            }
+
             url = source.get("url")
             params = source.get("params")
-            keys = source.get("key", [])
             source_topic = source.get("topic", "")
             user = source.get("user", self.default_user)
             password = source.get("password", self.default_password)
-            path = source.get("path", [])
-
-            if isinstance(path, str):
-                path = [path]
-
-            if isinstance(keys, str):
-                keys = [keys]
+            source_type = source.get("type", "raw")
 
             kwargs = {"params":params}
 
@@ -169,58 +198,147 @@ class ExternalJSON(Agent):
             try:
                 r = requests.get(url, **kwargs)
                 r.raise_for_status()
-                data = r.json()
-            except (requests.exceptions.HTTPError, ValueError) as e:
+            except requests.exceptions.HTTPError as e:
                 _log.error("Failure to read from source {url} {reason}".format(url=url, reason=str(e)))
                 continue
 
-            try:
-                for path_name in path:
-                    data = data[path_name]
-            except (KeyError, IndexError) as e:
-                _log.error("Failure to read from source {url} {reason}".format(url=url, reason=str(e)))
-                continue
+            if source_type.lower() == "json":
+                self._handle_json(headers, r, url, source_topic, source)
+            elif source_type.lower() == "csv":
+                self._handle_csv(headers, r, url, source_topic, source)
+            elif source_type.lower() == "raw":
+                self._handle_raw(headers, r, url, source_topic, source)
 
-            if isinstance(data, list) and keys:
-                dropped_rows = False
-                for row in data:
-                    missing_key = False
-                    key_value = row
 
+    def _handle_json(self, headers, request, url, source_topic, source_params):
+        keys = source_params.get("key", [])
+        path = source_params.get("path", [])
+
+        if isinstance(path, str):
+            path = [path]
+
+        if isinstance(keys, str):
+            keys = [keys]
+
+        try:
+            data = request.json()
+        except ValueError as e:
+            _log.error("Failure to read from source {url} {reason}".format(url=url, reason=str(e)))
+            return
+
+        try:
+            for path_name in path:
+                data = data[path_name]
+        except (KeyError, IndexError, TypeError) as e:
+            _log.error("Failure to read from source {url} {reason}".format(url=url, reason=str(e)))
+            return
+
+        if isinstance(data, list) and keys:
+            dropped_rows = False
+            for row in data:
+                missing_key = False
+                key_value = row
+
+                try:
+                    for key_name in keys:
+                        key_value = key_value[key_name]
+                except (KeyError, IndexError, TypeError) as e:
+                    missing_key = dropped_rows = True
+
+                if missing_key:
+                    continue
+
+                if not isinstance(key_value, (str, unicode)) or not key_value:
+                    dropped_rows = True
+                    continue
+
+                topic = self.topic(source=source_topic, key=key_value)
+
+                if not topic:
+                    dropped_rows = True
+                    continue
+
+                self.vip.pubsub.publish(peer='pubsub', topic=topic, message=row, headers=headers).get(
+                    timeout=10.0)
+            if dropped_rows:
+                _log.error("At least one key missing from the data from source {url}".format(url=url))
+
+        else:
+            # The topic has already been verified as legit by now.
+            topic = self.topic(source=source_topic, key="")
+            self.vip.pubsub.publish(peer='pubsub', topic=topic, message=data, headers=headers).get(timeout=10.0)
+
+    def _handle_csv(self, headers, request, url, source_topic, source_params):
+        key_column = source_params.get("key", "")
+        flatten = source_params.get("flatten", False)
+        parse_columns = source_params.get("parse", [])
+
+        file_obj = StringIO(request.content)
+
+        if flatten:
+            to_flatten = csv.reader(file_obj)
+            row_data = {}
+            csv_rows_skipped = False
+            for row in to_flatten:
+                try:
+                    row_data[row[0]] = row[1]
+                except IndexError:
+                    csv_rows_skipped = True
+            if csv_rows_skipped:
+                _log.warning("Skipped incomplete flatten rows in {url}".format(url=url))
+
+            csv_data = [row_data]
+        else:
+            csv_data = csv.DictReader(file_obj)
+
+        if parse_columns:
+            new_csv_data = []
+            for row in csv_data:
+                for parse_column in parse_columns:
                     try:
-                        for key_name in keys:
-                            key_value = key_value[key_name]
-                    except (KeyError, IndexError, TypeError) as e:
-                        missing_key = dropped_rows = True
+                        value_string = row[parse_column]
+                        value = literal_eval(value_string)
+                        row[parse_column] = value
+                    except KeyError:
+                        pass
+                    except StandardError:
+                        if value_string == "":
+                            row[parse_column] = None
+                new_csv_data.append(row)
 
-                    if missing_key:
-                        continue
+            csv_data = new_csv_data
 
-                    if not isinstance(key_value, (str, unicode)) or not key_value:
-                        dropped_rows = True
-                        continue
-
-                    topic = self.topic(source=source_topic, key=key_value)
-
-                    if not topic:
-                        dropped_rows = True
-                        continue
-
+        if key_column:
+            dropped_rows = False
+            for row in csv_data:
+                key = row.pop(key_column, "")
+                topic = self.topic(source=source_topic, key=key)
+                if topic:
                     self.vip.pubsub.publish(peer='pubsub', topic=topic, message=row, headers=headers).get(
                         timeout=10.0)
-                if dropped_rows:
-                    _log.error("At least one key missing from the data from source {url}".format(url=url))
+                else:
+                    dropped_rows = True
+            if dropped_rows:
+                _log.warning("Skipped rows with invalid topic from {url}".format(url=url))
 
+        else:
+            # The topic has already been verified as legit by now.
+            topic = self.topic(source=source_topic, key="")
+            if flatten:
+                data = csv_data[0]
             else:
-                # The topic has already been verified as legit by now.
-                topic = self.topic(source=source_topic, key="")
-                self.vip.pubsub.publish(peer='pubsub', topic=topic, message=data, headers=headers).get(timeout=10.0)
+                data = csv_data
+            self.vip.pubsub.publish(peer='pubsub', topic=topic, message=data, headers=headers).get(
+                timeout=10.0)
 
+    def _handle_raw(self, headers, request, url, source_topic, source_params):
+        topic = self.topic(source=source_topic, key="")
+        self.vip.pubsub.publish(peer='pubsub', topic=topic, message=request.content, headers=headers).get(timeout=10.0)
 
 def main(argv=sys.argv):
     '''Main method called by the eggsecutable.'''
     try:
-        utils.vip_main(json_agent)
+        utils.vip_main(external_data_agent)
     except Exception as e:
         _log.exception('unhandled exception')
 
