0e59aaa03ed4737ff106b5b79c48007e15d7f3c3
Craig Allwardt <craig.allwardt@pnnl.gov>
2017-02-24 16:40:23 -0800

    Removed the encrypted param because we always encrypt now.

--- volttrontesting/fixtures/volttron_platform_fixtures.py
+++ volttrontesting/fixtures/volttron_platform_fixtures.py
@@ -137,8 +137,7 @@ def volttron_instance2_web(request):
 # Generic fixtures. Ideally we want to use the below instead of
 # Use this fixture when you want a single instance of volttron platform for
 # test
-@pytest.fixture(scope="module",
-                params=['encrypted'])
+@pytest.fixture(scope="module")
 def volttron_instance(request):
     """Fixture that returns a single instance of volttron platform for testing
 
@@ -162,8 +161,7 @@ def volttron_instance(request):
 # Usage example:
 # def test_function_that_uses_n_instances(request, get_volttron_instances):
 #     instances = get_volttron_instances(3)
-@pytest.fixture(scope="module",
-                params=['encrypted'])
+@pytest.fixture(scope="module")
 def get_volttron_instances(request):
     """ Fixture to get more than 1 volttron instance for test
     Use this fixture to get more than 1 volttron instance for test. This
