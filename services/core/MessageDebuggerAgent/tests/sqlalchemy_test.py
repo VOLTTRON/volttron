a87ee126601902be21c37a3914fc602e4be8c8b0
rob-calvert <rob@kisensum.com>
2017-03-24 13:35:11 -0700

    sqlalchemy_test.py

--- services/core/MessageDebuggerAgent/tests/sqlalchemy_test.py
+++ services/core/MessageDebuggerAgent/tests/sqlalchemy_test.py
@@ -7,15 +7,21 @@ ORMBase = declarative_base()
 
 
 def sqlite_helloworld():
-    path = '$VOLTTRON_HOME/data/test_db.sqlite'
+    import os
+    path = os.path.expandvars('$VOLTTRON_HOME/data/test_db.sqlite')
     engine = create_engine('sqlite:///' + path).connect()
     ORMBase.metadata.create_all(engine)
     db_session = sessionmaker(bind=engine)()
+    print 'db_session = {}'.format(db_session)
 
 
-class DebugMessage(ORMBase):
+class TestTable(ORMBase):
 
     __tablename__ = 'TestTable'
 
     rowid = Column(Integer, primary_key=True)
     another_field = Column(String)
+
+
+if __name__ == '__main__':
+    sqlite_helloworld()
