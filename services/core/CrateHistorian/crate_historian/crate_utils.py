15e10748e6f041fcfe7201ea63b5aed4088b15f6
Craig Allwardt <craig.allwardt@pnnl.gov>
2017-04-04 20:01:15 -0700

    Fixes #1274 by serializing the array and/dict if necessary to allow the crate db to handle it for us. Removed "raw" as its not necessary any longer.

--- services/core/CrateHistorian/crate_historian/crate_utils.py
+++ services/core/CrateHistorian/crate_historian/crate_utils.py
@@ -10,14 +10,16 @@ def insert_topic_query(schema):
 
 
 def insert_data_query(schema):
-    query = """INSERT INTO {schema}.data
-              (ts, topic, source, string_value, meta)
-              VALUES(?,?,?,?,?)
-              on duplicate key update
-                source = source,
-                string_value = string_value,
-                meta = meta
-              """.format(schema=schema)
+    query = """
+INSERT INTO {schema}.data
+(ts, topic, source, string_value, meta)
+VALUES(?,?,?,?,?)
+on duplicate key update
+source = source,
+string_value = string_value,
+meta = meta
+""".format(schema=schema)
+
     return query.replace("\n", "")
 
 
@@ -45,30 +47,30 @@ def create_schema(connection, schema="historian"):
     create_queries = [
         # Insert will attempt to put in double column then object column
         # finally string column.
-        """
-        CREATE TABLE IF NOT EXISTS {schema}.data(
-            source string,
-            topic string primary key,
-            ts timestamp NOT NULL primary key,
-            string_value string,
-            meta object,
-            -- Dynamically generated column that will either be a double
-            -- or a NULL based on the value in the string_value column.
-            double_value as  try_cast(string_value as double),
-            -- Full texted search index on the topic string
-            INDEX topic_ft using fulltext (topic),
-            -- must be part of primary key because of partitioning on monthly
-            -- table and the column is set on the table.
-            month as date_trunc('month', ts) primary key)
-        partitioned by (month)
-        CLUSTERED INTO 6 SHARDS
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS {schema}.topic(
-            topic string PRIMARY KEY
-        )
-        CLUSTERED INTO 3 SHARDS
-        """
+"""
+CREATE TABLE IF NOT EXISTS {schema}.data(
+    source string,
+    topic string primary key,
+    ts timestamp NOT NULL primary key,
+    string_value string,
+    meta object,
+    -- Dynamically generated column that will either be a double
+    -- or a NULL based on the value in the string_value column.
+    double_value as  try_cast(string_value as double),
+    -- Full texted search index on the topic string
+    INDEX topic_ft using fulltext (topic),
+    -- must be part of primary key because of partitioning on monthly
+    -- table and the column is set on the table.
+    month as date_trunc('month', ts) primary key)
+partitioned by (month)
+CLUSTERED INTO 6 SHARDS
+""".replace("\n", ""),
+"""
+CREATE TABLE IF NOT EXISTS {schema}.topic(
+    topic string PRIMARY KEY
+)
+CLUSTERED INTO 3 SHARDS
+""".replace("\n", "")
     ]
     try:
         cursor = connection.cursor()
