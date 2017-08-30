68d28711349c634eec2dea6c15c522bfbf52c120
Mike Roup <michael.roup@pnnl.gov>
2017-08-30 13:03:41 -0700

    fail startup when misconfigured, log log config

--- volttron/platform/main.py
+++ volttron/platform/main.py
@@ -433,6 +433,7 @@ def start_volttron_process(opts):
     level = max(1, opts.verboseness)
     if opts.monitor and level > logging.INFO:
         level = logging.INFO
+
     if opts.log is None:
         log_to_file(sys.stderr, level)
     elif opts.log == '-':
@@ -441,10 +442,17 @@ def start_volttron_process(opts):
         log_to_file(opts.log, level, handler_class=handlers.WatchedFileHandler)
     else:
         log_to_file(None, 100, handler_class=lambda x: logging.NullHandler())
+
     if opts.log_config:
+        with open(opts.log_config, 'r') as f:
+            for line in f.readlines():
+                _log.info(line.rstrip())
+
         error = configure_logging(opts.log_config)
+
         if error:
-            parser.error('{}: {}'.format(*error))
+            _log.error('{}: {}'.format(*error))
+            sys.exit(1)
 
     opts.publish_address = config.expandall(opts.publish_address)
     opts.subscribe_address = config.expandall(opts.subscribe_address)
@@ -468,11 +476,12 @@ def start_volttron_process(opts):
         opts.volttron_central_address = config.expandall(
             opts.volttron_central_address)
     opts.volttron_central_serverkey = opts.volttron_central_serverkey
+
+    # Log configuration options
     if getattr(opts, 'show_config', False):
-        print('volttron version: {}'.format(__version__))
+        _log.info('volttron version: {}'.format(__version__))
         for name, value in sorted(vars(opts).iteritems()):
-            print(name, repr(value))
-        return
+            _log.info("%s: %s" % (name, str(repr(value))))
 
     # Increase open files resource limit to max or 8192 if unlimited
     try:
@@ -680,7 +689,7 @@ def main(argv=sys.argv):
     )
     parser.add_argument(
         '-c', '--config', metavar='FILE', action='parse_config',
-        ignore_unknown=True, sections=[None, 'volttron'],
+        ignore_unknown=False, sections=[None, 'volttron'],
         help='read configuration from FILE')
     parser.add_argument(
         '-l', '--log', metavar='FILE', default=None,
