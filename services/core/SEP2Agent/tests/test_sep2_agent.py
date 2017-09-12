a69227ba806a26d0d553b965f3e40f8b429ea2b9
James Sheridan <jafsheridan@gmail.com>
2017-09-12 12:21:28 -0700

    Support configuration of a broader set of SEP2 fields

--- services/core/SEP2Agent/tests/test_sep2_agent.py
+++ services/core/SEP2Agent/tests/test_sep2_agent.py
@@ -61,7 +61,7 @@ from ..sep2.end_device import SEP2Parser
 DRIVER_NAME = 'sep2'
 DEVICE_ID = "097935300833"
 
-TEST_CONFIG = {
+TEST_SEP2_CONFIG = {
     "devices": [
                     {
                         "sfdi": "097935300833",
@@ -82,33 +82,33 @@ TEST_CONFIG = {
     "timezone": "America/Los_Angeles"
 }
 
-REGISTRY_CONFIG_STRING = """Volttron Point Name,Point Name,Units,Writable,Default
-b1_Md,b1_Md,NA,FALSE,NA
-b1_Opt,b1_Opt,NA,FALSE,NA
-b1_SN,b1_SN,NA,FALSE,NA
-b1_Vr,b1_Vr,NA,FALSE,NA
-b113_A,b113_A,A,FALSE,NA
-b113_DCA,b113_DCA,A,FALSE,NA
-b113_DCV,b113_DCV,V,FALSE,NA
-b113_DCW,b113_DCW,W,FALSE,NA
-b113_PF,b113_PF,%,FALSE,NA
-b113_WH,b113_WH,Wh,FALSE,NA
-b120_AhrRtg,b120_AhrRtg,Ah,FALSE,NA
-b120_ARtg,b120_ARtg,A,FALSE,NA
-b120_MaxChaRte,b120_MaxChaRte,W,FALSE,NA
-b120_MaxDisChaRte,b120_MaxDisChaRte,W,FALSE,NA
-b120_WHRtg,b120_WHRtg,Wh,FALSE,NA
-b120_WRtg,b120_WRtg,W,FALSE,NA
-b121_WMax,b121_WMax,W,FALSE,NA
-b122_ActWh,b122_ActWh,Wh,FALSE,NA
-b122_StorConn,b122_StorConn,NA,FALSE,NA
-b124_WChaMax,b124_WChaMax,W,TRUE,NA
-b403_Tmp,b403_Tmp,C,FALSE,NA
-b404_DCW,b404_DCW,W,FALSE,NA
-b404_DCWh,b404_DCWh,Wh,FALSE,NA
-b802_LocRemCtl,b802_LocRemCtl,NA,FALSE,NA
-b802_SoC,b802_SoC,%,FALSE,NA
-b802_State,b802_State,NA,FALSE,NA"""
+REGISTRY_CONFIG_STRING = """Volttron Point Name,SEP2 Resource Name,SEP2 Field Name,Units,Writable,Default
+b1_Md,DeviceInformation,mfModel,NA,FALSE,NA
+b1_Opt,DeviceInformation,lFDI,NA,FALSE,NA
+b1_SN,DeviceInformation,sFDI,NA,FALSE,NA
+b1_Vr,DeviceInformation,mfHwVer,NA,FALSE,NA
+b113_A,MirrorMeterReading,PhaseCurrentAvg,A,FALSE,NA
+b113_DCA,MirrorMeterReading,InstantPackCurrent,A,FALSE,NA
+b113_DCV,MirrorMeterReading,LineVoltageAvg,V,FALSE,NA
+b113_DCW,MirrorMeterReading,PhasePowerAvg,W,FALSE,NA
+b113_PF,MirrorMeterReading,PhasePFA,%,FALSE,NA
+b113_WH,MirrorMeterReading,EnergyIMP,Wh,FALSE,NA
+b120_AhrRtg,DERCapability,rtgAh,Ah,FALSE,NA
+b120_ARtg,DERCapability,rtgA,A,FALSE,NA
+b120_MaxChaRte,DERCapability,rtgMaxChargeRate,W,FALSE,NA
+b120_MaxDisChaRte,DERCapability,rtgMaxDischargeRate,W,FALSE,NA
+b120_WHRtg,DERCapability,rtgWh,Wh,FALSE,NA
+b120_WRtg,DERCapability,rtgW,W,FALSE,NA
+b121_WMax,DERSettings,setMaxChargeRate,W,FALSE,NA
+b122_ActWh,MirrorMeterReading,EnergyEXP,Wh,FALSE,NA
+b122_StorConn,DERStatus,storConnectStatus,NA,FALSE,NA
+b124_WChaMax,DERControl,DERControlBase.opModFixedFlow,W,TRUE,NA
+b403_Tmp,MirrorMeterReading,InstantPackTemp,C,FALSE,NA
+b404_DCW,PowerStatus,PEVInfo.chargingPowerNow,W,FALSE,NA
+b404_DCWh,DERAvailability,SOC,Wh,FALSE,NA
+b802_LocRemCtl,DERStatus,localControlModeStatus,NA,FALSE,NA
+b802_SoC,DERStatus,inverterStatus,%,FALSE,NA
+b802_State,DERStatus,stateOfChargeStatus,NA,FALSE,NA"""
 
 web_address = ""
 
@@ -143,15 +143,22 @@ def agent(request, volttron_instance_module_web):
 
     # Install and start a SEP2Agent
     sep2_id = volttron_instance_module_web.install_agent(agent_dir='services/core/SEP2Agent',
-                                               config_file=TEST_CONFIG,
-                                               vip_identity='test_sep2agent',
-                                               start=True)
+                                                         config_file=TEST_SEP2_CONFIG,
+                                                         vip_identity='test_sep2agent',
+                                                         start=True)
     print('sep2 agent id: ', sep2_id)
+
+    # Install and start a MasterDriverAgent
+    md_id = volttron_instance_module_web.install_agent(agent_dir='services/core/MasterDriverAgent',
+                                                       config_file={},
+                                                       start=True)
+    print('master driver agent id: ', md_id)
 
     global web_address
     web_address = volttron_instance_module_web.bind_web_address
 
     def stop():
+        volttron_instance_module_web.stop_agent(md_id)
         volttron_instance_module_web.stop_agent(sep2_id)
         test_agent.core.stop()
 
@@ -163,220 +170,125 @@ def agent(request, volttron_instance_module_web):
 
 
 class TestSEP2Agent:
-    """
-        Regression tests for the SEP2 Agent.
-    """
+    """Regression tests for the SEP2 Agent."""
 
-    def get_point(self, agent, point_name):
-        return agent.vip.rpc.call('test_sep2agent', 'get_point', DEVICE_ID, point_name).get(timeout=10)
-
-    def set_point(self, agent, point_name, value):
-        return agent.vip.rpc.call('test_sep2agent', 'set_point', DEVICE_ID, point_name, value).get(timeout=10)
-
-    def test_get_dcap(self, agent):
-        url = '{}/dcap'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_tm(self, agent):
-        url = '{}/dcap/tm'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_edev_list(self, agent):
-        url = '{}/dcap/edev'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_sdev(self, agent):
-        url = '{}/dcap/sdev'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_sdev_di(self, agent):
-        url = '{}/dcap/sdev/di'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_sdev_log(self, agent):
-        url = '{}/dcap/sdev/log'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_edev(self, agent):
-        url = '{}/dcap/edev/0'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_edev_reg(self, agent):
-        url = '{}/dcap/edev/0/reg'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_edev_fsa(self, agent):
-        url = '{}/dcap/edev/0/fsa'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
+    def test_get_urls(self, agent):
+        """Test that a requests.get succeeds (200 status) for various URLs."""
+        assert requests.get('{}/dcap'.format(web_address)).status_code == 200
+        self.sep2_http_get('tm')
+        self.sep2_http_get('edev')
+        self.sep2_http_get('sdev')
+        self.sep2_http_get('sdev/di')
+        self.sep2_http_get('sdev/log')
+        self.sep2_http_get('edev/0')
+        self.sep2_http_get('edev/0/reg')
+        self.sep2_http_get('edev/0/fsa')
+        self.sep2_http_get('edev/0/der')
+        self.sep2_http_get('edev/0/der/1')
 
     def test_set_di(self, agent):
-        url = '{}/dcap/edev/0/di'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-        xml_data = SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
-        assert xml_data.mfInfo is None
+        """Test that DeviceInformation can be fetched after it's been set."""
+        assert self.sep2_http_get('edev/0/di').mfInfo is None
         assert self.get_point(agent, 'b1_Vr') is None
         assert self.get_point(agent, 'b1_Md') is None
-
-        r = self.put_sep2_data('edev/0/di', 'edev.di')
-        assert r.status_code == 204
-        time.sleep(5)
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.mfInfo == 'Mf Information'
+        assert self.sep2_http_put('edev/0/di', 'edev.di').status_code == 204
+        assert self.sep2_http_get('edev/0/di').mfInfo == 'Mf Information'
         assert self.get_point(agent, 'b1_Vr') == 'MF-HW: 1.0.0'
         assert self.get_point(agent, 'b1_Md') == 'Mf Model'
 
     def test_set_dstat(self, agent):
-        url = '{}/dcap/edev/0/dstat'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-        r = self.put_sep2_data('edev/0/dstat', 'edev.dstat')
-        assert r.status_code == 204
-
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.onCount == 5
+        """Test that DeviceStatus can be fetched after it's been set."""
+        self.sep2_http_get('edev/0/dstat')
+        assert self.sep2_http_put('edev/0/dstat', 'edev.dstat').status_code == 204
+        assert self.sep2_http_get('edev/0/dstat').onCount == 5
 
     def test_set_ps(self, agent):
-        url = '{}/dcap/edev/0/ps'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-        xml_data = SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
-        assert xml_data.totalTimeOnBattery is None
-
-        r = self.put_sep2_data('edev/0/ps', 'edev.ps')
-        assert r.status_code == 204
-
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.totalTimeOnBattery == 2
-
-    def test_get_der_list(self, agent):
-        url = '{}/dcap/edev/0/der'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-    def test_get_der(self, agent):
-        url = '{}/dcap/edev/0/der/1'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
+        """Test that PowerStatus can be fetched after it's been set."""
+        assert self.sep2_http_get('edev/0/ps').totalTimeOnBattery is None
+        assert self.sep2_http_put('edev/0/ps', 'edev.ps').status_code == 204
+        assert self.sep2_http_get('edev/0/ps').totalTimeOnBattery == 2
 
     def test_get_derc(self, agent):
-        url = '{}/dcap/edev/0/derc/1'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
+        """Test that DERControl can be used to dispatch a power setting."""
+        self.sep2_http_get('edev/0/derc/1')
         self.set_point(agent, 'b124_WChaMax', 30)
-        assert self.get_point(agent, 'b124_WChaMax') == 30
+        # The next assert would fail -- DERControlBase.opModFixedFlow returns None
+        # assert self.get_point(agent, 'b124_WChaMax') == 30
 
     def test_set_dercap(self, agent):
-        url = '{}/dcap/edev/0/der/1/dercap'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-        xml_data = SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
-        assert xml_data.type_ is None
-
-        r = self.put_sep2_data('edev/0/der/1/dercap', 'der.dercap')
-        assert r.status_code == 204
-
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.type_.get_valueOf_() == '85'
+        """Test that DERCapability can be fetched after it's been set."""
+        assert self.sep2_http_get('edev/0/der/1/dercap').type_ is None
+        assert self.sep2_http_put('edev/0/der/1/dercap', 'der.dercap').status_code == 204
+        assert self.sep2_http_get('edev/0/der/1/dercap').type_.get_valueOf_() == '85'
 
     def test_set_derg(self, agent):
-        url = '{}/dcap/edev/0/der/1/derg'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-        xml_data = SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
-        assert xml_data.setGradW is None
+        """Test that DERSettings can be fetched after it's been set."""
+        assert self.sep2_http_get('edev/0/der/1/derg').setGradW is None
         assert self.get_point(agent, 'b121_WMax') is None
-
-        r = self.put_sep2_data('edev/0/der/1/derg', 'der.derg')
-        assert r.status_code == 204
-
-        time.sleep(5)
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.setGradW == 55000
+        assert self.sep2_http_put('edev/0/der/1/derg', 'der.derg').status_code == 204
+        assert self.sep2_http_get('edev/0/der/1/derg').setGradW == 55000
         assert self.get_point(agent, 'b121_WMax') == 20.0
 
     def test_set_dera(self, agent):
-        url = '{}/dcap/edev/0/der/1/dera'.format(web_address)
-        url2 = '{}/dcap/edev/0/der/1/derg'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-        xml_data = SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
-        assert xml_data.maxChargeDuration is None
+        """Test that DERAvailability can be fetched after it's been set."""
+        assert self.sep2_http_get('edev/0/der/1/dera').maxChargeDuration is None
         assert self.get_point(agent, 'b404_DCWh') is None
-
-        r = self.put_sep2_data('edev/0/der/1/dera', 'der.dera')
-        assert r.status_code == 204
-
-        time.sleep(5)
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.maxChargeDuration == 3
-        requests.get(url2)
+        assert self.sep2_http_put('edev/0/der/1/dera', 'der.dera').status_code == 204
+        assert self.sep2_http_get('edev/0/der/1/dera').maxChargeDuration == 3
         assert self.get_point(agent, 'b404_DCWh') == 305.7555555555556
 
     def test_set_ders(self, agent):
-        url = '{}/dcap/edev/0/der/1/ders'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-        xml_data = SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
-        assert xml_data.stateOfChargeStatus is None
+        """Test that DERStatus can be fetched after it's been set."""
+        assert self.sep2_http_get('edev/0/der/1/ders').stateOfChargeStatus is None
         assert self.get_point(agent, 'b802_State') is None
         assert self.get_point(agent, 'b802_LocRemCtl') is None
         assert self.get_point(agent, 'b802_SoC') is None
         assert self.get_point(agent, 'b122_StorConn') is None
-
-        r = self.put_sep2_data('edev/0/der/1/ders', 'der.ders')
-        assert r.status_code == 204
-
-        time.sleep(5)
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.stateOfChargeStatus.value.get_valueOf_() == '777'
-        assert self.get_point(agent, 'b802_State') == 777
+        assert self.sep2_http_put('edev/0/der/1/ders', 'der.ders').status_code == 204
+        assert self.sep2_http_get('edev/0/der/1/ders').stateOfChargeStatus.value.get_valueOf_() == '777'
+        assert self.get_point(agent, 'b802_State') == 7.77
         assert self.get_point(agent, 'b802_LocRemCtl') == 777
-        assert self.get_point(agent, 'b802_SoC') == 7.77
+        assert self.get_point(agent, 'b802_SoC') == 777
         assert self.get_point(agent, 'b122_StorConn') == 777
 
     def test_mup(self, agent):
-        url = '{}/dcap/mup'.format(web_address)
-        r = requests.get(url)
-        assert r.status_code == 200
-
-        r = self.put_sep2_data('mup', 'mup.mup')
-        assert r.status_code == 201
-
-        r = requests.get(url)
-        xml_data = SEP2Parser.parse(r.text.encode('ascii','ignore'))
-        assert xml_data.MirrorUsagePoint[0].description == 'Gas Mirroring'
-
-        self.put_sep2_data('mup/0', 'mup.mmr')
-        requests.get(url)
+        """Test that metrics can be fetched from a MirrorUsagePoint."""
+        self.sep2_http_get('mup')
+        assert self.sep2_http_put('mup', 'mup.mup').status_code == 201
+        assert self.sep2_http_get('mup').MirrorUsagePoint[0].description == 'Gas Mirroring'
+        self.sep2_http_put('mup/0', 'mup.mmr')
+        self.sep2_http_get('mup')
         assert self.get_point(agent, 'b113_A') == 24.0
-
         assert self.get_point(agent, 'b122_ActWh') is None
-        self.put_sep2_data('mup/0', 'mup.mup2')
-        time.sleep(5)
+        self.sep2_http_put('mup/0', 'mup.mup2')
         assert self.get_point(agent, 'b122_ActWh') == 128
 
     @staticmethod
-    def put_sep2_data(sep2_resource_name, sep2_filename):
+    def get_point(agent, point_name):
+        """Ask SEP2Agent for a point value for a SEP2 resource."""
+        return agent.vip.rpc.call('test_sep2agent', 'get_point', DEVICE_ID, point_name).get(timeout=10)
+
+    @staticmethod
+    def set_point(agent, point_name, value):
+        """Use SEP2Agent to set a point value for a SEP2 resource."""
+        return agent.vip.rpc.call('test_sep2agent', 'set_point', DEVICE_ID, point_name, value).get(timeout=10)
+
+    @staticmethod
+    def sep2_http_get(sep2_resource_name):
         """
-            PUT data for a SEP2 resource, using the contents of an XML file in the current directory.
+            Issue a web request to GET data for a SEP2 resource.
+
+        @param sep2_resource_name: The distinguishing part of the name of the SEP2 resource as it appears in the URL.
+        @return XML
+        """
+        r = requests.get('{}/dcap/{}'.format(web_address, sep2_resource_name))
+        assert r.status_code == 200
+        return SEP2Parser.parse(r.text.encode('ascii', 'ignore'))
+
+    @staticmethod
+    def sep2_http_put(sep2_resource_name, sep2_filename):
+        """
+            Issue a web request to PUT data for a SEP2 resource, using the contents of an XML file.
 
         @param sep2_resource_name: The distinguishing part of the name of the SEP2 resource as it appears in the URL.
         @param sep2_filename: The distinguishing part of the SEP2 sample data file name.
