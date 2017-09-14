bca6aca2f249ef83e5f2d5e8827aefc3553bdcd6
Edward Ellis <edward.ellis@pnnl.gov>
2017-09-14 12:25:49 -0700

    Adding Sen's example agents.

---
+++ examples/MarketAgents/AHUAgent/ahu/agent.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+
+# Copyright (c) 2016, Battelle Memorial Institute
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation
+# are those of the authors and should not be interpreted as representing
+# official policies, either expressed or implied, of the FreeBSD
+# Project.
+#
+# This material was prepared as an account of work sponsored by an
+# agency of the United States Government.  Neither the United States
+# Government nor the United States Department of Energy, nor Battelle,
+# nor any of their employees, nor any jurisdiction or organization that
+# has cooperated in the development of these materials, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness or any
+# information, apparatus, product, software, or process disclosed, or
+# represents that its use would not infringe privately owned rights.
+#
+# Reference herein to any specific commercial product, process, or
+# service by trade name, trademark, manufacturer, or otherwise does not
+# necessarily constitute or imply its endorsement, recommendation, or
+# favoring by the United States Government or any agency thereof, or
+# Battelle Memorial Institute. The views and opinions of authors
+# expressed herein do not necessarily state or reflect those of the
+# United States Government or any agency thereof.
+#
+# PACIFIC NORTHWEST NATIONAL LABORATORY
+# operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# under Contract DE-AC05-76RL01830
+
+# }}}
+
+import sys
+import logging
+from volttron.platform.agent import utils
+from volttron.platform.vip.agent import Agent, Core
+from volttron.platform.agent.base_market_agent import MarketAgent
+from volttron.platform.agent.base_market_agent.poly_line import PolyLine
+from volttron.platform.agent.base_market_agent.point import Point
+from volttron.platform.agent.base_market_agent.buy_sell import BUYER
+from volttron.platform.agent.base_market_agent.buy_sell import SELLER
+from pnnl.models.firstorderzone import FirstOrderZone
+from volttron.platform.agent.known_identities import PLATFORM_MARKET_SERVICE
+
+_log = logging.getLogger(__name__)
+utils.setup_logging()
+__version__ = "0.01"
+
+def ahu_agent(config_path, **kwargs):
+    """Parses the Electric Meter Agent configuration and returns an instance of
+    the agent created using that configuation.
+
+    :param config_path: Path to a configuation file.
+
+    :type config_path: str
+    :returns: Market Service Agent
+    :rtype: MarketServiceAgent
+    """   
+    try:
+        config = utils.load_config(config_path)
+    except StandardError:
+        config = {}
+
+    if not config:
+        _log.info("Using defaults for starting configuration.")
+    market_name1 = config.get('market_name1')
+#    market_name2 = config.get('market_name2')	
+    agent_name= config.get('agent_name')		
+    return AHUAgent(market_name1,agent_name, **kwargs)
+
+
+	
+	
+
+class AHUAgent(MarketAgent):
+    """
+    The SampleElectricMeterAgent serves as a sample of an electric meter that
+    sells electricity for a single building at a fixed price.
+    """
+    def __init__(self, market_name,agent_name,**kwargs):
+        super(AHUAgent, self).__init__(**kwargs)
+        self.iniState()
+        self.market_name1 = market_name
+        self.agent_name = agent_name		
+        self.subscribing_topic='devices/CAMPUS/BUILDING1/AHU1/all'
+        self.join_market(self.market_name1, SELLER, None, None, self.aggregate_callback, self.price_callback, self.error_callback)
+#        self.join_market(self.market_name2, BUYER, None, self.offer_callback2, None, self.price_callback2, self.error_callback2)
+		
+    @Core.receiver('onstart')
+    def setup(self, sender, **kwargs):
+        _log.debug('Subscribing to '+self.subscribing_topic)
+        self.vip.pubsub.subscribe(peer='pubsub',
+                                  prefix=self.subscribing_topic,
+                                  callback=self.updateState)
+								  
+
+
+    def aggregate_callback(self, timestamp, market_name, buyer_seller, aggregate_curve):
+        _log.debug('total_curve')
+				
+        self.vip.rpc.call(PLATFORM_MARKET_SERVICE, 'make_offer', self.market_name1, SELLER, self.create_supply_curve().tuppleize()).get(timeout=5.0)
+
+								  
+		
+    def offer_callback(self, timestamp, market_name, buyer_seller):
+        return create_supply_curve()
+
+    def create_supply_curve(self):
+        supply_curve = PolyLine()
+        price = 65
+        quantity = 100000
+        supply_curve.add(Point(price,quantity))
+        price = 65
+        quantity = -1*10000
+        supply_curve.add(Point(price,quantity))
+        return supply_curve		
+		
+		
+		
+		
+#    def create_demand_curve(self):
+#        demand_curve = PolyLine()		
+#        pMin = 10
+#        pMax = 100
+#        qMin = abs(self.getQMin())
+#        qMax = abs(self.getQMax())
+#        if (self.hvacAvail > 0):
+#            demand_curve.add(Point(max(pMin, pMax),min(qMin, qMax)))
+#            demand_curve.add(Point(min(pMin, pMax),max(qMin, qMax)))
+#        else:
+#            demand_curve.add(Point(max(pMin, pMax), 0.0))
+#            demand_curve.add(Point(min(pMin, pMax),0.0))
+#        return demand_curve 
+
+    def iniState(self):
+	    '''inital state
+	    '''
+		
+    def updateState(self, peer, sender, bus, topic, headers, message):
+	    '''Subscribe to device data from message bus
+	    '''
+	    _log.debug('Received one new dataset')
+	    info = {}
+	    for key, value in message[0].items():
+                  info[key.lower()] = value
+						
+    def price_callback(self, timestamp, market_name, buyer_seller, price, quantity):
+        print "WOBAH"
+        _log.debug("Report cleared price for Market: {} {}, Message: {}".format(market_name, buyer_seller, price, quantity))
+        _log.debug("AHU_works")
+
+    def error_callback(self, timestamp, market_name, buyer_seller, error_message):
+        _log.debug("Report error for Market: {} {}, Message: {}".format(market_name, buyer_seller, error_message))
+
+
+def main():
+    """Main method called to start the agent."""
+    utils.vip_main(ahu_agent, version=__version__)
+
+
+if __name__ == '__main__':
+    # Entry point for script
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        pass
