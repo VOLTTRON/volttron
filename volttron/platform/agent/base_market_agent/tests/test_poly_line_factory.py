ff689f7c70024676c4f701df8e1b22e2a8d31511
Edward Ellis <edward.ellis@pnnl.gov>
2017-09-18 12:26:38 -0700

    Add test

--- volttron/platform/agent/base_market_agent/tests/test_poly_line_factory.py
+++ volttron/platform/agent/base_market_agent/tests/test_poly_line_factory.py
@@ -80,6 +80,15 @@ def test_poly_line_combine_demand():
     assert combined_curves.min_y() == 0
     assert combined_curves.max_y() == 1000
 
+def test_poly_line_combine_single():
+    demand_curve = create_demand_curve()
+    curves = [demand_curve]
+    combined_curves = PolyLineFactory.combine(curves, 100)
+    assert combined_curves.min_x() == 0
+    assert combined_curves.max_x() == 2000
+    assert combined_curves.min_y() == 0
+    assert combined_curves.max_y() == 1000
+
 
 def test_poly_line_from_tupples():
     demand_curve = create_demand_curve()
