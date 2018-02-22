7fc890da5ac1e23508115172175b92e54541bcae
Niddodi, Shwetha <shwetha.niddodi@pnnl.gov>
2018-02-22 15:52:25 -0800

    Changed comment

--- volttron/platform/vip/agent/core.py
+++ volttron/platform/vip/agent/core.py
@@ -172,7 +172,7 @@ class BasicCore(object):
         self.onfinish = Signal()
         self.oninterrupt = None
         prev_int_signal = gevent.signal.getsignal(signal.SIGINT)
-        # To avoid multiple handlers between parent and child agents
+        # To avoid a child agent handler overwriting the parent agent handler
         if prev_int_signal in [None, signal.SIG_IGN, signal.SIG_DFL]:
             self.oninterrupt = gevent.signal.signal(signal.SIGINT, self._on_sigint_handler)
         self._owner = owner
