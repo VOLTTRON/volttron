d01a181a82cc6855fef46eb412d6b1491ff357b2
Craig <craig.allwardt@pnnl.gov>
2018-03-23 14:18:52 -0700

    Fixed issue #1640

--- bootstrap.py
+++ bootstrap.py
@@ -1,59 +1,40 @@
 # -*- coding: utf-8 -*- {{{
 # vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
-
-# Copyright (c) 2015, Battelle Memorial Institute
-# All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
+# Copyright 2017, Battelle Memorial Institute.
 #
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# The views and conclusions contained in the software and documentation
-# are those of the authors and should not be interpreted as representing
-# official policies, either expressed or implied, of the FreeBSD
-# Project.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
-# This material was prepared as an account of work sponsored by an
-# agency of the United States Government.  Neither the United States
-# Government nor the United States Department of Energy, nor Battelle,
-# nor any of their employees, nor any jurisdiction or organization that
-# has cooperated in the development of these materials, makes any
-# warranty, express or implied, or assumes any legal liability or
-# responsibility for the accuracy, completeness, or usefulness or any
-# information, apparatus, product, software, or process disclosed, or
-# represents that its use would not infringe privately owned rights.
-#
-# Reference herein to any specific commercial product, process, or
-# service by trade name, trademark, manufacturer, or otherwise does not
-# necessarily constitute or imply its endorsement, recommendation, or
+# This material was prepared as an account of work sponsored by an agency of
+# the United States Government. Neither the United States Government nor the
+# United States Department of Energy, nor Battelle, nor any of their
+# employees, nor any jurisdiction or organization that has cooperated in the
+# development of these materials, makes any warranty, express or
+# implied, or assumes any legal liability or responsibility for the accuracy,
+# completeness, or usefulness or any information, apparatus, product,
+# software, or process disclosed, or represents that its use would not infringe
+# privately owned rights. Reference herein to any specific commercial product,
+# process, or service by trade name, trademark, manufacturer, or otherwise
+# does not necessarily constitute or imply its endorsement, recommendation, or
 # favoring by the United States Government or any agency thereof, or
-# Battelle Memorial Institute. The views and opinions of authors
-# expressed herein do not necessarily state or reflect those of the
+# Battelle Memorial Institute. The views and opinions of authors expressed
+# herein do not necessarily state or reflect those of the
 # United States Government or any agency thereof.
 #
-# PACIFIC NORTHWEST NATIONAL LABORATORY
-# operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# PACIFIC NORTHWEST NATIONAL LABORATORY operated by
+# BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
 # under Contract DE-AC05-76RL01830
-#}}}
+# }}}
 
 '''bootstrap - Prepare a VOLTTRON virtual environment.
 
@@ -110,8 +91,7 @@ def shescape(args):
                     '"' if ' ' in arg else '') for arg in args)
 
 
-def bootstrap(dest, prompt='(volttron)', version=None, verbose=None,
-              proxy=None):
+def bootstrap(dest, prompt='(volttron)', version=None, verbose=None):
     '''Download latest virtualenv and create a virtual environment.
 
     The virtual environment will be created in the given directory. The
@@ -145,13 +125,6 @@ def bootstrap(dest, prompt='(volttron)', version=None, verbose=None,
         def _fetch(self, url):
             '''Open url and return the response object (or bail).'''
             _log.debug('Fetching %s', url)
-            # Create proxy for retrieving data.
-            if proxy:
-                proxy_obj = urllib2.ProxyHandler({'http': proxy,
-                                                  'https': proxy})
-                opener = urllib2.build_opener(proxy_obj)
-                urllib2.install_opener(opener)
-
             response = urllib2.urlopen(url)
             if response.getcode() != 200:
                 _log.error('Server response is %s %s',
@@ -159,6 +132,17 @@ def bootstrap(dest, prompt='(volttron)', version=None, verbose=None,
                 _log.fatal('Download failed!')
                 sys.exit(1)
             return response
+
+        def _url_available(self, url):
+            '''Open url and if response is 200 then return True else return False'''
+            _log.debug('Checking url %s', url)
+            try:
+                response = urllib2.urlopen(url)
+                if response.getcode() != 200:
+                    return False
+            except urllib2.HTTPError:
+                return False
+            return True
 
         def get_version(self):
             '''Return the latest version from virtualenv DOAP record.'''
@@ -175,8 +159,13 @@ def bootstrap(dest, prompt='(volttron)', version=None, verbose=None,
             '''Download the virtualenv tarball into directory.'''
             if self.version is None:
                 self.get_version()
+            default_version = "15.1.0"
             url = ('https://github.com/pypa/virtualenv/archive/'
                    '{}.tar.gz'.format(self.version))
+            if not self._url_available(url):
+                self.version = default_version
+                url = ('https://github.com/pypa/virtualenv/archive/'
+                        '{}.tar.gz'.format(self.version))
             _log.info('Downloading virtualenv %s', self.version)
             tarball = os.path.join(directory, 'virtualenv.tar.gz')
             with contextlib.closing(self._fetch(url)) as response:
@@ -215,8 +204,7 @@ def bootstrap(dest, prompt='(volttron)', version=None, verbose=None,
     return builder.env_exe
 
 
-def pip(operation, args, verbose=None, upgrade=False, offline=False,
-    proxy=None):
+def pip(operation, args, verbose=None, upgrade=False, offline=False):
     '''Call pip in the virtual environment to perform operation.'''
     cmd = ['pip', operation]
     if verbose is not None:
@@ -225,17 +213,16 @@ def pip(operation, args, verbose=None, upgrade=False, offline=False,
         cmd.append('--upgrade')
     if offline:
         cmd.extend(['--retries', '0', '--timeout', '1'])
-    if proxy is not None:
-        cmd.extend(['--proxy', proxy])
     cmd.extend(args)
     _log.info('+ %s', shescape(cmd))
     cmd[:0] = [sys.executable, '-m']
     subprocess.check_call(cmd)
 
 
-def update(operation, verbose=None, upgrade=False, offline=False, proxy=None):
+def update(operation, verbose=None, upgrade=False, offline=False):
     '''Install dependencies in setup.py and requirements.txt.'''
-    from setup import (option_requirements, local_requirements)
+    from setup import (option_requirements, local_requirements,
+                       optional_requirements)
     assert operation in ['install', 'wheel']
     wheeling = operation == 'wheel'
     path = os.path.dirname(__file__) or '.'
@@ -244,7 +231,7 @@ def update(operation, verbose=None, upgrade=False, offline=False, proxy=None):
         try:
             import wheel
         except ImportError:
-            pip('install', ['wheel'], verbose, offline=offline, proxy=proxy)
+            pip('install', ['wheel'], verbose, offline=offline)
     # Build option_requirements separately to pass install options
     build_option = '--build-option' if wheeling else '--install-option'
     for requirement, options in option_requirements:
@@ -252,7 +239,11 @@ def update(operation, verbose=None, upgrade=False, offline=False, proxy=None):
         for opt in options:
             args.extend([build_option, opt])
         args.extend(['--no-deps', requirement])
-        pip(operation, args, verbose, upgrade, offline, proxy)
+        pip(operation, args, verbose, upgrade, offline)
+    # Build the optional requirements that the user specified via the command
+    # line.
+    for requirement in optional_requirements:
+        pip('install', [requirement], verbose, upgrade, offline)
     # Install local packages and remaining dependencies
     args = []
     for _, location in local_requirements:
@@ -261,7 +252,7 @@ def update(operation, verbose=None, upgrade=False, offline=False, proxy=None):
     requirements_txt = os.path.join(path, 'requirements.txt')
     if os.path.exists(requirements_txt):
         args.extend(['--requirement', requirements_txt])
-    pip(operation, args, verbose, upgrade, offline, proxy)
+    pip(operation, args, verbose, upgrade, offline)
 
 
 def main(argv=sys.argv):
@@ -309,9 +300,6 @@ def main(argv=sys.argv):
         '--envdir', default=None, metavar='VIRTUAL_ENV',
         help='alternate location for virtual environment')
     bs.add_argument(
-        '--proxy', default=None, metavar='PROXY',
-        help='Provide proxy argument for pip to use.')
-    bs.add_argument(
         '--force', action='store_true', default=False,
         help='force installing in non-empty directory')
     bs.add_argument(
@@ -322,6 +310,24 @@ def main(argv=sys.argv):
         'in activated environment (default: %(default)s)')
     bs.add_argument('--force-version', help=argparse.SUPPRESS)
 
+    # allows us to look and see if any of the dynamic optional arguments
+    # are on the command line.  We check this during the processing of the args
+    # variable at the end of the block.  If the option is set then it needs
+    # to be passed on.
+    optional_args = []
+    if os.path.exists('optional_requirements.json'):
+        po = parser.add_argument_group('Extra packaging options')
+        with open('optional_requirements.json', 'r') as optional_arguments:
+            data = json.load(optional_arguments)
+            for arg, vals in data.items():
+                optional_args.append(arg)
+                if 'help' in vals.keys():
+                    po.add_argument(arg, action='store_true', default=False,
+                                    help=vals['help'])
+                else:
+                    po.add_argument(arg, action='store_true', default=False)
+
+    # Update options
     up = parser.add_argument_group('update options')
     up.add_argument(
         '--offline', action='store_true', default=False,
@@ -353,7 +359,7 @@ def main(argv=sys.argv):
     if hasattr(sys, 'real_prefix'):
         # The script was called from a virtual environment Python, so update
         update(options.operation, options.verbose,
-               options.upgrade, options.offline, options.proxy)
+               options.upgrade, options.offline)
     else:
         # The script was called from the system Python, so bootstrap
         try:
@@ -373,16 +379,18 @@ def main(argv=sys.argv):
             if exc.errno != errno.ENOENT:
                 raise
         env_exe = bootstrap(options.envdir, options.prompt,
-                            options.force_version, options.verbose,
-                            options.proxy)
+                            options.force_version, options.verbose)
         if options.only_virtenv:
             return
         # Run this script within the virtual environment for stage2
         args = [env_exe, __file__]
         if options.verbose is not None:
             args.append('--verbose' if options.verbose else '--quiet')
-        if options.proxy is not None:
-            args.extend(['--proxy', options.proxy])
+        # Transfer dynamic properties to the subprocess call 'update'.
+        # Clip off the first two characters expecting long parameter form.
+        for arg in optional_args:
+            if getattr(options, arg[2:]):
+                args.append(arg)
         subprocess.check_call(args)
 
 
