736aece6ee98e9e73eb533a8a335ab4820899a59
Kyle Monson <kyle.monson@pnnl.gov>
2018-05-03 12:17:46 -0700

    Add docs to data cleaner.

--- examples/DataCleaner/data_cleaner/agent.py
+++ examples/DataCleaner/data_cleaner/agent.py
@@ -1,6 +1,40 @@
-"""
-Agent documentation goes here.
-"""
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+#
+# Copyright 2018, Battelle Memorial Institute.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This material was prepared as an account of work sponsored by an agency of
+# the United States Government. Neither the United States Government nor the
+# United States Department of Energy, nor Battelle, nor any of their
+# employees, nor any jurisdiction or organization that has cooperated in the
+# development of these materials, makes any warranty, express or
+# implied, or assumes any legal liability or responsibility for the accuracy,
+# completeness, or usefulness or any information, apparatus, product,
+# software, or process disclosed, or represents that its use would not infringe
+# privately owned rights. Reference herein to any specific commercial product,
+# process, or service by trade name, trademark, manufacturer, or otherwise
+# does not necessarily constitute or imply its endorsement, recommendation, or
+# favoring by the United States Government or any agency thereof, or
+# Battelle Memorial Institute. The views and opinions of authors expressed
+# herein do not necessarily state or reflect those of the
+# United States Government or any agency thereof.
+#
+# PACIFIC NORTHWEST NATIONAL LABORATORY operated by
+# BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# under Contract DE-AC05-76RL01830
+# }}}
 
 __docformat__ = 'reStructuredText'
 
@@ -101,6 +135,26 @@ class DataCleaner(Agent):
 
     def process_point(self, now, topic, min_value=None, max_value=None, output_topic=None,
                       aggregate_method=None):
+        """
+        This is where the magic happens.
+
+        Introducing new or changing methods to clean/massage/introduce new data go here.
+
+        Currently this function republishes the most recent data as is if it
+        is not too old and falls within the min and max settings.
+
+        If the most recent value is stale it uses an average of the last 30 days.
+
+        New keyword arguments to this function can be added as needed and will be passed
+        straight from the arguments to a topic in the configuration file.
+
+        :param now:
+        :param topic:
+        :param min_value:
+        :param max_value:
+        :param output_topic:
+        :param aggregate_method:
+        """
         _log.debug("Processing topic: {}".format(topic))
 
         if output_topic is None:
