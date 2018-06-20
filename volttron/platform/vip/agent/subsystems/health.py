b2ccd007f9cb92c51311d6d3ad61037a5658f4db
Kyle Monson <kyle.monson@pnnl.gov>
2018-06-20 14:05:25 -0700

    Removed unneeded import.

--- volttron/platform/vip/agent/subsystems/health.py
+++ volttron/platform/vip/agent/subsystems/health.py
@@ -40,7 +40,6 @@ import logging
 import os
 import weakref
 
-from volttron.platform.agent import utils
 from volttron.platform.messaging import topics
 from volttron.platform.messaging.health import *
 from .base import SubsystemBase
