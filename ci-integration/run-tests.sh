6e53cd81d14ff598ed9232cd1d455bc0d2f3f897
Niddodi, Shwetha <shwetha.niddodi@pnnl.gov>
2018-09-04 13:54:02 -0700

    erlang fix

--- ci-integration/run-tests.sh
+++ ci-integration/run-tests.sh
@@ -35,14 +35,13 @@ testdirs="services/core/VolttronCentral/tests services/core/VolttronCentralPlatf
 splitdirs="services/core/*"
 
 echo "installing ERLANG"
-ERLANG_DIST=esl-erlang_21.0.5-1~ubuntu~trusty_amd64.deb
+wget -O - 'https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc' | sudo apt-key add -
+echo 'deb https://dl.bintray.com/rabbitmq/debian trusty erlang-21.x'|sudo tee --append /etc/apt/sources.list.d/bintray.erlang.list
 sudo apt-get update
 sudo apt-get purge erlang*
-sudo apt-get install -y libsctp1
 sudo apt-get install -yf
-
-wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/$ERLANG_DIST
-sudo dpkg -i $ERLANG_DIST --auto-deconfigure
+sudo apt-get install -y erlang-diameter erlang-eldap
+sudo apt-get install -y erlang-nox
 
 echo "bootstrapping RABBITMQ"
 python bootstrap.py --rabbitmq --market
