306fef49dc4dbc77061a2137a9784c9568b6c8c3
C. Allwardt <Craig.Allwardt@pnnl.gov>
2019-01-04 18:01:57 -0500

    Added index.html and list certs to the web admin app. Integrated / and /certs to the api.

---
+++ webadmin/web_admin.py
@@ -0,0 +1,37 @@
+from os import path as p
+from flask import Flask, render_template
+from volttron.platform.certs import Certs, Subject
+
+from gevent.pywsgi import WSGIServer
+
+# Web root is going to be relative to the volttron central agents
+# current agent's installed path
+DEFAULT_STATIC = p.abspath(p.join(p.dirname(__file__), 'static/'))
+
+app = Flask(__name__) #, root_path='/', static_folder=DEFAULT_STATIC)  # pylint: disable=invalid-name
+app.debug = True
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/certs')
+def list_certs():
+    certs = Certs()
+    subjects = certs.get_subjects()
+    return render_template('list_certs.html', certs=subjects)
+
+
+def main():
+    "Start gevent WSGI server"
+    app.run()
+    # use gevent WSGI server instead of the Flask
+    #http = WSGIServer(('', 5000), app.wsgi_app)
+    # TODO gracefully handle shutdown
+    #http.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
