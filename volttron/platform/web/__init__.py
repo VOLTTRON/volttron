059be8519708c0fa56f234d5c9c554b854847808
C. Allwardt <Craig.Allwardt@pnnl.gov>
2019-01-29 21:33:41 -0500

    Keep web importable to maintain backward compatability of usage.

---
+++ volttron/platform/web/__init__.py
@@ -0,0 +1,34 @@
+import logging
+from urlparse import urlparse
+
+from master_web_service import MasterWebService
+from discovery import DiscoveryInfo, DiscoveryError
+
+_log = logging.getLogger(__name__)
+
+
+def build_vip_address_string(vip_root, serverkey, publickey, secretkey):
+    """ Build a full vip address string based upon the passed arguments
+
+    All arguments are required to be non-None in order for the string to be
+    created successfully.
+
+    :raises ValueError if one of the parameters is None.
+    """
+    _log.debug("root: {}, serverkey: {}, publickey: {}, secretkey: {}".format(
+        vip_root, serverkey, publickey, secretkey))
+    parsed = urlparse(vip_root)
+    if parsed.scheme == 'tcp':
+        if not (serverkey and publickey and secretkey and vip_root):
+            raise ValueError("All parameters must be entered.")
+
+        root = "{}?serverkey={}&publickey={}&secretkey={}".format(
+            vip_root, serverkey, publickey, secretkey)
+
+    elif parsed.scheme == 'ipc':
+        root = vip_root
+    else:
+        raise ValueError('Invalid vip root specified!')
+
+    return root
+
---
+++ volttron/platform/web/discovery.py
@@ -0,0 +1,89 @@
+import logging
+from urlparse import urlparse, urljoin
+
+import requests
+from requests.packages.urllib3.connection import (ConnectionError,
+                                                  NewConnectionError)
+
+from volttron.platform.agent import json as jsonapi
+from volttron.platform.certs import Certs
+
+_log = logging.getLogger(__name__)
+
+
+class DiscoveryError(StandardError):
+    """ Raised when a different volttron central tries to register.
+    """
+    pass
+
+
+class DiscoveryInfo(object):
+    """ A DiscoveryInfo class.
+
+    The DiscoveryInfo class provides a wrapper around the return values from
+    a call to the /discovery/ endpoint of the `volttron.platform.web.
+    """
+
+    def __init__(self, **kwargs):
+
+        self.discovery_address = kwargs.pop('discovery_address')
+        self.vip_address = kwargs.pop('vip-address')
+        self.serverkey = kwargs.pop('serverkey')
+        self.instance_name = kwargs.pop('instance-name')
+        self.vc_rmq_address = kwargs.pop('vc-rmq-address')
+        self.rmq_ca_cert = kwargs.pop('rmq-ca-cert')
+        self.certs = Certs()
+
+        assert len(kwargs) == 0
+
+    @staticmethod
+    def request_discovery_info(web_address):
+        """  Construct a `DiscoveryInfo` object.
+
+        Requests a response from discovery_address and constructs a
+        `DiscoveryInfo` object with the returned json.
+
+        :param web_address: An http(s) address with volttron running.
+        :return:
+        """
+
+        try:
+            parsed = urlparse(web_address)
+
+            assert parsed.scheme
+            assert not parsed.path
+
+            real_url = urljoin(web_address, "/discovery/")
+            _log.info('Connecting to: {}'.format(real_url))
+            response = requests.get(real_url)
+
+            if not response.ok:
+                raise DiscoveryError(
+                    "Invalid discovery response from {}".format(real_url)
+                )
+        except AttributeError as e:
+            raise DiscoveryError(
+                "Invalid web_address passed {}"
+                .format(web_address)
+            )
+        except (ConnectionError, NewConnectionError) as e:
+            raise DiscoveryError(
+                "Connection to {} not available".format(real_url)
+            )
+        except Exception as e:
+            raise DiscoveryError("Unhandled exception {}".format(e))
+
+        return DiscoveryInfo(
+            discovery_address=web_address, **(response.json()))
+
+    def __str__(self):
+        dk = {
+            'discovery_address': self.discovery_address,
+            'vip_address': self.vip_address,
+            'serverkey': self.serverkey,
+            'instance_name': self.instance_name,
+            'vc_rmq_address': self.vc_rmq_address,
+            'rmq_ca_cert': self.rmq_ca_cert
+        }
+
+        return jsonapi.dumps(dk)
