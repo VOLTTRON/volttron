bbaa85e5989474c7f524244509fd14c6f10fd4aa
C. Allwardt <Craig.Allwardt@pnnl.gov>
2019-02-15 02:52:08 -0500

    csr endpoint correctly fails if invalid formed csr common name.

--- volttron/platform/web/csr_endpoints.py
+++ volttron/platform/web/csr_endpoints.py
@@ -3,7 +3,7 @@ import logging
 import weakref
 
 from volttron.platform.agent import json
-from volttron.platform.agent.utils import get_fq_identity
+from volttron.platform.agent.utils import get_fq_identity, get_platform_instance_name
 from volttron.platform.agent.web import Response
 from volttron.platform.certs import Certs
 
@@ -41,18 +41,27 @@ class CSREndpoints(object):
         csr = request_data.get('csr')
         identity = request_data.get('identity')
 
-        instance_new_name = get_fq_identity(identity)
+        # The identity
+        if not identity.startswith(get_platform_instance_name()):
+            json_response = dict(status="FAILURE",
+                                 message="CSR must start with instance name: {}".format(
+                                     get_platform_instance_name()))
+            Response(json.dumps(json_response),
+                     content_type='application/json',
+                     headers={'Content-type': 'application/json'})
 
-        csr_file = self._certs.csr_create_file(instance_new_name)
+        cert_identity = self._certs.get_csr_common_name(str(csr))  # csr.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value csr.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+        csr_file = self._certs.csr_create_file(identity)
         if csr:
             with open(csr_file, "wb") as fw:
                 fw.write(csr)
 
         auto_accept = True
         if auto_accept:
-            _log.debug("Creating cert and permissions for user: {}".format(instance_new_name))
-            permissions = self._core().rmq_mgmt.get_default_permissions(instance_new_name)
-            self._core().rmq_mgmt.create_user_with_permissions(instance_new_name,
+            _log.debug("Creating cert and permissions for user: {}".format(identity))
+            permissions = self._core().rmq_mgmt.get_default_permissions(identity)
+            self._core().rmq_mgmt.create_user_with_permissions(identity,
                                                                permissions,
                                                                True)
             cert = self._certs.sign_csr(csr_file)
