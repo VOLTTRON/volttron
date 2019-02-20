ab1411039b1fe4cafd66e310d5e8506c48204d2d
Craig <craig.allwardt@pnnl.gov>
2019-02-20 07:30:40 -0800

    Remote Listener Delete

--- examples/RemoteListener/install-listener.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-VOLTTRON_ROOT=$HOME/repos/volttron-rabbitmq
-
-cmd = "$VOLTTRON_ROOT/scripts/install-agent.py"
-
-python $cmd -s "$VOLTTRON_ROOT/examples/RemoteListener" \
-    -i "remote.agent"
