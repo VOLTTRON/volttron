6361337594477db9f9d092746c1cc1d10309b32e
schandrika <chandrika@pnnl.gov>
2019-03-04 14:58:23 -0800

    Checking for erlang install before rmq install

--- bootstrap.py
+++ bootstrap.py
@@ -81,6 +81,8 @@ import sys
 import os
 from distutils.version import LooseVersion
 import traceback
+from volttron.platform.agent.utils import execute_command
+
 
 _log = logging.getLogger(__name__)
 
@@ -276,6 +278,12 @@ def update(operation, verbose=None, upgrade=False, offline=False,
 
 
 def install_rabbit(rmq_install_dir):
+    try:
+        execute_command(['which', 'erl'])
+    except RuntimeError as e:
+        sys.stderr.write("\nERROR:\nUnable to find erlang in path. Please install necessary pre-requisites. "
+                   "Reference: https://github.com/schandrika/volttron/blob/rabbitmq-volttron/README.md")
+        sys.exit(60)
     import wget
     if rmq_install_dir == default_rmq_dir and not os.path.exists(
             default_rmq_dir):
