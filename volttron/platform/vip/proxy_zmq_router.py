adc4b7206adda7e27990731b9a6f3baea165fc5e
Shwetha <shwetha.niddodi@pnnl.gov>
2019-03-26 23:18:53 -0700

    Fixes for Issue#1962. Proxy ZMQ router agent now sends new peer add or drop message to RMQ router so that it has comeprehensiv list of all agent connections

--- volttron/platform/vip/proxy_zmq_router.py
+++ volttron/platform/vip/proxy_zmq_router.py
@@ -152,22 +152,33 @@ class ZMQProxyRouter(Agent):
         routing_key = str(method.routing_key)
         platform, to_identity = routing_key.split(".", 1)
         platform, from_identity = props.app_id.split(".", 1)
+        op = None
         try:
             args = json.loads(body)
-            args = json.loads(args[0])
+            try:
+                # This is necessary because jsonrpc request/response is inside a list which the
+                # ZMQ agent subsystem does not like
+                args = json.loads(args[0])
+            except ValueError as e:
+                if isinstance(args, list) and len(args) > 1:
+                    op = args.pop(0)
         except TypeError as e:
             # TODO: to be fixed
             _log.error("Invalid json format {}".format(e))
             return
         userid = props.headers.get('user', b'')
-        # _log.debug("Proxy ZMQ Router Outbound handler {0}, {1}".format(to_identity, args))
+
+        _log.debug("Proxy ZMQ Router Outbound handler {0}, {1}".format(to_identity, args))
         # Reformat message into ZMQ VIP format
         frames = [bytes(to_identity), bytes(from_identity), b'VIP1', bytes(userid),
-                  bytes(props.message_id), bytes(props.type), json.dumps(args)]
+                  bytes(props.message_id), bytes(props.type)]
+        if op is not None:
+            frames.append(bytes(op))
+        frames.append(json.dumps(args))
         try:
             self.zmq_router.socket.send_multipart(frames, copy=True)
         except ZMQError as ex:
-            _log.debug("ZMQ Error {}".format(ex))
+            _log.error("ZMQ Error {}".format(ex))
 
     def outbound_request_handler(self, ch, method, props, body):
         """
@@ -220,6 +231,12 @@ class ZMQProxyRouter(Agent):
                 frames = self.zmq_router.socket.recv_multipart(copy=False)
                 sender, recipient, proto, auth_token, msg_id, subsystem = frames[:6]
                 recipient = bytes(recipient)
+                subsystem = bytes(subsystem)
+                if subsystem == b'hello':
+                    self.vip.peerlist.add_peer(sender)
+                elif subsystem == b'agentstop':
+                    self.vip.peerlist.drop_peer(sender)
+
                 if not recipient:
                     # Handle router specific messages
                     self.zmq_router.route(frames)
