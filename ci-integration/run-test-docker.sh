e81eef635655710db12efdefb19bd4d227492dcb
Shwetha <shwetha.niddodi@pnnl.gov>
2019-03-28 09:53:46 -0700

    run-test-docker.sh modified to run multiple tests and wait for them to finish and print error logs

--- ci-integration/run-test-docker.sh
+++ ci-integration/run-test-docker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #Preliminary script to run pytests in separate docker containers
 
-docker build --network=host -t volttron_test_base -f ./ci-integration/virtualization/Dockerfile .
-docker build --network=host -t volttron_test_image -f ./ci-integration/virtualization/Dockerfile.testing .
+#docker build --network=host -t volttron_test_base -f ./ci-integration/virtualization/Dockerfile .
+#docker build --network=host -t volttron_test_image -f ./ci-integration/virtualization/Dockerfile.testing .
 
 #testdirs="examples services volttron volttrontesting"
-testdirs="services/core/ActuatorAgent"
+testdirs="services"
 
+NUM_PROCESSES=$1
+
+pids=""
+i=0
+tasks=()
+cids=()
 for dir in ${testdirs[@]}
 do
     files=`find $dir -type f -name "*.py"`
@@ -15,9 +21,17 @@ do
     do
         #echo $filename
         base_filename=`basename $filename`
-        if [[ $base_filename == *"test"* ]]; then
+        if [[ $base_filename == *"test"* ]] && [[ $base_filename != "conftest.py" ]]; then
             echo $filename
-            docker run -e "IGNORE_ENV_CHECK=1" -it volttron_test_image pytest $filename &> "$base_filename.result.txt" &
+            docker run -e "IGNORE_ENV_CHECK=1" -t volttron_test_image pytest $filename &
+            pids="$pids $!"
+            echo $pids
+            cids[$i]=`docker ps -l -q`
+            task[$i]=$filename
+            echo ${task[$i]}
+            let i++
         fi
     done
 done
+
+
