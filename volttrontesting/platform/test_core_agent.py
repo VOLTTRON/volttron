c92f8f0968029740e6d3302db0e1582a7152a2d1
C. Allwardt <craig.allwardt@pnnl.gov>
2019-04-19 14:00:04 -0400

    Fixed core agent health specific tests.

--- volttrontesting/platform/test_core_agent.py
+++ volttrontesting/platform/test_core_agent.py
@@ -28,10 +28,17 @@ def test_agent_can_get_platform_version(volttron_instance):
 def test_agent_status_set_when_created(volttron_instance):
     agent = volttron_instance.build_agent()
     assert agent.vip.health.get_status() is not None
-    assert isinstance(agent.vip.health.get_status(), str)
-    l = json.loads(agent.vip.health.get_status())
+    assert isinstance(agent.vip.health.get_status(), dict)
+    l = agent.vip.health.get_status()
     assert l['status'] == STATUS_GOOD
     assert l['context'] is None
+
+    assert isinstance(agent.vip.health.get_status_json(), str)
+    l = json.loads(agent.vip.health.get_status_json())
+    assert l['status'] == STATUS_GOOD
+    assert l['context'] is None
+
+    assert agent.vip.health.get_status_value() == STATUS_GOOD
 
 
 @pytest.mark.agent
@@ -40,12 +47,12 @@ def test_agent_status_changes(volttron_instance):
     bad_message = "Bad kitty"
     agent = volttron_instance.build_agent()
     agent.vip.health.set_status(STATUS_UNKNOWN, unknown_message)
-    r = json.loads(agent.vip.health.get_status())
+    r = agent.vip.health.get_status()
     assert unknown_message == r['context']
     assert STATUS_UNKNOWN == r['status']
 
     agent.vip.health.set_status(STATUS_BAD, bad_message)
-    r = json.loads(agent.vip.health.get_status())
+    r = agent.vip.health.get_status()
     assert bad_message == r['context']
     assert STATUS_BAD == r['status']
 
@@ -53,10 +60,10 @@ def test_agent_status_changes(volttron_instance):
 @pytest.mark.agent
 def test_agent_last_update_increases(volttron_instance):
     agent = volttron_instance.build_agent()
-    s = json.loads(agent.vip.health.get_status())
+    s = agent.vip.health.get_status()
     dt = dateparse(s['last_updated'], fuzzy=True)
     agent.vip.health.set_status(STATUS_UNKNOWN, 'Unknown now!')
     gevent.sleep(1)
-    s = json.loads(agent.vip.health.get_status())
+    s = agent.vip.health.get_status()
     dt2 = dateparse(s['last_updated'], fuzzy=True)
     assert dt < dt2
