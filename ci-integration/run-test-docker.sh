b2b89139004e33b6dfcfae4dd58e6b0a9990fdd7
C. Allwardt <craig.allwardt@pnnl.gov>
2019-04-29 10:33:18 -0400

    Added documentation

--- ci-integration/run-test-docker.sh
+++ ci-integration/run-test-docker.sh
@@ -1,62 +1,121 @@
 #!/bin/bash
-#Preliminary script to run pytests in separate docker containers
+
+# The following script builds a volttron test image and then
+# runs each of the test modules inside a docker container based
+# upon the test image.
 
 export FAST_FAIL=true
 
+# A possible argument passed to the script is the number docker containers
+# to run at a single time.
 if [[ $# -eq 0 ]]; then
-    NUM_PROCESSES=5
+    NUM_PROCESSES=3
 else
     NUM_PROCESSES=$1
 fi
 
 echo "RUNNING $NUM_PROCESSES PARALLEL PROCESSESS AT A TIME"
 
+# Before actually running odcker containers prune all dangling images
+# and stopped containers.
 docker system prune --force
 
-declare -a testqueue
-declare -a runningprocs
-declare -a outputfiles
-declare -a containernames
-
-#docker system prune --force
+# Declare variables to maintain the state of the running
+# docker images.
+declare -a testqueue        # Holds all of the queued test modules
+declare -a runningprocs     # Holds the currently running docker processes
+declare -a outputfiles      # Holds the output files from the logs of the docker processes
+declare -a containernames   # Holds the name of the containers that have been started
 
 #docker build --network=host -t volttron_test_base -f ./ci-integration/virtualization/Dockerfile .
 #docker build --network=host -t volttron_test_image -f ./ci-integration/virtualization/Dockerfile.testing .
 
+# Specific directories to scan for tests in
 #testdirs=(examples services volttron volttrontesting)
 testdirs=(volttrontesting)
+
+# State variable for when a test has failed the entire set needs to be considered
+# failed.
 HAS_FAILED=0
 
+# method to push a test module into the queue
 push_test(){
     testpath="$1"
     testqueue+=($testpath)
 }
 
+# method to get a test module out of the queue
+# the variable next_test is available after this function to be used
+# with the popped value.
 pop_test(){
     next_test=${testqueue[0]}
     testqueue=("${testqueue[@]:1}")
 }
 
+# Starts a single test module running.  This updates all of the
+# global state arrays.
 run_test(){
 #    bash /home/osboxes/repos/volttron-rabbitmq/ci-integration/sleep_ten.sh &
     local filename=$1;
     echo "Running test module $filename"
     base_filename=`basename $filename`
+    # Start the docker run module.
     docker run -e "IGNORE_ENV_CHECK=1" --name $base_filename \
         -t volttron_test_image pytest $filename > "$base_filename.result.txt" 2>&1 &
     runningprocs+=($!)
-    sleep 1
     outputfiles+=("$base_filename.result.txt")
     containernames+=($base_filename)
+    sleep 0.5
 }
 
+
+# This method is used to clean up containers when FAST_FAIL is set to true
+# and failed tests are found.
 exit_cleanly(){
-    for container in containernames; do
+    for container in ${containernames[@]}; do
         docker stop $container
         docker container rm $container
     done
     docker system prune --force
     exit 1
+}
+
+# Process one of the docker pid files.
+process_pid(){
+    local index=$1
+    local pid=${runningprocs[$index]}
+
+    echo "Processing $index with $pid"
+    # Test whether or not the process id running the docker container
+    # is still executing.  If it is not then we need to see what the
+    # exit code was of the container.
+    if [[ ! -e "/proc/${pid}" ]]; then
+        exitcode=$(docker inspect ${containernames[$index]} --format='{{.State.ExitCode}}')
+
+        echo "Exit code is ${exitcode}"
+        if [[ $exitcode -ne 0 ]]; then
+            echo "module ${containernames[$index]} FAILED"
+            HAS_FAILED=1
+            if [[ ${FAST_FAIL} ]]; then
+                exit_cleanly
+            fi
+        else
+            # process passed so cleanup the result file.
+            rm ${outputfiles[$index]}
+            echo "module ${containernames[$index]} PASSED"
+        fi
+
+        # Clean up the test container now that this process is done.
+        docker container rm ${containernames[$index]}
+
+        # Remove pid from the array of running procs.
+        runningprocs=( ${runningprocs[@]:0:$index} ${runningprocs[@]:$((index + 1))} )
+        outputfiles=( ${outputfiles[@]:0:$index} ${outputfiles[@]:$((index + 1))} )
+        containernames=( ${containernames[@]:0:$index} ${containernames[@]:$((index + 1))} )
+    fi
+    i=$(( i+1 ))
+    sleep 1
+
 }
 
 #LOOP through set of directories and run bunch of test files in parallel
@@ -71,8 +130,10 @@ done
 
 echo "There are ${#testqueue[@]} test modules to run";
 
+# Lopo through the queue until there isn't any left
 while [[ ${#testqueue[@]} -gt 0 ]]; do
-    # echo "looping procs runing are ${#runningprocs[@]}  num_procs is ${NUM_PROCESSES} testqueue is ${#testqueue[@]} "
+
+    # Start the number of processes requested
     while [[ ${#runningprocs[@]} -lt ${NUM_PROCESSES} && ${#testqueue[@]} -gt 0 ]]; do
         # pop the front of the queue into the next_test variable
         pop_test
@@ -80,76 +141,23 @@ while [[ ${#testqueue[@]} -gt 0 ]]; do
     done
 
     i=0
-
+    # Loop through processes that are running.  Check each process in the
+    # array and process the return code for each process that is not running
+    # any longer.
     while [[ $i -lt ${#runningprocs[@]} ]]; do
-
-        pid=${runningprocs[$i]}
-        # Test whether or not the process id running the docker container
-        # is still executing.  If it is not then we need to see what the
-        # exit code was of the container.
-        if [[ ! -e "/proc/${pid}" ]]; then
-            exitcode=$(docker inspect ${containernames[$i]} --format='{{.State.ExitCode}}')
-
-            if [[ ! $exitcode ]]; then
-                HAS_FAILED=1
-                if [[ ${FAST_FAIL} ]]; then
-                    exit_cleanly
-                fi
-                echo "module ${containernames[$i]} FAILED"
-            else
-                rm ${outputfiles[$i]}
-                echo "module ${containernames[$i]} PASSED"
-            fi
-
-            # Clean up the test container now that this process is done.
-            docker container rm ${containernames[$i]}
-
-            # Remove pid from the array of running procs.
-            runningprocs=( ${runningprocs[@]:0:$i} ${runningprocs[@]:$((i + 1))} )
-            outputfiles=( ${outputfiles[@]:0:$i} ${outputfiles[@]:$((i + 1))} )
-            containernames=( ${containernames[@]:0:$i} ${containernames[@]:$((i + 1))} )
-        fi
-        i=$(( i+1 ))
-        sleep 1
+        process_pid $i
     done
 done
 
-
+# Final loop to finish the running processes before exiting the script.
 while [[ ${#runningprocs[@]} -gt 0 ]]; do
     i=0
 
+    # Loop through processes that are running.  Check each process in the
+    # array and process the return code for each process that is not running
+    # any longer.
     while [[ $i -lt ${#runningprocs[@]} ]]; do
-
-        pid=${runningprocs[$i]}
-        # Test whether or not the process id running the docker container
-        # is still executing.  If it is not then we need to see what the
-        # exit code was of the container.
-        if [[ ! -e "/proc/${pid}" ]]; then
-
-            exitcode=$(docker inspect ${containernames[$i]} --format='{{.State.ExitCode}}')
-
-            if [[ ! $exitcode ]]; then
-                HAS_FAILED=1
-                if [[ ${FAST_FAIL} ]]; then
-                    exit_cleanly
-                fi
-                echo "module ${containernames[$i]} FAILED"
-            else
-                rm ${outputfiles[$i]}
-                echo "module ${containernames[$i]} PASSED"
-            fi
-
-            # Clean up the test container now that this process is done.
-            docker container rm ${containernames[$i]}
-
-            # Remove pid from the array of running procs.
-            runningprocs=( ${runningprocs[@]:0:$i} ${runningprocs[@]:$((i + 1))} )
-            outputfiles=( ${outputfiles[@]:0:$i} ${outputfiles[@]:$((i + 1))} )
-            containernames=( ${containernames[@]:0:$i} ${containernames[@]:$((i + 1))} )
-            # echo "Process running ${#runningprocs[@]}"
-        fi
-        i=$(( i+1 ))
-        sleep 0.5
+        process_pid $i
     done
 done
 
