764c0ac5c28e9e577595a287c30794f8ef4755bc
schandrika <chandrika@pnnl.gov>
2019-11-04 18:23:21 -0800

    corrected the user name used for volttron platform. Added check for existing instance name

--- scripts/security_user_permissions.sh
+++ scripts/security_user_permissions.sh
@@ -5,31 +5,47 @@ if [ -z "$UID" ] || [ $UID -ne 0 ]; then
   exit
 fi
 
-if [ -z ${USER} ]; then
- echo '$USER is not set. Finding user name for UID 0'
- USER=`awk -v val=0 -F ":" '$3==val{print $1}' /etc/passwd`
- echo "USER got based on uid 0 is $USER"
-fi
+echo -n "Enter volttron platform user. User would be provided sudo access to volttron related commands:"
+read volttron_user
 
 echo "Creating volttron_agent user group"
 echo "Adding Permissions to sudoers for VOLTTRON:"
 # allow user to add and delete users using the volttron agent user pattern
 while true; do
+    valid=0
     echo -n "Enter Volttron instance name (volttron_<instance name>must be valid unix group):"
     read name
     if [[ $name =~ ^[a-z_]([a-z0-9_-]{1,23}|[a-z0-9_-]{1,23}\$)$ ]]
     # TODO check if that instance name is taken
     then
-        echo "Setting Volttron instance name to volttron_$name"
-        break
+        if [ -f "/etc/sudoers.d/volttron" ]; then
+            exists=`grep "volttron_$name" /etc/sudoers.d/volttron`
+            if [ -z "$exists" ]; then
+                valid=1
+            else
+                echo "Entries exists in /etc/sudoers.d/volttron for instance name $name"
+                echo -n "Do you want to setup a different instance of volttron(Y/N)"
+                read continue
+                if [ $continue == "N" ] || [ $continue == "N" ]; then
+                    exit
+                fi
+            fi
+        else
+            valid=1
+        fi
+
+        if [ $valid -eq 1 ]; then
+            echo "Setting Volttron instance name to volttron_$name"
+            break
+        fi
     fi
 done
 
-echo "$USER ALL= NOPASSWD: /usr/sbin/groupadd volttron_$name" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
-echo "$USER ALL= NOPASSWD: /usr/sbin/usermod -a -G volttron_$name $USER" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
-echo "$USER ALL= NOPASSWD: /usr/sbin/useradd volttron_* -r -G volttron_$name" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
+echo "$volttron_user ALL= NOPASSWD: /usr/sbin/groupadd volttron_$name" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
+echo "$volttron_user ALL= NOPASSWD: /usr/sbin/usermod -a -G volttron_$name $USER" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
+echo "$volttron_user ALL= NOPASSWD: /usr/sbin/useradd volttron_* -r -G volttron_$name" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
 # TODO want delete only users with pattern of particular group
-echo "$USER ALL= NOPASSWD: /usr/sbin/userdel volttron_*" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
+echo "$volttron_user ALL= NOPASSWD: /usr/sbin/userdel volttron_*" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
 # allow user to run all non-sudo commands for all volttron agent users
-echo "$USER ALL=(%volttron_$name) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
-echo "Permissions set for $USER"
+echo "$volttron_user ALL=(%volttron_$name) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/volttron
+echo "Permissions set for $volttron_user"
