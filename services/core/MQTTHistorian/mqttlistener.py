d50bb735d6651f7b6c32406861a5d76513959c21
jklarson <james.larson@pnnl.gov>
2020-03-19 09:13:56 -0700

    Added config tests for MQTT Historian, made several pep8 updates, fixed paho requirement

--- services/core/MQTTHistorian/mqttlistener.py
+++ services/core/MQTTHistorian/mqttlistener.py
@@ -4,9 +4,11 @@ from paho.mqtt.subscribe import callback
 PORT = 5000
 PROTOCOL = MQTTv311
 
+
 # Callback function to print out message topics and payloads
 def listen(client, userdata, message):
     print(message.topic, message.payload)
+
 
 # Subscribe to all messages and loop forever
 callback(listen, '#', port=PORT, protocol=PROTOCOL)
