e3cc2fe1d820bc29eb9bf5063fbf35c2d07958eb
Shwetha Niddodi <shwetha.niddodi@pnnl.gov>
2020-04-10 15:43:01 -0700

    Update auth.py

--- volttron/platform/auth.py
+++ volttron/platform/auth.py
@@ -892,6 +892,8 @@ class AuthFile(object):
             with open(self.auth_file) as fil:
                 # Use gevent FileObject to avoid blocking the thread
                 before_strip_comments = FileObject(fil, close=False).read()
+                if isinstance(before_strip_comments, bytes):
+                    before_strip_comments.decode("utf-8")
                 data = strip_comments(before_strip_comments)
                 if data:
                     auth_data = jsonapi.loads(data)
