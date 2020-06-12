ca8aae8b98f30699256314ebfee46a01be21db31
jklarson <james.larson@pnnl.gov>
2020-06-12 11:24:23 -0700

    Ecobee now uses persistent dict, data is fetched per-device, and tests added

--- services/core/MasterDriverAgent/tests/test_ecobee_driver.py
+++ services/core/MasterDriverAgent/tests/test_ecobee_driver.py
@@ -1,18 +1,15 @@
 import copy
+import datetime
 import requests
+from requests.exceptions import HTTPError
 import pytest
 
-from volttron.platform.jsonrpc import RemoteError
-from services.core.MasterDriverAgent.master_driver.interfaces import BaseInterface
 from services.core.MasterDriverAgent.master_driver.interfaces import ecobee
-
-BaseInterface.__abstractmethods__ = frozenset()
+from volttron.platform.agent import utils
 
 VALID_ECOBEE_CONFIG = {
     "API_KEY": "TEST_KEY",
     "DEVICE_ID": 8675309,
-    "GROUP_ID": "test",
-    "CACHE_IDENTITY": "platform.drivercache"
 }
 
 VALID_ECOBEE_REGISTRY = [
@@ -40,6 +37,27 @@ VALID_ECOBEE_REGISTRY = [
     }
 ]
 
+REMOTE_RESPONSE = {
+    "thermostatList": [
+        {
+            "identifier": 8675309,
+            "settings": {
+                "setting1": 0,
+                "setting2": 1
+            },
+            "runtime": {
+                "hold1": 0,
+                "hold2": 1
+            },
+            "events": [
+                {"test1": "test1", "type": "program"},
+                {"test2": "test2", "type": "vacation"}
+            ],
+            "equipmentStatus": "testEquip1,testEquip3"
+        }
+    ]
+}
+
 
 class MockEcobee(ecobee.Interface):
 
@@ -50,7 +68,7 @@ class MockEcobee(ecobee.Interface):
         self.access_token = False
         self.refresh_token = False
         self.refresh_state = False
-        self.poll_greenlet = "test"
+        self.poll_greenlet_thermostats = "test"
 
     def get_auth_config_from_store(self):
         if not self.auth_config_stored:
@@ -63,7 +81,7 @@ class MockEcobee(ecobee.Interface):
             }
 
     def update_auth_config(self):
-        return
+        self.auth_config_stored = True
 
     def authorize_application(self):
         self.authorization_code = True
@@ -85,44 +103,13 @@ class MockEcobee(ecobee.Interface):
         else:
             raise requests.exceptions.HTTPError("Not authorized to refresh tokens")
 
-    def get_ecobee_data_from_cache(self, refresh=False):
-        if self.access_token is True:
-            self.ecobee_data = {
-                "thermostatList": [
-                    {
-                        "identifier": 8675309,
-                        "settings": {
-                            "setting1": 0,
-                            "setting2": 1
-                        },
-                        "runtime": {
-                            "hold1": 0,
-                            "hold2": 1
-                        },
-                        "events": [
-                            {"test1": "test1", "type": "program"},
-                            {"test2": "test2", "type": "vacation"}
-                        ]
-                    }, {
-                        "identifier": 1,
-                        "settings": {
-                            "setting1": 3,
-                            "setting2": 4
-                        },
-                        "runtime": {
-                            "hold1": 3,
-                            "hold2": 4
-                        }
-                    }, {
-                        "events": [
-                            {"test3": "test3", "type": "program"},
-                            {"test4": "test4", "type": "vacation"}
-                        ]
-                    }
-                ]
-            }
+    def get_data_remote(self, request_type, url, **kwargs):
+        if self.authorization_stage != "AUTHORIZED" or not self.access_token:
+            self.update_authorization()
+        if self.authorization_stage == "AUTHORIZED" and self.access_token:
+            return REMOTE_RESPONSE
         else:
-            raise RemoteError("Time to refresh tokens")
+            raise HTTPError("Failed to get remote Ecobee data")
 
 
 @pytest.fixture()
@@ -202,23 +189,13 @@ def test_configure_ecobee_success(mock_ecobee):
     assert mock_ecobee.refresh_token
     assert mock_ecobee.access_token
     # Test configure from existing auth
-    assert mock_ecobee.group_id == "test"
-    auth_config_path = ecobee.AUTH_CONFIG_PATH.format("test")
+    auth_config_path = ecobee.AUTH_CONFIG_PATH.format(VALID_ECOBEE_CONFIG.get("DEVICE_ID"))
     assert mock_ecobee.auth_config_path == auth_config_path
-    # TODO compare list of registers
     assert mock_ecobee.authorization_stage == "AUTHORIZED"
-    settings = {register.point_name for register in
-                mock_ecobee.get_registers_by_type("setting", True) + mock_ecobee.get_registers_by_type("setting", False)}
-    holds = {register.point_name for register in
-             mock_ecobee.get_registers_by_type("hold", True) + mock_ecobee.get_registers_by_type("hold", False)}
-    program = mock_ecobee.get_register_by_name("Programs").point_name
-    vacation = mock_ecobee.get_register_by_name("Vacations").point_name
-    status = mock_ecobee.get_register_by_name("Status").point_name
-    assert {"testNoRead", "testSetting"} == settings
-    assert {"testHold"} == holds
-    assert program == "Programs"
-    assert vacation == "Vacations"
-    assert status == "Status"
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    registers = {register.point_name for register in
+                 mock_ecobee.get_registers_by_type("byte", False) + mock_ecobee.get_registers_by_type("byte", True)}
+    assert {"testNoRead", "testSetting", "testHold", "Programs", "Vacations", "Status"} == registers
 
 
 def test_configure_ecobee_invalid_id(mock_ecobee):
@@ -239,19 +216,10 @@ def test_configure_ecobee_invalid_registers(mock_ecobee):
         "Readable": "True"
     }, VALID_ECOBEE_REGISTRY[1]]
     mock_ecobee.configure(VALID_ECOBEE_CONFIG, no_point_name)
-    settings = {register.point_name for register in
-                mock_ecobee.get_registers_by_type("setting", True) + mock_ecobee.get_registers_by_type("setting",
-                                                                                                       False)}
-    holds = {register.point_name for register in
-             mock_ecobee.get_registers_by_type("hold", True) + mock_ecobee.get_registers_by_type("hold", False)}
-    program = mock_ecobee.get_register_by_name("Programs").point_name
-    vacation = mock_ecobee.get_register_by_name("Vacations").point_name
-    status = mock_ecobee.get_register_by_name("Status").point_name
-    assert {"testSetting"} == settings
-    assert not holds
-    assert program == "Programs"
-    assert vacation == "Vacations"
-    assert status == "Status"
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    registers = {register.point_name for register in
+                 mock_ecobee.get_registers_by_type("byte", False) + mock_ecobee.get_registers_by_type("byte", True)}
+    assert {"testSetting", "Programs", "Vacations", "Status"} == registers
 
     # An unsupported type should cause no point to be added, but no error to be thrown
     # all other registers should still be built
@@ -263,59 +231,204 @@ def test_configure_ecobee_invalid_registers(mock_ecobee):
         "Readable": "True"
     }, VALID_ECOBEE_REGISTRY[1]]
     mock_ecobee.configure(VALID_ECOBEE_CONFIG, no_point_name)
-    settings = {register.point_name for register in
-                mock_ecobee.get_registers_by_type("setting", True) + mock_ecobee.get_registers_by_type("setting",
-                                                                                                       False)}
-    holds = {register.point_name for register in
-             mock_ecobee.get_registers_by_type("hold", True) + mock_ecobee.get_registers_by_type("hold", False)}
-    program = mock_ecobee.get_register_by_name("Programs").point_name
-    vacation = mock_ecobee.get_register_by_name("Vacations").point_name
-    status = mock_ecobee.get_register_by_name("Status").point_name
-    assert {"testSetting"} == settings
-    assert not holds
-    assert program == "Programs"
-    assert vacation == "Vacations"
-    assert status == "Status"
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    registers = {register.point_name for register in
+                 mock_ecobee.get_registers_by_type("byte", False) + mock_ecobee.get_registers_by_type("byte", True)}
+    assert {"testSetting", "Programs", "Vacations", "Status"} == registers
+
+
+def test_get_thermostat_data_success(mock_ecobee):
+    mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    curr_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+
+    # Check that we get cached data when possible
+    mock_ecobee.get_thermostat_data()
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    refresh_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+    assert refresh_timestamp == curr_timestamp
+
+    # cause a request_tokens request to occur during get_ecobee_data
+    mock_ecobee.authorization_code = True
+    mock_ecobee.refresh_token = True
+    mock_ecobee.access_token = True
+    mock_ecobee.ecobee_data = None
+    cleanup_mock_cache(mock_ecobee)
+    mock_ecobee.get_thermostat_data()
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    refresh_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+    assert refresh_timestamp > curr_timestamp
+
+    # should handle having to get a new refresh token and still fetch data
+    mock_ecobee.access_token = False
+    mock_ecobee.authorization_stage = "REFRESH_TOKENS"
+    mock_ecobee.ecobee_data = None
+    cleanup_mock_cache(mock_ecobee)
+    mock_ecobee.get_thermostat_data()
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    assert mock_ecobee.access_token is True
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    refresh_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+    assert refresh_timestamp > curr_timestamp
+
+    # should handle having to get a new refresh token and still fetch data
+    mock_ecobee.refresh_token = False
+    mock_ecobee.access_token = False
+    mock_ecobee.authorization_stage = "REQUEST_TOKENS"
+    mock_ecobee.ecobee_data = None
+    cleanup_mock_cache(mock_ecobee)
+    mock_ecobee.get_thermostat_data()
+    assert mock_ecobee.thermostat_data == REMOTE_RESPONSE
+    assert mock_ecobee.access_token is True
+    assert mock_ecobee.refresh_token is True
+
+    # now should pull from cache again
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    timestamp = data_cache.get("request_timestamp")
+    mock_ecobee.get_thermostat_data()
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    next_timestamp = data_cache.get("request_timestamp")
+    assert timestamp == next_timestamp
+
+def test_get_thermostat_data_no_auth(mock_ecobee):
+    mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
+    mock_ecobee.authorization_code = False
+    mock_ecobee.refresh_token = False
+    mock_ecobee.access_token = False
+    mock_ecobee.ecobee_data = None
+    cleanup_mock_cache(mock_ecobee)
+    with pytest.raises(HTTPError, match=r""):
+        mock_ecobee.get_thermostat_data()
+    assert mock_ecobee.ecobee_data is None
+
+
+def cleanup_mock_cache(mock_ecobee):
+    pop_keys = list(mock_ecobee.cache.keys())
+    for key in pop_keys:
+        mock_ecobee.cache.pop(key)
 
 
 @pytest.mark.parametrize("point_name,expected_value", [("testSetting", 0),
                                                        ("testHold", 0),
                                                        ("Programs", [{"test1": "test1", "type": "program"}]),
-                                                       ("Vacations", [{"test2": "test2", "type": "vacation"}])
-                                                       ]
-                         )
+                                                       ("Vacations", [{"test2": "test2", "type": "vacation"}]),
+                                                       ("Status", ["testEquip1", "testEquip3"])
+                                                       ])
 def test_ecobee_get_point_success(mock_ecobee, point_name, expected_value):
     mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
     assert mock_ecobee.get_point(point_name) == expected_value
+    # Set the Ecobee data to None to try to force to the ValueError check which resets the Ecobee data
+    mock_ecobee.thermostat_data = None
+    assert mock_ecobee.get_point(point_name) == expected_value
 
 
+def test_ecobee_get_point_unreadable(mock_ecobee):
+    mixed_readable_registry = [{
+        "Point Name": "hold1",
+        "Volttron Point Name": "testHold",
+        "Units": "%",
+        "Type": "hold",
+        "Writable": "True",
+        "Readable": "True"
+    }, {
+        "Point Name": "setting1",
+        "Volttron Point Name": "testSetting",
+        "Units": "degC",
+        "Type": "setting",
+        "Writable": "False",
+        "Readable": "True"
+    }, {
+        "Point Name": "hold2",
+        "Volttron Point Name": "testHoldNoRead",
+        "Units": "%",
+        "Type": "hold",
+        "Writable": "True",
+        "Readable": "False"
+    }, {
+        "Point Name": "setting2",
+        "Volttron Point Name": "testSettingNoRead",
+        "Units": "degC",
+        "Type": "setting",
+        "Writable": "False",
+        "Readable": "False"
+    }]
+    mock_ecobee.configure(VALID_ECOBEE_CONFIG, mixed_readable_registry)
+    assert mock_ecobee.get_point("testHold") == 0
+    assert mock_ecobee.get_point("testSetting") == 0
+    with pytest.raises(RuntimeError, match=r"Requested read of write-only point testHoldNoRead"):
+        mock_ecobee.get_point("testHoldNoRead")
+    with pytest.raises(RuntimeError, match=r"Requested read of write-only point testSettingNoRead"):
+        mock_ecobee.get_point("testSettingNoRead")
 
 
-# def test_get_ecobee_data_success(mock_ecobee):
-#     # cause a request_tokens request to occur during get_ecobee_data
-#     mock_ecobee.authorization_code = True
-#     mock_ecobee.refresh_token = True
-#     mock_ecobee.access_token = Truet
-#     mock_ecobee.authorization_stage = "REFRESH_TOKENS"
-#     mock_ecobee.ecobee_data = None
-#     # should handle having to get a new refresh token and still fetch data
-#     mock_ecobee.get_ecobee_data()
-#     assert mock_ecobee.refresh_token is True
-#     assert mock_ecobee.access_token is True
-#     assert isinstance(mock_ecobee, dict)
-#
-#
-# def test_ecobee_get_point_success(mock_ecobee):
-#     mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
-#     hold_data = mock_ecobee.get_point("testHold")
-#     setting_data = mock_ecobee.get_point("testSetting")
-#     status_data = mock_ecobee.get_point("Status")
-#     vacation_data = mock_ecobee.get_point("Vacation")
-#     program_data = mock_ecobee.get_point("Program")
-#
-#     assert hold_data
-#     assert setting_data
-#     assert status_data
-#     assert vacation_data
-#     assert program_data
+@pytest.mark.parametrize("point_name,expected_value", [("testSetting", 0),
+                                                       ("testHold", 0),
+                                                       ("Programs", [{"test1": "test1", "type": "program"}]),
+                                                       ("Vacations", [{"test2": "test2", "type": "vacation"}]),
+                                                       ("Status", ["testEquip1", "testEquip3"])
+                                                       ])
+def test_get_point_malformed_data(mock_ecobee, point_name, expected_value):
+    mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    curr_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
 
+    # Malformed data should cause ValueErrors, which then trigger the data to be refreshed
+    mock_ecobee.thermostat_data = { }
+    assert mock_ecobee.get_point(point_name) == expected_value
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    refresh_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+    assert refresh_timestamp > curr_timestamp
+    curr_timestamp = refresh_timestamp
+    mock_ecobee.thermostat_data = {
+        "thermostatsList": [{
+            "identifier": 8675309,
+        }]
+    }
+    assert mock_ecobee.get_point(point_name) == expected_value
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    refresh_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+    assert refresh_timestamp > curr_timestamp
+    curr_timestamp = refresh_timestamp
+    mock_ecobee.thermostat_data = {
+        "thermostatsList": [{
+            "identifier": 8675309,
+            "settings": {},
+            "runtime": {},
+            "events": [""]
+        }]
+    }
+    assert mock_ecobee.get_point(point_name) == expected_value
+    data_cache = mock_ecobee.cache.get('https://api.ecobee.com/1/thermostat')
+    refresh_timestamp = utils.parse_timestamp_string(data_cache.get("request_timestamp"))
+    assert refresh_timestamp > curr_timestamp
+
+
+def test_scrape_all_success(mock_ecobee):
+    mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
+    all_scrape = mock_ecobee._scrape_all()
+    result = {
+        "testSetting": 0,
+        "testHold": 0,
+        "Status": ["testEquip1", "testEquip3"],
+        "Vacations": [{"test2": "test2", "type": "vacation"}],
+        "Programs": [{"test1": "test1", "type": "program"}]
+    }
+    assert result == all_scrape
+
+
+def test_scrape_all_trigger_refresh(mock_ecobee):
+    mock_ecobee.configure(VALID_ECOBEE_CONFIG, VALID_ECOBEE_REGISTRY)
+    mock_ecobee.thermostat_data = None
+    cleanup_mock_cache(mock_ecobee)
+    all_scrape = mock_ecobee._scrape_all()
+    result = {
+        "testSetting": 0,
+        "testHold": 0,
+        "Status": ["testEquip1", "testEquip3"],
+        "Vacations": [{"test2": "test2", "type": "vacation"}],
+        "Programs": [{"test1": "test1", "type": "program"}]
+    }
+    assert result == all_scrape
