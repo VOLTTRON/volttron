f02f3b9634243cc0ad24c13ac8a52594d5ac8c5d
Shwetha Niddodi <shwetha.niddodi@pnnl.gov>
2020-08-04 17:59:15 -0400

    Tested gridappsd example changes

--- examples/GridAPPS-D/gridappsd_example/agent.py
+++ examples/GridAPPS-D/gridappsd_example/agent.py
@@ -43,14 +43,15 @@ from gevent.monkey import patch_all
 patch_all()
 
 import gevent
+import stomp
 import logging
-import random
 import sys
+from gridappsd import topics as t
+import yaml
+from volttron.platform.messaging import headers as headers_mod
 from volttron.platform.agent import utils
 from volttron.platform.vip.agent import Agent, Core, RPC
 from integrations.gridappsd_integration import GridAPPSDSimIntegration
-from gridappsd import topics as t
-import yaml
 
 _log = logging.getLogger(__name__)
 utils.setup_logging()
@@ -80,7 +81,8 @@ def gridappsd_example(config_path, **kwargs):
 
 class GridappsdExample(Agent):
     """
-    GridappsdExampleAgent demonstrates how VOLTTRON agent can interact with Gridappsd simulation environment
+    GridappsdExampleAgent demonstrates how VOLTTRON agent can interact with
+    Gridappsd simulation environment
     """
 
     def __init__(self, config, **kwargs):
@@ -94,6 +96,7 @@ class GridappsdExample(Agent):
         self.rcvd_first_measurement = 0
         self.are_we_paused = False
         self.sim_complete = True
+        self.sim_running = False
 
     @Core.receiver("onstart")
     def onstart(self, sender, **kwargs):
@@ -110,41 +113,57 @@ class GridappsdExample(Agent):
                                           prefix=sub,
                                           callback=self.on_receive_publisher_message)
 
-        # Exit if Gridappsd isn't installed in the current environment.
+        # Exit if GridAPPSD isn't installed in the current environment.
         if not self.Gridappsd_sim.is_sim_installed():
-            _log.error("Gridappsd module is unavailable please add it to the python environment.")
+            _log.error("GridAPPSD module is unavailable please add it to the python environment.")
             self.core.stop()
             return
 
-        # Register inputs with Gridappsd and start simulation
         try:
+            # Register events with GridAPPSD
+            # There are 4 event callbacks that GridAPPSD provides to monitor the status
+            # - onstart, ontimestep, onmesurement, oncomplete
+            # This example shows how to register with GridAPPSD simulation to get
+            # event notifications
             event_callbacks = {'MEASUREMENT': self.onmeasurement,
                                "TIMESTEP": self.ontimestep,
                                "FINISH": self.onfinishsimulation}
             self.Gridappsd_sim.register_event_callbacks(event_callbacks)
 
-#            self.Gridappsd_sim.register_topic_callbacks(topic_callbacks)
             # Register the config file with GridAPPS-D
             self.Gridappsd_sim.register_inputs(self.config, self.do_work)
+            # Start the simulation
             self.Gridappsd_sim.start_simulation()
+
+            # Waiting for simulation to start
             i = 1
             while not self.Gridappsd_sim.sim_id and i < 20:
                 _log.debug(f"waiting for simulation to start {i}")
                 gevent.sleep(1)
                 i += 1
+
+            # Subscribe to GridAPPSD log messages
             if self.Gridappsd_sim.sim_id:
-                self.Gridappsd_sim.gridappsd.subscribe(t.simulation_log_topic(self.Gridappsd_sim.sim_id),
-                                             self.on_message)
+                self.Gridappsd_sim.gridappsd.subscribe(
+                    t.simulation_log_topic(self.Gridappsd_sim.sim_id),
+                    self.on_message)
+                self.sim_running = True
             else:
+                self.sim_running = False
                 _log.debug("Simulation did not start")
+        except stomp.exception.NotConnectedException as ex:
+            _log.error("Unable to connect to GridAPPSD: {}".format(ex))
+            _log.error("Exiting!!")
+            self.core.stop()
         except ValueError as ex:
-            _log.error("Unable to register inputs with Gridappsd: {}".format(ex))
+            _log.error("Unable to register inputs with GridAPPSD: {}".format(ex))
             self.core.stop()
             return
 
     def do_work(self):
         """
         Dummy callback for GridAPPS-D sim
+        Unused
         :return:
         """
         pass
@@ -157,25 +176,23 @@ class GridappsdExample(Agent):
         val = message[0]
         # Do something with message
 
-    def on_receive_volttron_message(self, peer, sender, bus, topic, headers, message):
-        """
-        Subscribe to publisher publications and change the data accordingly
-        """
-        _log.debug("Received volttron topic: {}, value: {}".format(topic, message))
-        # Update controller data
-        val = message
-        self.volttron_messages[topic]['value'] = val
-        self.volttron_messages[topic]['received'] = True
-
     @Core.receiver("onstop")
     def onstop(self, sender, **kwargs):
         """
-        This method is called when the Agent is about to shutdown, but before it
-        disconnects from the message bus.
+        This method is called when the Agent is about to shutdown.
+        Disconnect from GridAPPSD simulation
         """
-        pass
+        if self.sim_running:
+            self.Gridappsd_sim.stop_simulation()
 
     def onmeasurement(self, sim, timestep, measurements):
+        """
+        Callback method to receive measurements
+        :param sim: simulation object
+        :param timestep: time step
+        :param measurements: measurement value
+        :return:
+        """
         _log.info('Measurement received at %s', timestep)
 
         if not self.are_we_paused and not self.rcvd_first_measurement:
@@ -183,49 +200,79 @@ class GridappsdExample(Agent):
             self.Gridappsd_sim.pause_simulation()
             self.are_we_paused = True
             _log.debug(f"ARWEPAUSED {self.are_we_paused}")
-            # Setting up so if we get another measurement wheil we
+            # Setting up so if we get another measurement while we
             # are paused we know it
             self.rcvd_measurement = False
             # Resume simulation after 30 sec
             self.core.spawn_later(30, self.resume_simulation)
 
         if not self.rcvd_measurement:
-            print(f"A measurement happened at {timestep}")
-            # outfile.write(f"{timestep}|{json.dumps(measurements)}\n")
+            print(f"A measurement {measurements} happened at {timestep}")
             data = {"data": measurements}
 
+            headers = {
+                headers_mod.DATE: utils.format_timestamp(utils.get_aware_utc_now()),
+                headers_mod.CONTENT_TYPE: headers_mod.CONTENT_TYPE.JSON
+            }
+            # Publishing measurement on VOLTTRON message bus
+            self.vip.pubsub.publish(peer='pubsub',
+                                    topic='gridappsd/measurement',
+                                    message=data,
+                                    headers=headers)
             self.rcvd_measurement = True
         else:
             self.rcvd_measurement = True
         self.rcvd_first_measurement = True
 
     def ontimestep(self, sim, timestep):
+        """
+        Event callback for timestep change
+        :param sim:
+        :param timestep:
+        :return:
+        """
         _log.debug("Timestamp: {}".format(timestep))
 
     def onfinishsimulation(self, sim):
+        """
+        Event callback to get notified when simulation has completed
+        :param sim:
+        :return:
+        """
         self.sim_complete = True
         _log.info('Simulation Complete')
 
     def resume_simulation(self):
+        """
+        Resume simulation if paused
+        :return:
+        """
         if self.are_we_paused:
             _log.debug('Resuming simulation')
             self.Gridappsd_sim.resume_simulation()
             _log.debug('Resumed simulation')
             self.are_we_paused = False
 
-    def on_message(self, message):
+    def on_message(self, headers, message):
+        """
+        Callback method to receive GridAPPSD log messages
+        :param headers: headers
+        :param message: log message
+        :return:
+        """
         try:
-            _log.debug("Received GridAppsD log message: {}".format(message))
+            _log.debug("Received GridAPPSD log message: {}".format(message))
             json_msg = yaml.safe_load(str(message))
 
             if "PAUSED" in json_msg["processStatus"]:
-                _log.debug("GridAppsD simulation has paused!!")
+                _log.debug("GridAPPSD simulation has paused!!")
 
             if "resume" in json_msg["logMessage"]:
-                _log.debug("GridAppsD simulation has resumed!!")
+                _log.debug("GridAPPSD simulation has resumed!!")
 
         except Exception as e:
             message_str = "An error occurred while trying to translate the  message received" + str(e)
+
 
 def main():
     """Main method called to start the agent."""
