a92995f265629289b7dfd24c51f71b081e699b2d
schandrika <chandrika@pnnl.gov>
2020-11-03 23:01:35 -0800

    updated based on review comments. Fixed create if not exists statements in aggrgation table creation

--- services/core/SQLHistorian/mysql-create.sql
+++ services/core/SQLHistorian/mysql-create.sql
@@ -1,5 +1,8 @@
 -- This script assumes that the user has access to create the database.
 -- update database name, user name, and password before executing the below commands
+-- table names used below are default names used by historian. If you would like to customize table names
+-- customize using the configuration tables_def and change the names in the below commands
+
 CREATE DATABASE test_historian;
 
 USE test_historian;
@@ -12,9 +15,9 @@ CREATE TABLE data (ts timestamp(6) NOT NULL,
 CREATE INDEX data_idx ON data (ts ASC);
 
 CREATE TABLE topics (topic_id INTEGER NOT NULL AUTO_INCREMENT,
-                                 topic_name varchar(512) NOT NULL,
-                                 PRIMARY KEY (topic_id),
-                                 UNIQUE(topic_name));
+                     topic_name varchar(512) NOT NULL,
+                     PRIMARY KEY (topic_id),
+                     UNIQUE(topic_name));
 
 CREATE TABLE meta(topic_id INTEGER NOT NULL,
                   metadata TEXT NOT NULL,
@@ -37,10 +40,34 @@ GRANT SELECT, INSERT, DELETE ON test_historian.* TO 'historian'@'localhost';
 # GRANT UPDATE ON <dbname>.<topics_table> TO 'username'@'localhost';
 GRANT UPDATE ON test_historian.topics TO 'historian'@'localhost';
 
-# If you are using aggregate historians with mysql also grant udpate access to aggregate_topics
-# GRANT UPDATE ON <dbname>.aggregate_topics TO 'username'@'localhost';
-GRANT UPDATE ON test_historian.aggregate_topics TO 'historian'@'localhost';
-
-
 # TO Run test_historian.py you need additional create and index privileges
 GRANT CREATE, INDEX ON test_historian.* TO 'historian'@'localhost';
+
+# If you are using aggregate historians with mysql create and grant access to additional tables
+
+CREATE TABLE aggregate_topics
+      (agg_topic_id INTEGER NOT NULL AUTO_INCREMENT,
+       agg_topic_name varchar(512) NOT NULL,
+       agg_type varchar(512) NOT NULL,
+       agg_time_period varchar(512) NOT NULL,
+       PRIMARY KEY (agg_topic_id),
+       UNIQUE(agg_topic_name, agg_type, agg_time_period));
+
+CREATE TABLE aggregate_meta
+    (agg_topic_id INTEGER NOT NULL,
+     metadata TEXT NOT NULL,
+     PRIMARY KEY(agg_topic_id));
+
+# FOR EACH CONFIGURED AGGREGATION execute the following where aggregate_data_table is aggregation_type+"_"+aggregation_period
+# for example avg_10m for 10 minute average
+
+CREATE TABLE <aggregate_data_table>
+      (ts timestamp(6) NOT NULL, topic_id INTEGER NOT NULL,
+       value_string TEXT NOT NULL, topics_list TEXT,
+       UNIQUE(topic_id, ts),
+       INDEX (ts ASC))
+
+# GRANT UPDATE ON <dbname>.aggregate_topics TO 'username'@'localhost';
+GRANT UPDATE ON test_historian.aggregate_topics TO 'historian'@'localhost';
+GRANT UPDATE ON test_historian.aggregate_meta TO 'historian'@'localhost';
+
--- volttron/platform/dbutils/mysqlfuncts.py
+++ volttron/platform/dbutils/mysqlfuncts.py
@@ -107,14 +107,14 @@ class MySqlFuncts(DbDriver):
         try:
             if self.MICROSECOND_SUPPORT:
                 self.execute_stmt(
-                    'CREATE TABLE IF NOT EXISTS ' + self.data_table +
+                    'CREATE TABLE ' + self.data_table +
                     ' (ts timestamp(6) NOT NULL,\
                      topic_id INTEGER NOT NULL, \
                      value_string TEXT NOT NULL, \
                      UNIQUE(topic_id, ts))')
             else:
                 self.execute_stmt(
-                    'CREATE TABLE IF NOT EXISTS ' + self.data_table +
+                    'CREATE TABLE ' + self.data_table +
                     ' (ts timestamp NOT NULL,\
                      topic_id INTEGER NOT NULL, \
                      value_string TEXT NOT NULL, \
@@ -122,13 +122,13 @@ class MySqlFuncts(DbDriver):
 
             self.execute_stmt('''CREATE INDEX data_idx
                                     ON ''' + self.data_table + ''' (ts ASC)''')
-            self.execute_stmt('''CREATE TABLE IF NOT EXISTS ''' +
+            self.execute_stmt('''CREATE TABLE  ''' +
                               self.topics_table +
                               ''' (topic_id INTEGER NOT NULL AUTO_INCREMENT,
                                    topic_name varchar(512) NOT NULL,
                                    PRIMARY KEY (topic_id),
                                    UNIQUE(topic_name))''')
-            self.execute_stmt('''CREATE TABLE IF NOT EXISTS '''
+            self.execute_stmt('''CREATE TABLE  '''
                               + self.meta_table +
                               '''(topic_id INTEGER NOT NULL,
                                metadata TEXT NOT NULL,
@@ -189,21 +189,26 @@ class MySqlFuncts(DbDriver):
         self.agg_topics_table = table_names.get('agg_topics_table', None)
         self.agg_meta_table = table_names.get('agg_meta_table', None)
 
-        self.execute_stmt(
-            'CREATE TABLE IF NOT EXISTS ' + self.agg_topics_table +
-            ' (agg_topic_id INTEGER NOT NULL AUTO_INCREMENT, \
-               agg_topic_name varchar(512) NOT NULL, \
-               agg_type varchar(512) NOT NULL, \
-               agg_time_period varchar(512) NOT NULL, \
-               PRIMARY KEY (agg_topic_id), \
-               UNIQUE(agg_topic_name, agg_type, agg_time_period));')
+        rows = self.select("show tables like %s", [self.agg_topics_table])
+        if rows:
+            _log.debug("Found table {}. Historian table exists".format(
+                self.agg_topics_table))
+        else:
+            self.execute_stmt(
+                'CREATE TABLE ' + self.agg_topics_table +
+                ' (agg_topic_id INTEGER NOT NULL AUTO_INCREMENT, \
+                   agg_topic_name varchar(512) NOT NULL, \
+                   agg_type varchar(512) NOT NULL, \
+                   agg_time_period varchar(512) NOT NULL, \
+                   PRIMARY KEY (agg_topic_id), \
+                   UNIQUE(agg_topic_name, agg_type, agg_time_period));')
 
-        self.execute_stmt(
-            'CREATE TABLE IF NOT EXISTS ' + self.agg_meta_table +
-            '(agg_topic_id INTEGER NOT NULL, \
-              metadata TEXT NOT NULL, \
-              PRIMARY KEY(agg_topic_id));')
-        self.commit()
+            self.execute_stmt(
+                'CREATE TABLE ' + self.agg_meta_table +
+                '(agg_topic_id INTEGER NOT NULL, \
+                  metadata TEXT NOT NULL, \
+                  PRIMARY KEY(agg_topic_id));')
+            self.commit()
         _log.debug("Created aggregate topics and meta tables")
 
     def query(self, topic_ids, id_name_map, start=None, end=None, skip=0,
@@ -401,18 +406,22 @@ class MySqlFuncts(DbDriver):
         if self.MICROSECOND_SUPPORT is None:
             self.init_microsecond_support()
 
-        stmt = "CREATE TABLE IF NOT EXISTS " + table_name + \
-               " (ts timestamp(6) NOT NULL, topic_id INTEGER NOT NULL, " \
-               "value_string TEXT NOT NULL, topics_list TEXT," \
-               " UNIQUE(topic_id, ts)," \
-               "INDEX (ts ASC))"
-        if not self.MICROSECOND_SUPPORT:
-            stmt = "CREATE TABLE IF NOT EXISTS " + table_name + \
-                   " (ts timestamp NOT NULL, topic_id INTEGER NOT NULL, " \
+        rows = self.select("show tables like %s", [table_name])
+        if rows:
+            _log.debug("Found table {}. Historian table exists".format(table_name))
+        else:
+            stmt = "CREATE TABLE " + table_name + \
+                   " (ts timestamp(6) NOT NULL, topic_id INTEGER NOT NULL, " \
                    "value_string TEXT NOT NULL, topics_list TEXT," \
                    " UNIQUE(topic_id, ts)," \
                    "INDEX (ts ASC))"
-        return self.execute_stmt(stmt, commit=True)
+            if not self.MICROSECOND_SUPPORT:
+                stmt = "CREATE TABLE " + table_name + \
+                       " (ts timestamp NOT NULL, topic_id INTEGER NOT NULL, " \
+                       "value_string TEXT NOT NULL, topics_list TEXT," \
+                       " UNIQUE(topic_id, ts)," \
+                       "INDEX (ts ASC))"
+            return self.execute_stmt(stmt, commit=True)
 
     def insert_aggregate_stmt(self, table_name):
         return '''REPLACE INTO ''' + table_name + \
