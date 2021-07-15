13964192a443fd6a9eacb042d1c9115673bee126
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2021-07-15 12:55:01 -0700

    Fix IMAGES

--- volttrontesting/platform/dbutils/test_postgresqlfuncts.py
+++ volttrontesting/platform/dbutils/test_postgresqlfuncts.py
@@ -25,7 +25,7 @@ except ImportError:
 
 # Current documentation claims that we have tested Historian on Postgres 10
 # See https://volttron.readthedocs.io/en/develop/core_services/historians/SQL-Historian.html#postgresql-and-redshift
-IMAGES = ["postgres:9.6.18"] #, "postgres:10.13"]
+IMAGES = ["postgres:9.6.18", "postgres:10.13"]
 
 if "CI" in os.environ:
     IMAGES.extend(
