ac0fce711869e0b8fc3f142cd5b4dd691f68f7f6
schandrika <chandrika@pnnl.gov>
2021-07-16 12:50:39 -0700

    added time_error in header info

--- volttrontesting/services/historian/test_base_historian_unit.py
+++ volttrontesting/services/historian/test_base_historian_unit.py
@@ -62,6 +62,7 @@ def test_base_historian_agent_should_filter_duplicates(base_historian_agent):
             "headers": {
                 "Date": "2015-11-17 21:24:10.189393+00:00",
                 "TimeStamp": "2015-11-17 21:24:10.189393+00:00",
+                "time_error": False
             },
             "meta": {},
         }
