509ca07a7de91629f7173105d492487a67388016
David M. Raker <david.raker@utoledo.edu>
2022-01-30 03:37:45 -0500

    Changed response for all failed authentication (e.g. invalid token) to 401 Not Authorized.

--- volttron/platform/web/vui_endpoints.py
+++ volttron/platform/web/vui_endpoints.py
@@ -38,7 +38,7 @@ def endpoint(func):
         from volttron.platform.web import get_bearer, NotAuthorized
         try:
             claims = self._agent.get_user_claims(get_bearer(env))
-        except (NotAuthorized, ExpiredSignatureError) as e:
+        except Exception as e:
             _log.warning(f"Unauthorized user attempted to connect to {env.get('PATH_INFO')}. Caught Exception: {e}")
             return Response(json.dumps({'error': 'Not Authorized'}), 401, content_type='app/json')
 
