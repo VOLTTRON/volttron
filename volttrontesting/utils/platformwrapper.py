320a0871ba04ff1803f25761e6b28480ba87ab5e
Chandrika Sivaramakrishnan <chandrika@pnnl.gov>
2023-08-18 14:02:04 -0700

    Fix for issue #3102. creating keys only when auth is enabled

--- volttrontesting/utils/platformwrapper.py
+++ volttrontesting/utils/platformwrapper.py
@@ -330,9 +330,12 @@ class PlatformWrapper:
 
             # Writes the main volttron config file for this instance.
             store_message_bus_config(self.messagebus, self.instance_name)
-
-            # Update volttron config file with non-auth setting if auth_enabled is False
-            if not self.auth_enabled:
+            if self.auth_enabled:
+                keystorefile = os.path.join(self.volttron_home, 'keystore')
+                self.keystore = KeyStore(keystorefile)
+                self.keystore.generate()
+            else:
+                # Update volttron config file with non-auth setting if auth_enabled is False
                 config_path = os.path.join(self.volttron_home, "config")
                 if os.path.exists(config_path):
                     config = configparser.ConfigParser()
@@ -358,7 +361,6 @@ class PlatformWrapper:
             if not self.debug_mode:
                 self.debug_mode = self.env.get('DEBUG', False)
             self.skip_cleanup = self.env.get('SKIP_CLEANUP', False)
-
             self._web_admin_api = None
 
     @property
@@ -419,10 +421,11 @@ class PlatformWrapper:
             self.logit(
                 'Default address was None so setting to current instances')
             address = self.vip_address
+
         if self.auth_enabled:
-            if address is None:
-                serverkey = self.serverkey
             if serverkey is None:
+                serverkey = self.serverkey
+            if serverkey is None and address is not None:
                 self.logit("serverkey wasn't set but the address was.")
                 raise Exception("Invalid state.")
             if publickey is None or secretkey is None:
@@ -479,31 +482,32 @@ class PlatformWrapper:
         print(f"Publickey is: {publickey}\nServerkey is: {serverkey}")
         if serverkey is None:
             serverkey = self.serverkey
-        if publickey is None:
-            self.logit(f'generating new public secret key pair {KeyStore.get_agent_keystore_path(identity=identity)}')
-            ks = KeyStore(KeyStore.get_agent_keystore_path(identity=identity))
-            publickey = ks.public
-            secretkey = ks.secret
 
         if address is None:
             self.logit('Using vip-address {address}'.format(
                 address=self.vip_address))
             address = self.vip_address
-
-        if publickey and not serverkey:
-            self.logit('using instance serverkey: {}'.format(publickey))
-            serverkey = publickey
-        self.logit("BUILD agent VOLTTRON HOME: {}".format(self.volttron_home))
-        if self.bind_web_address:
-            kwargs['enable_web'] = True
-
-        if 'enable_store' not in kwargs:
-            kwargs['enable_store'] = False
-
-        if capabilities is None:
-            capabilities = dict(edit_config_store=dict(identity=identity))
-        print(f"Publickey is: {publickey}\nServerkey is: {serverkey}")
         if self.auth_enabled:
+            if publickey is None:
+                self.logit(
+                    f'generating new public secret key pair {KeyStore.get_agent_keystore_path(identity=identity)}')
+                ks = KeyStore(KeyStore.get_agent_keystore_path(identity=identity))
+                publickey = ks.public
+                secretkey = ks.secret
+            if publickey and not serverkey:
+                self.logit('using instance serverkey: {}'.format(publickey))
+                serverkey = publickey
+            self.logit("BUILD agent VOLTTRON HOME: {}".format(self.volttron_home))
+            if self.bind_web_address:
+                kwargs['enable_web'] = True
+
+            if 'enable_store' not in kwargs:
+                kwargs['enable_store'] = False
+
+            if capabilities is None:
+                capabilities = dict(edit_config_store=dict(identity=identity))
+            print(f"Publickey is: {publickey}\nServerkey is: {serverkey}")
+
             entry = AuthEntry(user_id=identity, identity=identity, credentials=publickey,
                               capabilities=capabilities,
                               comments="Added by platform wrapper")
@@ -845,12 +849,13 @@ class PlatformWrapper:
             pconfig = os.path.join(self.volttron_home, 'config')
             config = {}
 
-            # Add platform's public key to known hosts file
-            publickey = self.keystore.public
-            known_hosts_file = os.path.join(self.volttron_home, 'known_hosts')
-            known_hosts = KnownHostsStore(known_hosts_file)
-            known_hosts.add(self.opts['vip_local_address'], publickey)
-            known_hosts.add(self.opts['vip_address'], publickey)
+            if self.auth_enabled:
+                # Add platform's public key to known hosts file
+                publickey = self.keystore.public
+                known_hosts_file = os.path.join(self.volttron_home, 'known_hosts')
+                known_hosts = KnownHostsStore(known_hosts_file)
+                known_hosts.add(self.opts['vip_local_address'], publickey)
+                known_hosts.add(self.opts['vip_address'], publickey)
 
             # Set up the configuration file based upon the passed parameters.
             parser = configparser.ConfigParser()
@@ -918,8 +923,9 @@ class PlatformWrapper:
 
             utils.wait_for_volttron_startup(self.volttron_home, timeout)
 
-            self.serverkey = self.keystore.public
-            assert self.serverkey
+            if self.auth_enabled:
+                self.serverkey = self.keystore.public
+                assert self.serverkey
 
             # Use dynamic_agent so we can look and see the agent with peerlist.
             if not setupmode:
