5445d47b107713149555cbacd22eeda90a45e46e
riley206 <rileymaltos@gmail.com>
2023-09-05 10:59:47 -0700

    Removed subscription function

--- services/core/PlatformDriverAgent/platform_driver/interfaces/home_assistant.py
+++ services/core/PlatformDriverAgent/platform_driver/interfaces/home_assistant.py
@@ -78,8 +78,6 @@ class Interface(BasicRevert, BaseInterface):
     def __init__(self, **kwargs):
         super(Interface, self).__init__(**kwargs)
         self.point_name = None
-        self.previous_states = {} # storing previous states to only send commands when changed 
-        self.first_pass = True # first pass
   
     def configure(self, config_dict, registry_config_str): # grabbing from config
         self.ip_address = config_dict.get("ip_address", "0.0.0.0")
@@ -94,22 +92,17 @@ class Interface(BasicRevert, BaseInterface):
         register = self.get_register_by_name(point_name)
         return register.value
 
-
     def _set_point(self, point_name, value):
-        print(f"Attempting to set point: {point_name} with value: {value}")
         register = self.get_register_by_name(point_name)
         if register.read_only:
-            print(f"Cannot set point: {point_name} because it is read-only.")
             raise RuntimeError(
                 "Trying to write to a point configured read only: " + point_name)
-        print(f"Current value of point: {point_name} is {register.value}")
         register.value = register.reg_type(value) # setting the value
 
         if register.value == True:
             self.turn_on_lights(point_name)
         elif register.value == False:
             self.turn_off_lights(point_name)
-        print(f"New value of point: {point_name} is {register.value}")
 
         return register.value
     
@@ -151,7 +144,6 @@ class Interface(BasicRevert, BaseInterface):
                     if attribute_value is not None:
                         result[attribute_name] = attribute_value
             else:
-                # This will simply add register's value with a prefix of entity_id
                 result[register.point_name] = register.value
                 _log.info(f"Entity: {register.point_name} data is None")
 
@@ -279,74 +271,5 @@ class Interface(BasicRevert, BaseInterface):
                     _log.info(f"Successfully changed the temp of {entity} to {temperature}")
                 else:
                     _log.info(f"Failed to change the temp of {entity}. Response: {response.text}")
-
-    # def _create_subscriptions(self, topic):
-    #     """
-    #     Unsubscribe from all pub/sub topics and create a subscription to a topic in the configuration which triggers
-    #     the _handle_publish callback
-    #     """
-    #     self.vip.pubsub.unsubscribe("pubsub", None, None)
-
-    #     self.vip.pubsub.subscribe(peer='pubsub',
-    #                               prefix=topic,
-    #                               callback=self._handle_publish)    
-        
-    # def _handle_publish(self, peer, sender, bus, topic, headers, messages):
-    #     for message in messages: #subscribes to itself then stores states and if they change it runs the commands 
-    #         for entity_id, entity_data in message.items():
-                
-    #             state = entity_data.get("state", None)
-    #             brightness = entity_data.get("brightness", None)
-    #             temperature = entity_data.get("temperature", None)
-
-    #             previous_state = self.previous_states.get(entity_id, None)
-    #             previous_brightness = self.previous_states.get(f"{entity_id}_brightness", None)
-    #             previous_temperature = self.previous_states.get(f"{entity_id}_temperature", None)
-
-    #             #LIGHTS
-    #             if entity_id.startswith("light.") and not self.first_pass: #if it starts with light and its not the first pass to store previous values. 
-    #                 if state != previous_state:  # if state changed
-    #                     if state == "on":
-    #                         _log.info(f"{entity_id} value has been detected as on")
-    #                         self.turn_on_lights(entity_id, 255 if brightness is None else brightness)
-    #                     elif state == "off":
-    #                         _log.info(f"{entity_id} detected as off!")
-    #                         self.turn_off_lights(entity_id)
-    #                     else:
-    #                         continue
-
-    #                 # this handles brightness change even when state doesn't change
-    #                 if brightness != previous_brightness:
-    #                     _log.info(f"{entity_id} brightness has been detected and changed to {brightness} / 254")
-    #                     self.turn_on_lights(entity_id, brightness)
-
-    #                 self.previous_states[entity_id] = state
-    #                 self.previous_states[f"{entity_id}_brightness"] = brightness # example previous_states[light.entity_brightness] = brightness
-
-    #             # THERMOSTATS
-    #             elif entity_id.startswith("climate.") and not self.first_pass:
-
-    #                 if state != previous_state:
-    #                     if state == "cool":
-    #                         self.change_thermostat_mode("cool")
-    #                         _log.info(f"{entity_id} value has been changed to cool")
-    #                     elif state == "heat":
-    #                         _log.info(f"{entity_id} value has been changed to heat")
-    #                         self.change_thermostat_mode("heat")
-    #                     elif state == "off":
-    #                         _log.info(f"{entity_id} value has been changed to off")
-    #                         self.change_thermostat_mode("off")
-    #                     else: 
-    #                         continue
-    #                 if temperature != previous_temperature:
-    #                     _log.info(f"{entity_id} temperature has been detected and changed to {temperature} degrees F")
-    #                     self.set_thermostat_temperature(temperature)
-
-    #                 self.previous_states[entity_id] = state
-    #                 self.previous_states[f"{entity_id}_temperature"] = temperature # example previous_states[light.entity_brightness] = brightness
-    #             else:
-    #                 continue
-    #         if self.first_pass:
-    #             self.first_pass = False
 
         
