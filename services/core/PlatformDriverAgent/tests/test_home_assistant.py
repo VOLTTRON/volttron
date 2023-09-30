a411c9b136dbe5051b5ef83be4dd9e95089f15e5
Chandrika Sivaramakrishnan <chandrika@pnnl.gov>
2023-09-30 13:04:22 -0700

    correct initial test_home_assistant.py

---
+++ services/core/PlatformDriverAgent/tests/test_home_assistant.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+#
+# Copyright 2020, Battelle Memorial Institute.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This material was prepared as an account of work sponsored by an agency of
+# the United States Government. Neither the United States Government nor the
+# United States Department of Energy, nor Battelle, nor any of their
+# employees, nor any jurisdiction or organization that has cooperated in the
+# development of these materials, makes any warranty, express or
+# implied, or assumes any legal liability or responsibility for the accuracy,
+# completeness, or usefulness or any information, apparatus, product,
+# software, or process disclosed, or represents that its use would not infringe
+# privately owned rights. Reference herein to any specific commercial product,
+# process, or service by trade name, trademark, manufacturer, or otherwise
+# does not necessarily constitute or imply its endorsement, recommendation, or
+# favoring by the United States Government or any agency thereof, or
+# Battelle Memorial Institute. The views and opinions of authors expressed
+# herein do not necessarily state or reflect those of the
+# United States Government or any agency thereof.
+#
+# PACIFIC NORTHWEST NATIONAL LABORATORY operated by
+# BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# under Contract DE-AC05-76RL01830
+# }}}
+import json
+import logging
+import os
+import pytest
+import gevent
+import socket
+
+from mock import MagicMock
+from volttron.platform.agent.known_identities import (
+    PLATFORM_DRIVER,
+    CONFIGURATION_STORE,
+)
+from volttron.platform import get_services_core
+from volttron.platform.agent import utils
+from volttron.platform.keystore import KeyStore
+from volttrontesting.utils.platformwrapper import PlatformWrapper
+
+utils.setup_logging()
+logger = logging.getLogger(__name__)
+
+HOMEASSISTANT_DEVICE_TOPIC = "devices/home_assistant"
+HOMEASSISTANT_TEST_IP = "you ip"
+ACCESS_TOKEN = "your access token"
+PORT = "8123"
+
+
+# skip_msg = f"Env var {HOMEASSISTANT_TEST_IP} not set. Please set the env var to the proper IP to run this integration test."
+# # apply skipif to all tests
+# pytestmark = pytest.mark.skipif(os.environ.get(HOMEASSISTANT_TEST_IP) is None, reason=skip_msg)
+
+def test_data_poll(volttron_instance: PlatformWrapper, config_store):
+    expected_values = "off"
+    agent = volttron_instance.dynamic_agent
+    result = agent.vip.rpc.call(PLATFORM_DRIVER, 'get_point', 'home_assistant', 'state').get(timeout=20)
+    assert result == expected_values, "The result does not match the expected result."
+
+
+@pytest.fixture(scope="module")
+def config_store(volttron_instance, platform_driver):
+
+    capabilities = [{"edit_config_store": {"identity": PLATFORM_DRIVER}}]
+    volttron_instance.add_capabilities(volttron_instance.dynamic_agent.core.publickey, capabilities)
+
+    registry_config = "homeassistant_test.json"
+    registry_obj = [{
+        "Entity ID": "input_boolean.test",
+        "Volttron Point Name": "state",
+        "Units": "On / Off",
+        "Units Details": "on/off",
+        "Writable": True,
+        "Starting Value": True,
+        "Type": "boolean",
+        "Notes": "lights hallway"
+    }]
+
+    volttron_instance.dynamic_agent.vip.rpc.call(CONFIGURATION_STORE,
+                                                 "manage_store",
+                                                 PLATFORM_DRIVER,
+                                                 registry_config,
+                                                 json.dumps(registry_obj),
+                                                 config_type="json")
+    gevent.sleep(2)
+    # driver config
+    driver_config = {
+        "driver_config": {"ip_address": HOMEASSISTANT_TEST_IP, "access_token": ACCESS_TOKEN, "port": PORT},
+        "driver_type": "home_assistant",
+        "registry_config": f"config://{registry_config}",
+        "timezone": "US/Pacific",
+        "interval": 30,
+    }
+
+    volttron_instance.dynamic_agent.vip.rpc.call(CONFIGURATION_STORE,
+                                                 "manage_store",
+                                                 PLATFORM_DRIVER,
+                                                 HOMEASSISTANT_DEVICE_TOPIC,
+                                                 json.dumps(driver_config),
+                                                 config_type="json"
+                                                 )
+    gevent.sleep(2)
+
+    yield platform_driver
+
+    print("Wiping out store.")
+    volttron_instance.dynamic_agent.vip.rpc.call(CONFIGURATION_STORE, "manage_delete_store", PLATFORM_DRIVER)
+    gevent.sleep(0.1)
+
+
+@pytest.fixture(scope="module")
+def platform_driver(volttron_instance):
+    # Start the platform driver agent which would in turn start the bacnet driver
+    platform_uuid = volttron_instance.install_agent(
+        agent_dir=get_services_core("PlatformDriverAgent"),
+        config_file={
+            "publish_breadth_first_all": False,
+            "publish_depth_first": False,
+            "publish_breadth_first": False,
+        },
+        start=True,
+    )
+    gevent.sleep(2)  # wait for the agent to start and start the devices
+    assert volttron_instance.is_agent_running(platform_uuid)
+    yield platform_uuid
+
+    volttron_instance.stop_agent(platform_uuid)
+    #volttron_instance.remove_agent(platform_uuid)
