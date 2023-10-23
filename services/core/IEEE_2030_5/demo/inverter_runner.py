49340d23825e458b95fa2ca22b53107d519504bb
C. Allwardt <3979063+craig8@users.noreply.github.com>
2023-10-23 14:55:11 -0700

    Remove duplicate run_inverter

--- services/core/IEEE_2030_5/demo/inverter_runner.py
+++ services/core/IEEE_2030_5/demo/inverter_runner.py
@@ -130,66 +130,6 @@ class AllPoints:
         return points
 
 
-def run_inverter(timesteps=50) -> Generator:
-    # PV module
-    sandia_modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
-    module = sandia_modules['Canadian_Solar_CS5P_220M___2009_']
-    # Inverter model
-    sapm_inverters = pvlib.pvsystem.retrieve_sam('cecinverter')
-    inverter = sapm_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
-    irradiance = [900, 1000, 925]
-    temperature = [25, 28, 20]
-    # Assumed constant power factor
-    PF = 0.99
-    # Assumed constant AC voltage
-    v_ac = 120
-    latitude = 32
-    longitude = -111.0
-
-    weather_path = Path("~/weather.txt").expanduser()
-    if weather_path.exists():
-        header = [
-            "time(UTC)", "temp_air", "relative_humidity", "ghi", "dni", "dhi", "IR(h)",
-            "wind_speed", "wind_direction", "pressure"
-        ]
-        weather = pd.read_csv(weather_path)
-    else:
-        weather = pvlib.iotools.get_pvgis_tmy(latitude, longitude, map_variables=True)[0]
-        result = weather.to_csv(weather_path, header=True)
-        print(f"The result is: {result}")
-
-    total_solar_radiance = weather['ghi']
-    # assumed that the total solar radiance is equal to ghi(global horizontal irradiance)
-    outdoor_temp = weather["temp_air"]
-    # both the total_solar_radiace and outdoor_temp has 1hr sampling rate.
-    # you should be able modify the sampling rate by resampling it
-    for x, y in zip(total_solar_radiance, outdoor_temp):
-        dc = pvlib.pvsystem.sapm(x, y, module)
-        p_ac = pvlib.inverter.sandia(dc['v_mp'], dc['p_mp'], inverter)
-        s_ac = p_ac / PF
-        q_ac = math.sqrt(p_ac**2 + s_ac**2)
-        i_ac = (s_ac / v_ac) * 1000
-        print(
-            f"p_ac = {p_ac}, s_ac = {s_ac}, q_ac= {q_ac}, PF = {PF}, v_ac = {v_ac}, i_ac = {i_ac}")
-        yield dict(INV_REAL_PWR=p_ac,
-                   INV_REAC_PWR=q_ac,
-                   BAT_SOC=int(v_ac / p_ac),
-                   INV_OP_STATUS_MODE=3)
-        # v_mp=dc['v_mp'],
-        #        p_mp=dc['p_mp'],
-        #        i_x=dc['i_x'],
-        #        i_xx=dc['i_xx'],
-        #        v_oc=dc['v_oc'],
-        #        i_sc=dc['i_sc'],
-        #        p_ac=p_ac,
-        #        s_ac=p_ac,
-        #        q_ac=q_ac,
-        #        v_ac=v_ac,
-        #        i_ac=i_ac,
-        #        PF=PF)
-        # single phase circuit calculation
-
-
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
