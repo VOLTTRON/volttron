857d2b90e5aed06ca307e1efca7954f3b1c0e144
Andrew Rodgers <acedrew@gmail.com>
2024-04-23 11:16:33 -0400

    added fix for file event overload (#23)
    Co-authored-by: Andrew Rodgers <andrew@aceiotsolutions.com>

--- volttron/utils/__init__.py
+++ volttron/utils/__init__.py
@@ -122,7 +122,7 @@ class VolttronHomeFileReloader(PatternMatchingEventHandler):
         _log.debug("patterns is {}".format([get_home() + '/' + filetowatch]))
         self._callback = callback
 
-    def on_any_event(self, event):
+    def on_closed(self, event):
         _log.debug("Calling callback on event {}. Calling {}".format(event, self._callback))
         try:
             self._callback()
@@ -147,7 +147,7 @@ class AbsolutePathFileReloader(PatternMatchingEventHandler):
     def watchfile(self):
         return self._filetowatch
 
-    def on_any_event(self, event):
+    def on_closed(self, event):
         _log.debug("Calling callback on event {}. Calling {}".format(event, self._callback))
         try:
             self._callback(self._filetowatch)
